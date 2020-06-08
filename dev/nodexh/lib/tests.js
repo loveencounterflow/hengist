a1c8e4543f81654d152c726b40725ebf4348f75b
flow <flow@enceladus.orbit>
2020-06-08 17:13:06 +0200

    update

--- dev/nodexh/lib/tests.js
+++ dev/nodexh/lib/tests.js
@@ -97,15 +97,15 @@
       R = new this.Location();
       R.original = {};
       R.generated = {};
-      R.original.line_idx = p.originalLine;
-      R.original.column_idx = p.originalColumn;
       R.original.line_nr = p.originalLine + 1;
       R.original.column_nr = p.originalColumn + 1;
+      R.original.line_idx = p.originalLine;
+      R.original.column_idx = p.originalColumn;
       R.original.path = p.originalSource;
-      R.generated.line_idx = p.generatedLine;
-      R.generated.column_idx = p.generatedColumn;
       R.generated.line_nr = p.generatedLine + 1;
       R.generated.column_nr = p.generatedColumn + 1;
+      R.generated.line_idx = p.generatedLine;
+      R.generated.column_idx = p.generatedColumn;
       R.generated.path = path;
       return R;
     };
@@ -145,20 +145,91 @@
       T.ok(Object.isFrozen(p1.generated));
       T.ok(Object.isFrozen(p1));
       T.eq(type_of(p1), 'location');
-      T.eq(p1.original.line_idx, 16);
-      T.eq(p1.original.column_idx, 10);
       T.eq(p1.original.line_nr, 17);
       T.eq(p1.original.column_nr, 11);
+      T.eq(p1.original.line_idx, 16);
+      T.eq(p1.original.column_idx, 10);
       T.eq(p1.original.path.endsWith('/nodexh/src/assets-file1.coffee'));
-      T.eq(p1.generated.line_idx, 19);
-      T.eq(p1.generated.column_idx, 12);
       T.eq(p1.generated.line_nr, 20);
       T.eq(p1.generated.column_nr, 13);
+      T.eq(p1.generated.line_idx, 19);
+      T.eq(p1.generated.column_idx, 12);
       T.eq(p1.generated.path.endsWith('/nodexh/lib/assets-file1.js'));
     }
     if (error == null) {
       T.fail("^776^ expected error, got none");
     }
+    return done();
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this["find segments"] = function(T, done) {
+    var NODEXH, column_nr, error, frame, frames, i, isa, j, line_nr, p1, p2, ref, sourcemap, type_of, types, validate;
+    types = new (require('intertype')).Intertype();
+    ({isa, type_of, validate} = types);
+    // SMR                       = require 'source-map-resolve'
+    // SMC                       = require 'source-map'
+    provide.apply(NODEXH = {});
+    try {
+      //.........................................................................................................
+      require('./assets-file2');
+    } catch (error1) {
+      error = error1;
+      warn(error);
+      frames = NODEXH.frames_from_error(error);
+      frame = frames[0];
+      urge('^336^', frame);
+      sourcemap = NODEXH.new_sourcemap(frame, error);
+      p1 = NODEXH.map_frame(sourcemap, frame);
+      urge('^336^', p1);
+      for (line_nr = i = 1, ref = p1.generated.line_nr; (1 <= ref ? i <= ref : i >= ref); line_nr = 1 <= ref ? ++i : --i) {
+        for (column_nr = j = 1; j <= 10; column_nr = ++j) {
+          p2 = NODEXH._map_frame(sourcemap, frame.path, line_nr, column_nr);
+          // whisper '^77762^', line_nr, column_nr, p2.original
+          debug('^77762^', {line_nr, column_nr}, '->', {
+            line_nr: p2.original.line_nr,
+            column_nr: p2.original.column_nr
+          });
+        }
+      }
+    }
+    if (error == null) {
+      T.fail("^776^ expected error, got none");
+    }
+    //.........................................................................................................
+    return done();
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this["retrieve source code"] = function(T, done) {
+    /* TAINT can re-use sourcemap from same file */
+    /* TAINT how to deal with unmapped sources? */
+    var MODULE, NODEXH, error, frame, frames, i, isa, len, location, sourcemap, type_of, types, validate;
+    types = new (require('intertype')).Intertype();
+    ({isa, type_of, validate} = types);
+    // SMR                       = require 'source-map-resolve'
+    // SMC                       = require 'source-map'
+    provide.apply(NODEXH = {});
+    //.........................................................................................................
+    MODULE = require('./assets-file1');
+    T.eq(MODULE.my_function(42), 1764);
+    try {
+      MODULE.my_function('helo');
+    } catch (error1) {
+      error = error1;
+      error.stack = error.stack.replace(/\n.*guy-test[\s\S]*/, '');
+      help(error.stack);
+      error.stack = (error.stack + '\n').replace(/\n\s*->\s+[^\n]+\n/g, '\n');
+      warn(error);
+      frames = NODEXH.frames_from_error(error);
+      for (i = 0, len = frames.length; i < len; i++) {
+        frame = frames[i];
+        sourcemap = NODEXH.new_sourcemap(frame, error);
+        location = NODEXH.map_frame(sourcemap, frame);
+        urge('^336^', frame, location);
+      }
+    }
+    //.........................................................................................................
     return done();
   };
 
