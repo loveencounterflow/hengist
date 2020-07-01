12ff1ff0b27ada243ce6c81d8505b95551522e30
flow <flow@enceladus.orbit>
2020-07-01 12:52:39 +0200

    update

--- dev/datom/lib/xemitter.test.js
+++ dev/datom/lib/xemitter.test.js
@@ -8,7 +8,7 @@
 
   rpr = CND.rpr;
 
-  badge = 'DATOM/TESTS/SELECT';
+  badge = 'DATOM/TESTS/XEMITTER';
 
   debug = CND.get_logger('debug', badge);
 
@@ -29,46 +29,25 @@
 
   jr = JSON.stringify;
 
-  //-----------------------------------------------------------------------------------------------------------
-  this["_"] = async function(T, done) {
-    var DATOM, error, i, len, matcher, new_datom, probe, probes_and_matchers, select;
-    DATOM = require('../../../apps/datom');
-    ({new_datom, select} = DATOM.export());
-    //.........................................................................................................
-    probes_and_matchers = [
-      [
-        [
-          '^foo',
-          {
-            time: 1500000,
-            value: "msg#1"
-          }
-        ],
-        {
-          "time": 1500000,
-          "value": "msg#1",
-          "$key": "^foo"
-        },
-        null
-      ]
-    ];
-//.........................................................................................................
-    for (i = 0, len = probes_and_matchers.length; i < len; i++) {
-      [probe, matcher, error] = probes_and_matchers[i];
-      await T.perform(probe, matcher, error, function() {
-        return new Promise(function(resolve, reject) {
-          var key, value;
-          [key, value] = probe;
-          return resolve(new_datom(key, value));
-        });
-      });
-    }
-    done();
-    return null;
-  };
+  // #-----------------------------------------------------------------------------------------------------------
+  // @[ "_XEMITTER: _" ] = ( T, done ) ->
+  //   DATOM                     = require '../../../apps/datom'
+  //   { new_datom
+  //     select }                = DATOM.export()
+  //   #.........................................................................................................
+  //   probes_and_matchers = [
+  //     [['^foo', { time: 1500000, value: "msg#1", }],{"time":1500000,"value":"msg#1","$key":"^foo"},null]
+  //     ]
+  //   #.........................................................................................................
+  //   for [ probe, matcher, error, ] in probes_and_matchers
+  //     await T.perform probe, matcher, error, -> return new Promise ( resolve, reject ) ->
+  //       [ key, value, ] = probe
+  //       resolve new_datom key, value
+  //   done()
+  //   return null
 
   //-----------------------------------------------------------------------------------------------------------
-  this["public API shape"] = function(T, done) {
+  this["XEMITTER: public API shape"] = function(T, done) {
     var DATOM, XE, arity_of, isa, k, known_keys, new_datom, new_xemitter, select, type_of, types, validate;
     DATOM = require('../../../apps/datom');
     ({new_datom, new_xemitter, select} = DATOM.export());
@@ -103,8 +82,8 @@
   };
 
   //-----------------------------------------------------------------------------------------------------------
-  this["emit equivalently accepts key, value or datom"] = async function(T, done) {
-    var DATOM, XE, count, new_datom, new_xemitter, select;
+  this["XEMITTER: emit equivalently accepts key, value or datom"] = async function(T, done) {
+    var DATOM, XE, count, error, new_datom, new_xemitter, pattern, select;
     DATOM = require('../../../apps/datom');
     ({new_datom, new_xemitter, select} = DATOM.export());
     //.........................................................................................................
@@ -119,14 +98,35 @@
     });
     await XE.emit('^mykey', 42);
     await XE.emit(new_datom('^mykey', 42));
+    try {
+      //.........................................................................................................
+      await XE.emit({
+        $value: 42
+      });
+    } catch (error1) {
+      error = error1;
+      pattern = /expected a text or a datom got a object/;
+      if (pattern.test(error.message)) {
+        T.ok(true);
+      } else {
+        T.fail(`expected error to match ${pattern}, got ${rpr(error.message)}`);
+        throw error;
+      }
+    }
+    T.ok(error != null);
+    //.........................................................................................................
+    await XE.emit({
+      $key: '^mykey',
+      $value: 42
+    });
     await XE.emit(new_datom('^notmykey', 42));
-    T.eq(count, 2);
+    T.eq(count, 3);
     done();
     return null;
   };
 
   //-----------------------------------------------------------------------------------------------------------
-  this["throws when more than one contractor is added for given event key"] = function(T, done) {
+  this["XEMITTER: throws when more than one contractor is added for given event key"] = function(T, done) {
     var DATOM, XE, new_datom, new_xemitter, select;
     DATOM = require('../../../apps/datom');
     ({new_datom, new_xemitter, select} = DATOM.export());
@@ -142,7 +142,7 @@
   };
 
   //-----------------------------------------------------------------------------------------------------------
-  this["can listen to events that have no specific listener"] = async function(T, done) {
+  this["XEMITTER: can listen to events that have no specific listener"] = async function(T, done) {
     var DATOM, XE, keys, new_datom, new_xemitter, select;
     DATOM = require('../../../apps/datom');
     ({new_datom, new_xemitter, select} = DATOM.export());
@@ -158,7 +158,8 @@
       return keys.listen.push(d.$key);
     });
     XE.contract('^otherkey', function(d) {
-      return keys.contract.push(d.$key);
+      keys.contract.push(d.$key);
+      return "some value";
     });
     XE.listen_to_all(function(key, d) {
       return keys.all.push(d.$key);
@@ -169,12 +170,60 @@
     await XE.emit('^mykey');
     await XE.emit('^otherkey');
     await XE.emit('^thirdkey');
+    await XE.delegate('^otherkey');
     // debug keys
     T.eq(keys, {
       listen: ['^mykey'],
-      contract: ['^otherkey'],
-      all: ['^mykey', '^otherkey', '^thirdkey'],
+      contract: ['^otherkey', '^otherkey'],
+      all: ['^mykey', '^otherkey', '^thirdkey', '^otherkey'],
       unheard: ['^thirdkey']
+    });
+    done();
+    return null;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this["XEMITTER: delegation"] = async function(T, done) {
+    var DATOM, XE, keys, new_datom, new_xemitter, select;
+    DATOM = require('../../../apps/datom');
+    ({new_datom, new_xemitter, select} = DATOM.export());
+    XE = new_xemitter();
+    //.........................................................................................................
+    keys = {
+      listen: [],
+      all: [],
+      contract: []
+    };
+    XE.listen_to('^log', function(d) {
+      keys.listen.push(d.$key);
+      return urge(d);
+    });
+    XE.contract('^add', function(d) {
+      var ref, ref1;
+      keys.contract.push(d.$key);
+      return ((ref = d != null ? d.a : void 0) != null ? ref : 0) + ((ref1 = d != null ? d.b : void 0) != null ? ref1 : 0);
+    });
+    XE.contract('^multiply', function(d) {
+      var ref, ref1;
+      keys.contract.push(d.$key);
+      return ((ref = d != null ? d.a : void 0) != null ? ref : 1) * ((ref1 = d != null ? d.b : void 0) != null ? ref1 : 1);
+    });
+    XE.listen_to_all(function(key, d) {
+      return keys.all.push(d.$key);
+    });
+    await XE.emit('^log', "message");
+    T.eq((await XE.delegate('^add', {
+      a: 123,
+      b: 456
+    })), 579);
+    T.eq((await XE.delegate('^multiply', {
+      a: 123,
+      b: 456
+    })), 56088);
+    T.eq(keys, {
+      listen: ['^log'],
+      all: ['^log', '^add', '^multiply'],
+      contract: ['^add', '^multiply']
     });
     done();
     return null;
@@ -188,6 +237,10 @@
   }
 
   // test @[ "public API shape" ]
+// test @[ "XEMITTER: can listen to events that have no specific listener" ]
+// test @[ "XEMITTER: delegation" ]
+// test @[ "can listen to events that have no specific listener 2" ]
+// test @[ "emit equivalently accepts key, value or datom" ]
 
 }).call(this);
 
