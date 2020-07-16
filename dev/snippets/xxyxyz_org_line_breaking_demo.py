997a6aa07111b1ace99d9eb9e56cd8c871c8367d
flow <flow@enceladus.orbit>
2020-07-16 16:02:10 +0200

    add comments, examples

--- dev/snippets/xxyxyz_org_line_breaking_demo.py
+++ dev/snippets/xxyxyz_org_line_breaking_demo.py
@@ -24,7 +24,51 @@ $ python3.6 dev/snippets/wordwrap-with-dynamic-programming.py
                     |strength and for his numerous far-ranging adventures.                                                                        |
 ```
 
-This fault seems to appear at some arbitrary line length.
+This fault seems to appear at some arbitrary line length. That it *is* a fault and not a desirable outcome
+is corroborated by the observation that it fails at the examples given in [Jacob N Smith's detailed
+discussion of the Knuth-Plass
+algorithm](https://github.com/jaroslov/knuth-plass-thoughts/blob/master/plass.md). Where Smith gives this
+solution for an optimal word wrap (colons inserted to indicate an 'ideal rectangle of type'):
+
+```
+xxxx x xxxx xxxxx xx xx xxxxx : |
+x xx xxxxxxx xxxxx xxx xxxx xxx |
+xxxxx x x xxx xxxx xxx xx xx  : |
+xxxxxxx x xxxx xx xxxx xxx xxx: |
+xx
+```
+
+the algorithms implemented in the present module result in this solution:
+
+```
+xxxx x xxxx xxxxx xx xx  :      |
+xxxxx x xx xxxxxxx xxxxx :      |
+xxx xxxx xxx xxxxx x x xxx      |
+xxxx xxx xx xx xxxxxxx x :      |
+xxxx xx xxxx xxx xxx xx  :      |
+```
+
+for the exact same text and line width. The solution arrived at by `wordwrap-with-dynamic-programming.py` is
+the exact same as Smiths, only set justified by filling in extra spaces:
+
+```
+xxxx x  xxxx  xxxxx xx xx  xxxxx|
+x xx xxxxxxx xxxxx xxx  xxxx xxx|
+xxxxx x  x xxx  xxxx xxx  xx  xx|
+xxxxxxx  x xxxx  xx xxxx xxx xxx|
+xx                              |
+```
+
+If we were to justify the xxyxyz.org solution, the gaps between words become appreciably wider, so much that
+yawning rivers open up in the typeset material:
+
+```
+xxxx   x   xxxx   xxxxx  xx   xx|
+xxxxx   x   xx   xxxxxxx   xxxxx|
+xxx  xxxx  xxx  xxxxx  x  x  xxx|
+xxxx   xxx   xx  xx   xxxxxxx  x|
+xxxx xx xxxx xxx xxx xx         |
+```
 
 """
 
@@ -44,7 +88,6 @@ def show( title, width, lines ):
 #-----------------------------------------------------------------------------------------------------------
 def demo():
   import re
-  text            = "Tushar Roy likes to code"
   text            = "Geeks for Geeks presents word wrap problem"
   # text            = "a b c d e f g h i j k l m n o p qqqqqqqqq"
   text            = """Hercules (/ˈhɜːrkjuliːz, -jə-/) is a Roman hero and god. He was the Roman equivalent
@@ -52,11 +95,13 @@ def demo():
                       and the mortal Alcmene. In classical mythology, Hercules is famous for his strength
                       and for his numerous far-ranging adventures."""
   text            = 'x ' * 200
+  text            = "Tushar Roy likes to code"
   text            = "aaaaaa bbb ccccc dd eeee xxxxxxxxxxx"
+  text            = "xxxx x xxxx xxxxx xx xx xxxxx x xx xxxxxxx xxxxx xxx xxxx xxx xxxxx x x xxx xxxx xxx xx xx xxxxxxx x xxxx xx xxxx xxx xxx xx"
   text            = re.sub( r'\n', ' ', text )
   text            = re.sub( r'\s+', ' ', text )
   text            = text.strip()
-  width           = 10
+  width           = 32
   # width           = max( 9, *( len( word ) for word in ' '.split( text ) ) )
   print( f"line width set to {width}" )
   # show( 'naive',    width, XX.naive(    text, width ) )
@@ -64,7 +109,7 @@ def demo():
   # show( 'shortest', width, XX.shortest( text, width ) )
   # show( 'binary',   width, XX.binary(   text, width ) )
   # show( 'linear',   width, XX.linear(   text, width ) )
-  show( 'linear2',  width, XX.linear2(  text, width ) )
+  # show( 'linear2',  width, XX.linear2(  text, width ) )
   show( 'divide',   width, XX.divide(   text, width ) )
 
 ############################################################################################################
