606ff02896bd7837e294b8094fa44c927b967d7a
flow <flow@enceladus.orbit>
2020-07-16 16:02:47 +0200

    add samples

--- dev/snippets/wordwrap-with-dynamic-programming.py
+++ dev/snippets/wordwrap-with-dynamic-programming.py
@@ -141,7 +141,6 @@ def get_lines( words, line_width, line_breaks ):
 #-----------------------------------------------------------------------------------------------------------
 def demo():
   import re
-  text            = "Tushar Roy likes to code"
   text            = "Geeks for Geeks presents word wrap problem"
   text            = """a b c d e
                     f g h i j
@@ -160,13 +159,100 @@ def demo():
                       of the Greek divine hero Heracles, who was the son of Zeus (Roman equivalent Jupiter)
                       and the mortal Alcmene. In classical mythology, Hercules is famous for his strength
                       and for his numerous far-ranging adventures."""
+  # text            = """\xa0\xa0In olden times when wishing still helped one, there lived a king whose daughters were
+  text            = """__In olden times when wishing still helped one, there lived a king whose daughters were
+                      all beautiful, but the youngest was so beautiful that the sun itself, which has seen so
+                      much, was astonished whenever it shone in her face.
+
+                      Close by the king's castle lay a great dark forest, and under an old lime-tree in the
+                      forest was a well, and when the day was very warm, the king's child went out into the
+                      forest and sat down by the side of the cool fountain, and when she was bored she took
+                      a golden ball, and threw it up on high and caught it, and this ball was her favorite
+                      plaything.
+
+                      Now it so happened that on one occasion the princess's golden ball did not fall into
+                      the little hand which she was holding up for it, but on to the ground beyond, and
+                      rolled straight into the water. The king's daughter followed it with her eyes, but it
+                      vanished, and the well was deep, so deep that the bottom could not be seen. At this
+                      she began to cry, and cried louder and louder, and could not be comforted.
+
+                      And as she thus lamented someone said to her, "What ails you, king's daughter? You
+                      weep so that even a stone would show pity."
+
+                      She looked round to the side from whence the voice came, and saw a frog stretching
+                      forth its big, ugly head from the water.
+
+                      "Ah, oldwater-splasher, is it you," she said, "I am weeping for my golden ball, which
+                      has fallen into the well."
+
+                      "Be quiet, and do not weep," answered the frog, "I can help you, but what will you
+                      give me if I bring your plaything up again?"
+
+                      "Whatever you will have, dear frog," said she, "My clothes, my pearls and jewels, and
+                      even the golden crown which I am wearing."
+
+                      The frog answered, "I do not care for your clothes, your pearls and jewels, nor for
+                      your golden crown, but if you will love me and let me be your companion and
+                      play-fellow, and sit by you at your little table, and eat off your little golden
+                      plate, and drink out of your little cup, and sleep in your little bed - if you will
+                      promise me this I will go down below, and bring you your golden ball up again."
+
+                      "Oh yes," said she, "I promise you all you wish, if you will but bring me my ball back
+                      again." But she thought, "How the silly frog does talk. All he does is to sit in the
+                      water with the other frogs, and croak. He can be no companion to any human being."
+
+                      But the frog when he had received this promise, put his head into the water and sank
+                      down; and in a short while came swimming up again with the ball in his mouth, and
+                      threw it on the grass.
+
+                      The king's daughter was delighted to see her pretty plaything once more, and picked it
+                      up, and ran away with it. "Wait, wait," said the frog. "Take me with you. I can't run
+                      as you can." But what did it avail him to scream his croak, croak, after her, as
+                      loudly as he could. She did not listen to it, but ran home and soon forgot the poor
+                      frog, who was forced to go back into his well again.
+
+                      The next day when she had seated herself at table with the king and all the courtiers,
+                      and was eating from her little golden plate, something came creeping splish splash,
+                      splish splash, up the marble staircase, and when it had got to the top, it knocked at
+                      the door and cried, "Princess, youngest princess, open the door for me."
+
+                      She ran to see who was outside, but when she opened the door, there sat the frog in
+                      front of it. Then she slammed the door to, in great haste, sat down to dinner again,
+                      and was quite frightened.
+
+                      The king saw plainly that her heart was beating violently, and said, "My child, what
+                      are you so afraid of? Is there perchance a giant outside who wants to carry you away?"
+
+                      "Ah, no," replied she. "It is no giant but a disgusting frog. Yesterday as I was in
+                      the forest sitting by the well, playing, my golden ball fell into the water. And
+                      because I cried so, the frog brought it out again for me, and because he so insisted,
+                      I promised him he should be my companion, but I never thought he would be able to come
+                      out of his water. And now he is outside there, and wants to come in to me."
+
+                      In the meantime it knocked a second time, and cried, "Princess, youngest princess,
+                      open the door for me, do you not know what you said to me yesterday by the cool waters
+                      of the well. Princess, youngest princess, open the door for me."
+
+                      Then said the king, "That which you have promised must you perform. Go and let him
+                      in."
+
+                      She went and opened the door, and the frog hopped in and followed her, step by step,
+                      to her chair. There he sat and cried, "Lift me up beside you."
+
+                      She delayed, until at last the king commanded her to do it. Once the frog was on the
+                      chair he wanted to be on the table, and when he was on the table he said, "Now, push
+                      your little golden plate nearer to me that we may eat together."
+                      """
+  text            = "the im- mediately following week"
+  text            = "Tushar Roy likes to code"
+  text            = "xxxx x xxxx xxxxx xx xx xxxxx x xx xxxxxxx xxxxx xxx xxxx xxx xxxxx x x xxx xxxx xxx xx xx xxxxxxx x xxxx xx xxxx xxx xxx xx"
   text            = re.sub( r'\n', ' ', text )
-  text            = re.sub( r'\s+', ' ', text )
+  text            = re.sub( r'\x20+', ' ', text )
   text            = text.strip()
-  _words          = re.split( r'\s+', text )
+  _words          = re.split( r'\x20+', text )
   words           = []
   # print( "^786^ words:", words )
-  line_width      = 50
+  line_width      = 32
   for word in _words:
     word_length = len( word )
     if word_length > line_width:
