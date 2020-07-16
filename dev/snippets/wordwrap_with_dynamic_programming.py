274fe9d3a60a5d698ee9bd468cb62447905ab72b
flow <flow@enceladus.orbit>
2020-07-16 20:28:14 +0200

    improve code comments, variable names

--- dev/snippets/wordwrap_with_dynamic_programming.py
+++ dev/snippets/wordwrap_with_dynamic_programming.py
@@ -35,20 +35,18 @@ def new_vector( x, value = 0 ):
 def wrap_monospaced_dp( word_lengths, word_count, line_width ):
   # For simplicity, 1 extra space is used in all below arrays.
   #.........................................................................................................
-  # * extras[ i ][ j ] will have number of extra spaces if words from i to j are put in a single line
-  # * lc[ i ][ j ] will have cost of a line which has words from i to j
-  # define two square matrix extraSpace and lineCost of order (size + 1)
-  # define two array totalCost and solution of size (size + 1)
+  # * `extras[ i ][ j ]` will have number of extra spaces if words from `i` to `j` are put in a single line
+  # * `lc[ i ][ j ]` will have cost of a line which has words from `i` to `j`
   extras  = new_matrix( word_count, value = 0 )
   lc      = new_matrix( word_count, value = 0 )
   #.........................................................................................................
-  # * c[ i ] will have total cost of optimal arrangement of words from 1 to i
-  # * p[] is used to print the solution.
-  c       = new_vector( word_count, value = 0 )
-  p       = new_vector( word_count, value = 0 )
+  # * `c[ i ]` will have total cost of optimal arrangement of words from `1` to `i`
+  # * `line_breaks[]` contains indexes into `word_lengths`
+  c           = new_vector( word_count, value = 0 )
+  line_breaks = new_vector( word_count, value = 0 )
   #.........................................................................................................
-  # calculate extra spaces in a single line. The value extra[ i ][ j ] indicates extra spaces if words from
-  # word number i to j are placed in a single line
+  # calculate extra spaces in a single line. The value `extras[ i ][ j ]` indicates extra spaces if words
+  # from word number `i` to `j` are placed in a single line
   for i in range( word_count + 1 ):
     local_word_count  = 1
     extras[ i ][ i ]  = get_badness( line_width, local_word_count, line_width - word_lengths[ i - 1 ] )
@@ -57,16 +55,16 @@ def wrap_monospaced_dp( word_lengths, word_count, line_width ):
       extraspace_count  = extras[ i ][ j - 1 ] - word_lengths[ j - 1 ] - 1
       extras[ i ][ j ]  = get_badness( line_width, local_word_count, extraspace_count )
   #.........................................................................................................
-  # Calculate line cost corresponding to the above calculated extra spaces. The value lc[ i ][ j ] indicates
-  # cost of putting words from word number i to j in a single line
+  # Calculate line cost corresponding to the above calculated extra spaces. The value `lc[ i ][ j ]`
+  # indicates cost of putting words from word number `i` to `j` in a single line
   for i in range( word_count + 1 ):
     for j in range( i, word_count + 1 ):
       if extras[ i ][ j ] < 0:                        lc[ i ][ j ] = infinity
       elif j == word_count and extras[ i ][ j ] >= 0: lc[ i ][ j ] = 0
       else:                                           lc[ i ][ j ] = ( extras[ i ][ j ] * extras[ i ][ j ] )
   #.........................................................................................................
-  # Calculate minimum cost and find minimum cost arrangement. The value c[ j ] indicates optimized cost to
-  # arrange words from word number 1 to j.
+  # Calculate minimum cost and find minimum cost arrangement. The value `c[ j ]` indicates optimized cost to
+  # arrange words from word number `1` to `j`.
   c[ 0 ] = 0
   for j in range( 1, word_count + 1 ):
     c[ j ] = infinity
@@ -76,8 +74,8 @@ def wrap_monospaced_dp( word_lengths, word_count, line_width ):
       xxx = c[ i - 1 ] + lc[ i ][ j ]
       if xxx >= c[ j ]: continue
       c[ j ] = xxx
-      p[ j ] = i
-  return p
+      line_breaks[ j ] = i
+  return line_breaks
 
 #-----------------------------------------------------------------------------------------------------------
 def lines_from_line_breaks( words, line_breaks ):
