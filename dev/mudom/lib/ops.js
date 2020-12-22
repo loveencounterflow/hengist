c2d373cff9414384373e8c5b9790dc906c8286e6
flow <flow@enceladus.orbit>
2020-12-22 15:36:14 +0100

    update

--- dev/mudom/lib/ops.js
+++ dev/mudom/lib/ops.js
@@ -1,5 +1,5 @@
 (function() {
-  var CND, INTERTYPE, Intermatic, after, handler, log, rpr, sleep;
+  var CND, INTERTYPE, Intermatic, after, log, rpr, sleep, update_button;
 
   globalThis.µ = require('mudom');
 
@@ -53,59 +53,61 @@
   */
   //-----------------------------------------------------------------------------------------------------------
   µ.DOM.ready(function() {
-    var button, i, latches, len, ref;
-    latches = {};
+    var button, i, len, ref;
     ref = µ.DOM.select_all('.btn');
     for (i = 0, len = ref.length; i < len; i++) {
       button = ref[i];
       (function(button) {
         var keyname;
         keyname = µ.DOM.get(button, 'name');
-        latches[keyname] = false;
-        if (keyname !== ' ') {
-          µ.KB._listen_to_key(keyname, 'down', function(event) {
-            return µ.DOM.swap_class(button, 'false', 'true');
-          });
-          µ.KB._listen_to_key(keyname, 'up', function(event) {
-            return µ.DOM.swap_class(button, 'true', 'false');
-          });
-        }
-        return µ.KB._listen_to_key(keyname, 'double', function(event) {
-          var latched;
-          latched = latches[keyname] = !latches[keyname];
-          if (latched) {
-            µ.DOM.swap_class(button, 'false', 'true');
-          } else {
-            µ.DOM.swap_class(button, 'true', 'false');
-          }
-          return null;
+        //.....................................................................................................
+        µ.KB._listen_to_key(keyname, 'down', function(d) {
+          return update_button(button, keyname, d.state);
+        });
+        //.....................................................................................................
+        µ.KB._listen_to_key(keyname, 'up', function(d) {
+          return update_button(button, keyname, d.state);
+        });
+        //.....................................................................................................
+        return µ.KB._listen_to_key(keyname, 'double', function(d) {
+          return update_button(button, keyname, d.state);
         });
       })(button);
     }
+    log('^44454^', µ.KB._registry);
     return null;
   });
 
   //-----------------------------------------------------------------------------------------------------------
-  handler = (d) => {
-    return log("^22209^", d);
+  update_button = (button, name, state) => {
+    var classes, k, text, v;
+    text = `${name} `;
+    text += ((function() {
+      var results;
+      results = [];
+      for (k in state) {
+        v = state[k];
+        if (v) {
+          results.push(k);
+        }
+      }
+      return results;
+    })()).join(' ');
+    classes = `btn ${text}`;
+    µ.DOM.set(button, 'class', classes);
+    button.innerHTML = text;
+    return null;
   };
 
-  µ.KB._listen_to_key('Alt', 'down', handler);
-
-  µ.KB._listen_to_key('ä', 'down', handler);
-
-  µ.KB._listen_to_key('Shift', 'up', handler);
-
-  µ.KB._listen_to_key('Shift', 'down', handler);
-
-  µ.KB._listen_to_key('Shift', 'double', handler);
-
-  µ.KB._listen_to_key('Alt', 'double', handler);
-
-  µ.KB._listen_to_key('AltGraph', 'double', handler);
-
-  // µ.KB._listen_to_key 'Alt', null,   ( d ) => log "^22209^ 'Alt', null,   ", d
-// µ.KB._listen_to_key null, 'down',  ( d ) => log "^22209^ null, 'down',  ", d
+  // µ.KB._listen_to_key 'Alt',      'down',   handler
+// µ.KB._listen_to_key 'ä',        'down',   handler
+// µ.KB._listen_to_key 'Shift',    'up',     handler
+// µ.KB._listen_to_key 'Shift',    'down',   handler
+// µ.KB._listen_to_key 'Shift',    'double', handler
+// µ.KB._listen_to_key 'Alt',      'double', handler
+// µ.KB._listen_to_key 'AltGraph', 'double', handler
+// # µ.KB._listen_to_key 'Alt', null,   ( d ) => log "^22209^ 'Alt', null,   ", d
+// # µ.KB._listen_to_key null, 'down',  ( d ) => log "^22209^ null, 'down',  ", d
 
   // µ.KB._listen_to_key null, null,    ( d ) => log "^22209^ null, null,    ", d
 
