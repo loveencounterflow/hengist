dffc8c65891a7cd37bb11c0c94feeb681a304f73
flow <flow@enceladus.orbit>
2020-12-27 20:38:06 +0100

    update

--- dev/mudom/lib/ops.js
+++ dev/mudom/lib/ops.js
@@ -27,37 +27,25 @@
 
   //-----------------------------------------------------------------------------------------------------------
   µ.DOM.ready(function() {
-    var _keys_from_button, keynames, update_button;
+    var keynames, update_button;
+    // #---------------------------------------------------------------------------------------------------------
+    // _keys_from_button = ( button ) ->
+    //   switch µ.DOM.get button, 'latching'
+    //     when 'toggle' then  return  [ 'up', 'down', 'toggle', ]
+    //     when 'latch'  then  return  [ 'up', 'down', 'latch', ]
+    //     when 'both'   then  return  [ 'up', 'down', 'toggle', 'latch', ]
+    //     when 'tlatch' then  return  [ 'up', 'down', 'tlatch', ]
+    //   return                        [ 'up', 'down', ]
+
     //---------------------------------------------------------------------------------------------------------
-    _keys_from_button = function(button) {
-      switch (µ.DOM.get(button, 'latching')) {
-        case 'toggle':
-          return ['up', 'down', 'toggle'];
-        case 'latch':
-          return ['up', 'down', 'latch'];
-        case 'both':
-          return ['up', 'down', 'toggle', 'latch'];
-        case 'tlatch':
-          return ['up', 'down', 'tlatch'];
+    update_button = (button, keyname, behavior, state) => {
+      if (state) {
+        button.innerHTML = `${keyname} ${behavior}`;
+        µ.DOM.add_class(button, behavior);
+      } else {
+        button.innerHTML = `${keyname} <strike>${behavior}</strike>`;
+        µ.DOM.remove_class(button, behavior);
       }
-      return ['up', 'down'];
-    };
-    //---------------------------------------------------------------------------------------------------------
-    update_button = (button, name, state) => {
-      var classes, i, key, len, ref, ref1, text;
-      text = `${name} `;
-      ref = _keys_from_button(button);
-      for (i = 0, len = ref.length; i < len; i++) {
-        key = ref[i];
-        if (!((ref1 = state[key]) != null ? ref1 : false)) {
-          continue;
-        }
-        text += `${key} `;
-      }
-      // log '^4443^', ( µ.DOM.get button, 'id' ), name, state
-      classes = `btn ${text}`;
-      button.innerHTML = text;
-      µ.DOM.set(button, 'class', classes);
       return null;
     };
     //---------------------------------------------------------------------------------------------------------
@@ -77,30 +65,62 @@
       return keynames = [...(new Set(keynames))];
     })();
     (() => {      //.........................................................................................................
-      var behavior, i, j, keyname, len, len1, ref;
-//.......................................................................................................
+      var all_buttons, behavior, button, buttons, i, j, k, keyname, len, len1, len2, ref;
+      //.......................................................................................................
+      // debug '^3646346^', µ.DOM.select_all '.btn'
+      all_buttons = [...(µ.DOM.select_all('.btn'))];
       for (i = 0, len = keynames.length; i < len; i++) {
         keyname = keynames[i];
-        ref = ['up', 'down', 'toggle', 'latch', 'tlatch'];
+        ref = ['push', 'toggle', 'latch', 'tlatch'];
         for (j = 0, len1 = ref.length; j < len1; j++) {
           behavior = ref[j];
-          ((keyname, behavior) => {
-            //.................................................................................................
-            debug('^ops@4453^', {keyname, behavior});
-            µ.KB._listen_to_key(keyname, behavior, (d) => {
-              var button, k, len2, ref1;
-              ref1 = µ.DOM.select_all(`.btn[name=${keyname}]`);
-              for (k = 0, len2 = ref1.length; k < len2; k++) {
-                button = ref1[k];
-                update_button(button, keyname, d.state);
-              }
+          // debug '^ops@4453^', { keyname, behavior, }
+          buttons = all_buttons.filter((x) => {
+            return keyname === µ.DOM.get(x, 'name');
+          });
+          buttons = (function() {
+            switch (behavior) {
+              case 'push':
+                return buttons;
+              case 'toggle':
+                return buttons.filter((x) => {
+                  var ref1;
+                  return (ref1 = µ.DOM.get(x, 'latching')) === 'both' || ref1 === 'toggle';
+                });
+              case 'latch':
+                return buttons.filter((x) => {
+                  var ref1;
+                  return (ref1 = µ.DOM.get(x, 'latching')) === 'both' || ref1 === 'latch';
+                });
+              case 'tlatch':
+                return buttons.filter((x) => {
+                  return (µ.DOM.get(x, 'latching')) === 'tlatch';
+                });
+            }
+          }).call(this);
+// debug '^ops@338^', { keyname, behavior, buttons, }
+          for (k = 0, len2 = buttons.length; k < len2; k++) {
+            button = buttons[k];
+            ((keyname, behavior, button) => {
+              //...............................................................................................
+              µ.KB._listen_to_key(keyname, behavior, (d) => {
+                if (behavior === 'latch') {
+                  debug('^ops@338^', {
+                    keyname,
+                    behavior,
+                    state: d.state,
+                    d
+                  });
+                }
+                update_button(button, keyname, behavior, d.state);
+                return null;
+              });
               return null;
-            });
-            //.................................................................................................
-            return null;
-          })(keyname, behavior);
+            })(keyname, behavior, button);
+          }
         }
       }
+      //.................................................................................................
       return null;
     })();
     //.........................................................................................................
