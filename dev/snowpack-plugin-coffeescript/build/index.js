6596e41a5719952502deb52b949c9aed8b182e8f
flow <flow@enceladus.orbit>
2021-01-19 19:45:21 +0100

    TS transpilation is working!

--- dev/snowpack-plugin-coffeescript/build/index.js
+++ dev/snowpack-plugin-coffeescript/build/index.js
@@ -1,8 +1,4 @@
 // my-first-snowpack/index.js
 import { hello_world } from './hello-world.js';
-import { helo } from './dist/sample.js';
 
 hello_world();
-helo();
-
-
---
+++ dev/snowpack-plugin-coffeescript/src/sample.ts
@@ -0,0 +1,12 @@
+
+
+
+console.log( '^sample.ts@33381^', "loaded typescript module" );
+export function helo() {
+  console.log( '^sample.ts@33387^', "helo" ); }
+
+// export helo;
+
+
+
+
