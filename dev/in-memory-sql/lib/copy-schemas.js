ec0f6384f74af603c91f60872fbf5a601ed38785
flow <flow@enceladus.orbit>
2021-03-20 09:22:23 +0100

    update

--- dev/in-memory-sql/lib/copy-schemas.js
+++ dev/in-memory-sql/lib/copy-schemas.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  var BM, CND, DATA, FS, FSP, LFT, PATH, alert, badge, data_cache, debug, echo, gcfg, help, info, isa, jr, log, pragmas, resolve_path, rpr, show_result, test, try_to_remove_file, types, urge, validate, warn, whisper;
+  var BM, CND, DATA, FS, FSP, LFT, PATH, alert, badge, data_cache, debug, echo, gcfg, help, info, isa, jr, log, pragmas, resolve_path, rpr, show_result, try_to_remove_file, types, urge, validate, warn, whisper;
 
   //###########################################################################################################
   CND = require('cnd');
@@ -36,8 +36,7 @@
 
   DATA = require('../../../lib/data-providers-nocache');
 
-  test = require('guy-test');
-
+  // test                      = require 'guy-test'
   ({jr} = CND);
 
   BM = require('../../../lib/benchmarks');
@@ -135,14 +134,13 @@
         save: null
       };
       cfg = {...defaults, ...cfg};
-      db_work_path = cfg.db.work[cfg.mode].replaceAll('{ref}', cfg.ref);
-      validate.nonempty_text(cfg.ref);
       //.........................................................................................................
       /* TAINT use proper string interpolation */
+      validate.nonempty_text(cfg.ref);
+      db_work_path = cfg.db.work[cfg.mode].replaceAll('{ref}', cfg.ref);
       db_template_path = cfg.db.templates[cfg.size].replaceAll('{ref}', cfg.ref);
       db_target_path = cfg.db.target[cfg.size].replaceAll('{ref}', cfg.ref);
       db_temp_path = cfg.db.temp[cfg.size].replaceAll('{ref}', cfg.ref);
-      db_temp_path = db_temp_path.replaceAll('{save}', cfg.save);
       //.........................................................................................................
       validate.nonempty_text(db_template_path);
       validate.nonempty_text(db_target_path);
@@ -180,10 +178,20 @@
             _icql.pragma(pragma);
           }
           fle_schema = 'main';
-          work_schema = 'x';
-          work_schema_x = _icql.as_identifier('x');
-          _icql.attach(db_work_path, work_schema);
-          _icql.copy_schema(fle_schema, work_schema);
+          switch (cfg.mode) {
+            case 'mem':
+              work_schema = 'x';
+              work_schema_x = _icql.as_identifier(work_schema);
+              _icql.attach(db_work_path, work_schema);
+              _icql.copy_schema(fle_schema, work_schema);
+              break;
+            case 'fle':
+              work_schema = 'main';
+              work_schema_x = _icql.as_identifier(work_schema);
+              break;
+            default:
+              throw new Error(`^44788^ unknown value for \`cfg.mode\`: ${rpr(cfg.mode)}`);
+          }
           //-------------------------------------------------------------------------------------------------------
           db.exec(`drop table if exists ${work_schema_x}.test;`);
           db.exec(`create table ${work_schema_x}.test(
@@ -232,7 +240,7 @@
   this.btsql3_mem_small_backup = (cfg) => {
     return this._btsql3({
       ...cfg,
-      ref: 'small',
+      ref: 'mem_small_backup',
       mode: 'mem',
       size: 'small',
       pragmas: pragmas.mem,
@@ -243,7 +251,7 @@
   this.btsql3_mem_big_backup = (cfg) => {
     return this._btsql3({
       ...cfg,
-      ref: 'big',
+      ref: 'mem_big_backup',
       mode: 'mem',
       size: 'big',
       pragmas: pragmas.mem,
@@ -254,7 +262,7 @@
   this.btsql3_mem_small_copy = (cfg) => {
     return this._btsql3({
       ...cfg,
-      ref: 'small',
+      ref: 'mem_small_copy',
       mode: 'mem',
       size: 'small',
       pragmas: pragmas.mem,
@@ -265,7 +273,7 @@
   this.btsql3_mem_big_copy = (cfg) => {
     return this._btsql3({
       ...cfg,
-      ref: 'big',
+      ref: 'mem_big_copy',
       mode: 'mem',
       size: 'big',
       pragmas: pragmas.mem,
@@ -276,7 +284,7 @@
   this.btsql3_fle_small = (cfg) => {
     return this._btsql3({
       ...cfg,
-      ref: 'small',
+      ref: 'fle_small',
       mode: 'fle',
       size: 'small',
       pragmas: pragmas.fle
@@ -286,7 +294,7 @@
   this.btsql3_fle_big = (cfg) => {
     return this._btsql3({
       ...cfg,
-      ref: 'big',
+      ref: 'fle_big',
       mode: 'fle',
       size: 'big',
       pragmas: pragmas.fle
@@ -296,7 +304,7 @@
   this.btsql3_fle_small_bare = (cfg) => {
     return this._btsql3({
       ...cfg,
-      ref: 'small',
+      ref: 'fle_small_bare',
       mode: 'fle',
       size: 'small',
       pragmas: pragmas.bare
@@ -306,7 +314,7 @@
   this.btsql3_fle_big_bare = (cfg) => {
     return this._btsql3({
       ...cfg,
-      ref: 'big',
+      ref: 'fle_big_bare',
       mode: 'fle',
       size: 'big',
       pragmas: pragmas.bare
@@ -324,7 +332,7 @@
     gcfg.echo = false;
     bench = BM.new_benchmarks();
     cfg = {
-      word_count: 1000,
+      word_count: 100_000,
       db: {
         templates: {
           small: resolve_path('assets/icql/small-datamill.db'),
@@ -339,14 +347,18 @@
           fle: 'data/icql/copy-schemas-work-{ref}.db'
         },
         temp: {
-          small: resolve_path('data/icql/copy-schemas-benchmarks-temp-{ref}-{save}.db'),
-          big: resolve_path('data/icql/copy-schemas-benchmarks-temp-{ref}-{save}.db')
+          small: resolve_path('data/icql/copy-schemas-benchmarks-temp-{ref}.db'),
+          big: resolve_path('data/icql/copy-schemas-benchmarks-temp-{ref}.db')
         }
       }
     };
     repetitions = 3;
-    test_names = ['btsql3_fle_small', 'btsql3_mem_small_backup', 'btsql3_mem_big_backup', 'btsql3_mem_small_copy', 'btsql3_mem_big_copy', 'btsql3_fle_big', 'btsql3_fle_small_bare', 'btsql3_fle_big_bare'];
+    test_names = ['btsql3_mem_small_backup', 'btsql3_mem_big_backup', 'btsql3_fle_small', 'btsql3_fle_big'];
     if (global.gc != null) {
+      // 'btsql3_mem_small_copy'
+      // 'btsql3_mem_big_copy'
+      // 'btsql3_fle_small_bare'
+      // 'btsql3_fle_big_bare'
       global.gc();
     }
     data_cache = null;
@@ -370,6 +382,56 @@
       return (await this.run_benchmarks());
     })();
   }
+
+  /*
+
+ ~/jzr/hengist  master !3  nodexh ~/jzr/hengist/dev/in-memory-sql/lib/copy-schemas.js                                                            ✔
+00:00 IN-MEMORY-SQL  ▶  ------------------------------------------------------------------------------------------------------------
+00:01 IN-MEMORY-SQL  ▶  retrieving test data...
+00:01 IN-MEMORY-SQL  ▶  ...done
+btsql3_fle_big                             0.640 s          10,000 items          15,622⏶Hz          64,012⏷nspc
+btsql3_fle_small                           0.643 s          10,000 items          15,551⏶Hz          64,303⏷nspc
+btsql3_mem_small_backup                    0.386 s          10,000 items          25,934⏶Hz          38,560⏷nspc
+btsql3_mem_big_backup                      0.420 s          10,000 items          23,796⏶Hz          42,024⏷nspc
+00:03 IN-MEMORY-SQL  ▶  ------------------------------------------------------------------------------------------------------------
+btsql3_fle_small                           0.649 s          10,000 items          15,417⏶Hz          64,865⏷nspc
+btsql3_mem_big_backup                      0.422 s          10,000 items          23,672⏶Hz          42,245⏷nspc
+btsql3_fle_big                             0.630 s          10,000 items          15,884⏶Hz          62,955⏷nspc
+btsql3_mem_small_backup                    0.364 s          10,000 items          27,478⏶Hz          36,393⏷nspc
+00:05 IN-MEMORY-SQL  ▶  ------------------------------------------------------------------------------------------------------------
+btsql3_mem_big_backup                      0.425 s          10,000 items          23,509⏶Hz          42,536⏷nspc
+btsql3_fle_big                             0.630 s          10,000 items          15,870⏶Hz          63,012⏷nspc
+btsql3_mem_small_backup                    0.371 s          10,000 items          26,937⏶Hz          37,124⏷nspc
+btsql3_fle_small                           0.635 s          10,000 items          15,742⏶Hz          63,525⏷nspc
+00:07 HENGIST/BENCHMARKS  ▶  btsql3_mem_small_backup                           26,783 Hz ≙ 1 ÷ 1.0       100.0 % │████████████▌│
+00:07 HENGIST/BENCHMARKS  ▶  btsql3_mem_big_backup                             23,659 Hz ≙ 1 ÷ 1.1        88.3 % │███████████  │
+00:07 HENGIST/BENCHMARKS  ▶  btsql3_fle_big                                    15,792 Hz ≙ 1 ÷ 1.7        59.0 % │███████▍     │
+00:07 HENGIST/BENCHMARKS  ▶  btsql3_fle_small                                  15,570 Hz ≙ 1 ÷ 1.7        58.1 % │███████▎     │
+
+ ~/jzr/hengist  master !3  nodexh ~/jzr/hengist/dev/in-memory-sql/lib/copy-schemas.js                                                       ✔  8s
+00:00 IN-MEMORY-SQL  ▶  ------------------------------------------------------------------------------------------------------------
+00:01 IN-MEMORY-SQL  ▶  retrieving test data...
+00:01 IN-MEMORY-SQL  ▶  ...done
+btsql3_mem_big_backup                      3.453 s         100,000 items          28,961⏶Hz          34,529⏷nspc
+btsql3_fle_big                             5.991 s         100,000 items          16,692⏶Hz          59,909⏷nspc
+btsql3_mem_small_backup                    3.410 s         100,000 items          29,328⏶Hz          34,097⏷nspc
+btsql3_fle_small                           5.904 s         100,000 items          16,937⏶Hz          59,043⏷nspc
+00:20 IN-MEMORY-SQL  ▶  ------------------------------------------------------------------------------------------------------------
+btsql3_fle_big                             5.914 s         100,000 items          16,908⏶Hz          59,142⏷nspc
+btsql3_fle_small                           5.927 s         100,000 items          16,872⏶Hz          59,271⏷nspc
+btsql3_mem_big_backup                      3.495 s         100,000 items          28,611⏶Hz          34,952⏷nspc
+btsql3_mem_small_backup                    3.406 s         100,000 items          29,363⏶Hz          34,056⏷nspc
+00:39 IN-MEMORY-SQL  ▶  ------------------------------------------------------------------------------------------------------------
+btsql3_mem_big_backup                      3.482 s         100,000 items          28,717⏶Hz          34,823⏷nspc
+btsql3_fle_big                             5.905 s         100,000 items          16,935⏶Hz          59,050⏷nspc
+btsql3_mem_small_backup                    3.421 s         100,000 items          29,234⏶Hz          34,207⏷nspc
+btsql3_fle_small                           5.921 s         100,000 items          16,889⏶Hz          59,211⏷nspc
+00:58 HENGIST/BENCHMARKS  ▶  btsql3_mem_small_backup                           29,309 Hz ≙ 1 ÷ 1.0       100.0 % │████████████▌│
+00:58 HENGIST/BENCHMARKS  ▶  btsql3_mem_big_backup                             28,763 Hz ≙ 1 ÷ 1.0        98.1 % │████████████▎│
+00:58 HENGIST/BENCHMARKS  ▶  btsql3_fle_small                                  16,899 Hz ≙ 1 ÷ 1.7        57.7 % │███████▎     │
+00:58 HENGIST/BENCHMARKS  ▶  btsql3_fle_big                                    16,845 Hz ≙ 1 ÷ 1.7        57.5 % │███████▏     │
+
+*/
 
 }).call(this);
 
