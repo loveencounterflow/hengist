22e86df97d20befe0f5457c815ad0a55bcb19a56
flow <flow@enceladus.orbit>
2021-05-19 16:40:17 +0200

    `dev/snippets`: add demo for custom error classes

---
+++ dev/snippets/lib/custom-errors.js
@@ -0,0 +1,75 @@
+(function() {
+  'use strict';
+  var CND, X_error, X_snytax_error, badge, debug, demo, echo, help, info, isa, rpr, type_of, types, urge, validate, validate_list_of, warn, whisper;
+
+  //###########################################################################################################
+  CND = require('cnd');
+
+  rpr = CND.rpr;
+
+  badge = 'CUSTOM-ERRORS';
+
+  debug = CND.get_logger('debug', badge);
+
+  warn = CND.get_logger('warn', badge);
+
+  info = CND.get_logger('info', badge);
+
+  urge = CND.get_logger('urge', badge);
+
+  help = CND.get_logger('help', badge);
+
+  whisper = CND.get_logger('whisper', badge);
+
+  echo = CND.echo.bind(CND);
+
+  types = new (require('intertype')).Intertype();
+
+  ({isa, type_of, validate, validate_list_of} = types.export());
+
+  //-----------------------------------------------------------------------------------------------------------
+  X_error = class X_error extends Error {
+    constructor(ref, message) {
+      // super ( CND.grey ref ) + ' ' + ( CND.red CND.reverse message )
+      super(ref + ' ' + message);
+      this.ref = ref;
+      this._message = message;
+      this.type = this.constructor.name.toLowerCase();
+      return void 0/* always return `undefined` from constructor */;
+    }
+
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  X_snytax_error = class X_snytax_error extends X_error {};
+
+  //-----------------------------------------------------------------------------------------------------------
+  demo = function() {
+    var clasz, error, i, len, ref1;
+    ref1 = [X_error, X_snytax_error];
+    for (i = 0, len = ref1.length; i < len; i++) {
+      clasz = ref1[i];
+      try {
+        throw new clasz('^123^', `your message for ${clasz.name} here`);
+      } catch (error1) {
+        error = error1;
+        info();
+        info('^1345^', "message:      ", error.message);
+        info('^1345^', "_message:     ", error._message);
+        info('^1345^', "type:         ", error.type);
+        info('^1345^', "type_of:      ", type_of(error));
+      }
+    }
+    return null;
+  };
+
+  //###########################################################################################################
+  if (module === require.main) {
+    (async() => {
+      return (await demo());
+    })();
+  }
+
+}).call(this);
+
+//# sourceMappingURL=custom-errors.js.map
---
+++ dev/snippets/src/custom-errors.coffee
@@ -0,0 +1,56 @@
+
+'use strict'
+
+
+############################################################################################################
+CND                       = require 'cnd'
+rpr                       = CND.rpr
+badge                     = 'CUSTOM-ERRORS'
+debug                     = CND.get_logger 'debug',     badge
+warn                      = CND.get_logger 'warn',      badge
+info                      = CND.get_logger 'info',      badge
+urge                      = CND.get_logger 'urge',      badge
+help                      = CND.get_logger 'help',      badge
+whisper                   = CND.get_logger 'whisper',   badge
+echo                      = CND.echo.bind CND
+types                     = new ( require 'intertype' ).Intertype
+{ isa
+  type_of
+  validate
+  validate_list_of }      = types.export()
+
+
+
+#-----------------------------------------------------------------------------------------------------------
+class X_error extends Error
+  constructor: ( ref, message ) ->
+    # super ( CND.grey ref ) + ' ' + ( CND.red CND.reverse message )
+    super ref + ' ' + message
+    @ref      = ref
+    @_message = message
+    @type     = @constructor.name.toLowerCase()
+    return undefined ### always return `undefined` from constructor ###
+
+#-----------------------------------------------------------------------------------------------------------
+class X_snytax_error extends X_error
+
+#-----------------------------------------------------------------------------------------------------------
+demo = ->
+  for clasz in [ X_error, X_snytax_error, ]
+    try
+      throw new clasz '^123^', "your message for #{clasz.name} here"
+    catch error
+      info()
+      info '^1345^', "message:      ", error.message
+      info '^1345^', "_message:     ", error._message
+      info '^1345^', "type:         ", error.type
+      info '^1345^', "type_of:      ", type_of error
+  return null
+
+
+############################################################################################################
+if module is require.main then do =>
+  await demo()
+
+
+
