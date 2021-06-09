6561d0b74ab0595d5f5291b4e58026b67fd2d85f
flow <flow@enceladus.orbit>
2021-06-09 17:58:59 +0200

    update

--- dev/mojikura-idl/lib/idlx-list-tokens.js
+++ dev/mojikura-idl/lib/idlx-list-tokens.js
@@ -1,7 +1,8 @@
 (function() {
-  //###########################################################################################################
-  var CND, IDL, IDLX, TAP, alert, badge, debug, echo, equals, help, info, isa, log, rpr, type_of, urge, validate, warn, whisper;
+  'use strict';
+  var CND, IDL, IDLX, alert, badge, debug, echo, equals, help, info, isa, log, rpr, test, type_of, types, urge, validate, warn, whisper;
 
+  //###########################################################################################################
   CND = require('cnd');
 
   rpr = CND.rpr;
@@ -27,14 +28,16 @@
   echo = CND.echo.bind(CND);
 
   //...........................................................................................................
-  TAP = require('tap');
+  test = require('guy-test');
 
-  ({IDL, IDLX} = require('../..'));
+  ({IDL, IDLX} = require('../../../apps/mojikura-idl'));
 
-  ({isa, type_of, validate, equals} = require('../types'));
+  types = new (require('intertype')).Intertype();
+
+  ({isa, type_of, validate, equals} = types.export());
 
   //-----------------------------------------------------------------------------------------------------------
-  TAP.test("(IDLX) solitaires", function(T) {
+  this["(IDLX) solitaires"] = function(T, done) {
     var i, len, matcher, probe, probes_and_matchers, result;
     probes_and_matchers = [['↻', 'unary_operator'], ['〓', 'proxy'], ['§', 'proxy'], ['⿰', 'binary_operator'], ['⿻', 'binary_operator'], ['◰', 'binary_operator'], ['(', 'bracket'], ['x', 'component']];
 //.........................................................................................................
@@ -45,12 +48,12 @@
       T.ok(equals(result, matcher));
     }
     //.........................................................................................................
-    T.end();
+    done();
     return null;
-  });
+  };
 
   //-----------------------------------------------------------------------------------------------------------
-  TAP.test("(IDLX) get_formula", function(T) {
+  this["(IDLX) get_formula"] = function(T, done) {
     var diagram, i, len, matcher, probe, probes_and_matchers, result;
     probes_and_matchers = [['⿱⿰天天⿰天天', '⿱⿰天天⿰天天'], ['⿰(⿱一八土)⿱山电', '⿰(⿱一八土)⿱山电'], ['(⿱⿶凵⿰⿱丄一⿱丄一开土)', '(⿱⿶凵⿰⿱丄一⿱丄一开土)'], ['⿰⿱名土⿱勿中', '⿰⿱名土⿱勿中'], ['⿰⿱日有⿱犬土', '⿰⿱日有⿱犬土'], ['⿰⿱土坐⿱土坐', '⿰⿱土坐⿱土坐'], ['⿰土(⿱⿰一一⿰日日鹿)', '⿰土(⿱⿰一一⿰日日鹿)'], ['⿰土⿱⿰⿱一日⿱一日鹿', '⿰土⿱⿰⿱一日⿱一日鹿'], ['⿰土⿱⿰𣄼𣄼鹿', '⿰土⿱⿰𣄼𣄼鹿'], ['⿱𠀎冉', '⿱𠀎冉']];
 //.........................................................................................................
@@ -62,41 +65,47 @@
       T.ok(equals(result, matcher));
     }
     //.........................................................................................................
-    T.end();
+    done();
     return null;
-  });
+  };
 
   /*
-result = IDLX.type_from_literal probe
-urge ( CND.truth equals result, matcher ), JSON.stringify [ probe, result, ]
-T.ok equals result, matcher
-info @IDLX._get_literals_and_types IDLX_GRAMMAR
-info @IDLX.type_from_literal IDLX_GRAMMAR
-help '↻', @IDLX.type_from_literal '↻' # 'operator',
-help '〓', @IDLX.type_from_literal '〓' # 'proxy',
-help '§', @IDLX.type_from_literal '§' # 'proxy',
-help '⿰', @IDLX.type_from_literal '⿰' # 'operator',
-help '⿻', @IDLX.type_from_literal '⿻' # 'operator',
-help '◰', @IDLX.type_from_literal '◰' # 'operator',
-help '(', @IDLX.type_from_literal '(' # 'bracket',
-help 'x', @IDLX.type_from_literal 'x' # 'other',
-formula       = '⿹弓(⿰(⿱人人丨)(⿱人人丨)(⿱人人丨))'
-whisper formula
-help diagram  = @IDLX.parse formula
-whisper formula
-help tokens   = @IDLX.list_tokens diagram
-urge @IDLX.get_formula formula
-urge @IDLX.get_formula diagram
-urge @IDLX._get_treeshaker_litmus()
-urge ( CND.yellow formula    ), ( CND.blue CND.truth @IDLX.formula_may_be_nonminimal formula    )
-urge ( CND.yellow '⿱⿱𫝀口㐄'    ), ( CND.blue CND.truth @IDLX.formula_may_be_nonminimal '⿱⿱𫝀口㐄'    )
-urge ( CND.yellow '⿱𫝀⿱口㐄'    ), ( CND.blue CND.truth @IDLX.formula_may_be_nonminimal '⿱𫝀⿱口㐄'    )
-urge ( CND.yellow '⿰韋(⿱白大十)' ), ( CND.blue CND.truth @IDLX.formula_may_be_nonminimal '⿰韋(⿱白大十)' )
-info ( CND.yellow formula    ), ( CND.blue @IDLX.normalize_formula formula                       )
-info ( CND.yellow '⿱⿱𫝀口㐄'    ), ( CND.blue @IDLX.normalize_formula '⿱⿱𫝀口㐄'                       )
-info ( CND.yellow '⿱𫝀⿱口㐄'    ), ( CND.blue @IDLX.normalize_formula '⿱𫝀⿱口㐄'                       )
-info ( CND.yellow '⿰韋(⿱白大十)' ), ( CND.blue @IDLX.normalize_formula '⿰韋(⿱白大十)'                    )
-*/
+  result = IDLX.type_from_literal probe
+  urge ( CND.truth equals result, matcher ), JSON.stringify [ probe, result, ]
+  T.ok equals result, matcher
+  info @IDLX._get_literals_and_types IDLX_GRAMMAR
+  info @IDLX.type_from_literal IDLX_GRAMMAR
+  help '↻', @IDLX.type_from_literal '↻' # 'operator',
+  help '〓', @IDLX.type_from_literal '〓' # 'proxy',
+  help '§', @IDLX.type_from_literal '§' # 'proxy',
+  help '⿰', @IDLX.type_from_literal '⿰' # 'operator',
+  help '⿻', @IDLX.type_from_literal '⿻' # 'operator',
+  help '◰', @IDLX.type_from_literal '◰' # 'operator',
+  help '(', @IDLX.type_from_literal '(' # 'bracket',
+  help 'x', @IDLX.type_from_literal 'x' # 'other',
+  formula       = '⿹弓(⿰(⿱人人丨)(⿱人人丨)(⿱人人丨))'
+  whisper formula
+  help diagram  = @IDLX.parse formula
+  whisper formula
+  help tokens   = @IDLX.list_tokens diagram
+  urge @IDLX.get_formula formula
+  urge @IDLX.get_formula diagram
+  urge @IDLX._get_treeshaker_litmus()
+  urge ( CND.yellow formula    ), ( CND.blue CND.truth @IDLX.formula_may_be_nonminimal formula    )
+  urge ( CND.yellow '⿱⿱𫝀口㐄'    ), ( CND.blue CND.truth @IDLX.formula_may_be_nonminimal '⿱⿱𫝀口㐄'    )
+  urge ( CND.yellow '⿱𫝀⿱口㐄'    ), ( CND.blue CND.truth @IDLX.formula_may_be_nonminimal '⿱𫝀⿱口㐄'    )
+  urge ( CND.yellow '⿰韋(⿱白大十)' ), ( CND.blue CND.truth @IDLX.formula_may_be_nonminimal '⿰韋(⿱白大十)' )
+  info ( CND.yellow formula    ), ( CND.blue @IDLX.normalize_formula formula                       )
+  info ( CND.yellow '⿱⿱𫝀口㐄'    ), ( CND.blue @IDLX.normalize_formula '⿱⿱𫝀口㐄'                       )
+  info ( CND.yellow '⿱𫝀⿱口㐄'    ), ( CND.blue @IDLX.normalize_formula '⿱𫝀⿱口㐄'                       )
+  info ( CND.yellow '⿰韋(⿱白大十)' ), ( CND.blue @IDLX.normalize_formula '⿰韋(⿱白大十)'                    )
+  */
+  //###########################################################################################################
+  if (module === require.main) {
+    (() => {
+      return test(this);
+    })();
+  }
 
 }).call(this);
 
