8c53f27cf24fc2e2e27c965e9a2776091084fff9
flow <flow@enceladus.orbit>
2021-08-11 22:47:31 +0200

    update

--- dev/dpan/lib/demos.js
+++ dev/dpan/lib/demos.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  var CND, Dba, Dpan, Dpan_next, FS, PATH, SQL, badge, debug, def, demo_db_add_pkg_info, demo_fs_walk_dep_infos, echo, freeze, glob, got, help, info, isa, lets, rpr, semver_cmp, semver_satisfies, type_of, types, urge, validate, validate_list_of, warn, whisper;
+  var CND, Dba, Dpan, Dpan_next, FS, PATH, SQL, badge, debug, def, demo_db_add_pkg_info, demo_db_add_pkg_infos, demo_fs_walk_dep_infos, echo, freeze, glob, got, help, info, isa, lets, rpr, semver_cmp, semver_satisfies, type_of, types, urge, validate, validate_list_of, warn, whisper;
 
   //###########################################################################################################
   CND = require('cnd');
@@ -56,22 +56,32 @@
 
   //-----------------------------------------------------------------------------------------------------------
   demo_db_add_pkg_info = async function() {
-    var dpan, k, pkg_fspath, pkg_info/* TAINT not strictly true */, pkg_name;
+    var dpan, pkg_fspath, pkg_info/* TAINT not strictly true */, pkg_name;
     dpan = new Dpan_next();
     pkg_fspath = '../../../';
     pkg_fspath = PATH.resolve(PATH.join(__dirname, pkg_fspath));
     pkg_name = PATH.basename(pkg_fspath);
     pkg_info = (await dpan.fs_fetch_pkg_info({pkg_fspath}));
-    debug('^476^', (function() {
-      var results;
-      results = [];
-      for (k in pkg_info) {
-        results.push(k);
-      }
-      return results;
-    })());
-    // debug '^476^', pkg_info
-    dpan.db_add_pkg_info({pkg_info});
+    dpan.db_add_pkg_info(pkg_info);
+    return null;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  demo_db_add_pkg_infos = async function() {
+    var dpan, home_path, i, len, pkg_fspath, pkg_info, project_path, project_path_pattern, ref;
+    dpan = new Dpan_next();
+    home_path = PATH.resolve(PATH.join(__dirname, '../../../../'));
+    project_path_pattern = PATH.join(home_path, '*/package.json');
+    debug('^488^', project_path_pattern);
+    ref = glob.sync(project_path_pattern);
+    for (i = 0, len = ref.length; i < len; i++) {
+      project_path = ref[i];
+      pkg_fspath = PATH.dirname(project_path);
+      pkg_info = (await dpan.fs_fetch_pkg_info({pkg_fspath}));
+      // whisper '^564^', pkg_info
+      info('^564^', pkg_info.pkg_name, pkg_info.pkg_version);
+      dpan.db_add_pkg_info(pkg_info);
+    }
     return null;
   };
 
@@ -103,7 +113,8 @@
     (async() => {
       // await demo_fs_walk_dep_infos()
       // await demo_db_add_package()
-      return (await demo_db_add_pkg_info());
+      // await demo_db_add_pkg_info()
+      return (await demo_db_add_pkg_infos());
     })();
   }
 
