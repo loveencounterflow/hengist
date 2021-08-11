9dd9d5942a986250181b490e5194e1a278abcd77
flow <flow@enceladus.orbit>
2021-08-11 16:31:39 +0200

    update

--- dev/dpan/lib/_old.js
+++ dev/dpan/lib/_old.js
@@ -1,5 +1,5 @@
 (function() {
-  var X, demo, demo_custom_require, f, register_package;
+  var X, demo, f, register_package;
 
   f = function() {
     return {
@@ -267,58 +267,6 @@ on conflict do nothing;`, {pkg_vname, dep_vname});
     return null;
   };
 
-  //-----------------------------------------------------------------------------------------------------------
-  demo_custom_require = async function() {
-    var RPKGUP, createRequire, dep_description, dep_fspath, dep_json, dep_json_fspath, dep_json_info, dep_keywords, dep_version, i, k, len, path, pkg_name, pkg_names_and_svranges, ref, rq, svrange;
-    RPKGUP = (await import('read-pkg-up'));
-    debug((function() {
-      var results;
-      results = [];
-      for (k in RPKGUP) {
-        results.push(k);
-      }
-      return results;
-    })());
-    pkg_names_and_svranges = [['@ef-carbon/deep-freeze', '^1.0.1'], ['@scotttrinh/number-ranges', '^2.1.0'], ['argparse', '^2.0.1'], ['better-sqlite3', '7.4.0'], ['chance', '^1.1.7'], ['cnd', '^9.2.1']];
-    path = '../../../lib/main.js';
-    path = PATH.resolve(PATH.join(__dirname, path));
-    ({createRequire} = require('module'));
-    rq = createRequire(path);
-    for (i = 0, len = pkg_names_and_svranges.length; i < len; i++) {
-      [pkg_name, svrange] = pkg_names_and_svranges[i];
-      dep_fspath = rq.resolve(pkg_name);
-      dep_json_info = RPKGUP.readPackageUpSync({
-        cwd: dep_fspath,
-        normalize: true
-      });
-      dep_json = dep_json_info.packageJson;
-      dep_version = dep_json.version;
-      dep_description = dep_json.description;
-      dep_keywords = (ref = dep_json.keywords) != null ? ref : [];
-      dep_json_fspath = dep_json_info.path;
-      info();
-      info(CND.yellow(pkg_name));
-      info(CND.blue(dep_fspath));
-      info(CND.gold(dep_keywords));
-      // info ( CND.lime dep_pkgj_fspath )
-      info(dep_version);
-      info(dep_description);
-    }
-    // info ( CND.lime FS.realpathSync dep_fspath )
-    return null;
-  };
-
-  //###########################################################################################################
-  if (module === require.main) {
-    (async() => {
-      // await demo()
-      return (await demo_custom_require());
-    })();
-  }
-
-  // CP = require 'child_process'
-  // debug '^33442^', CP.execSync "npm view icql-dba@^6 dependencies", { encoding: 'utf-8', }
-  // debug '^33442^', CP.execSync "npm view icql-dba dependencies", { encoding: 'utf-8', }
   f = async function() {
     var dpan, k, package_d, package_info, pkg_name;
     dpan = new Dpan();
--- dev/dpan/lib/main.js
+++ dev/dpan/lib/main.js
@@ -1,187 +1,67 @@
 (function() {
-  'use strict';
-  var CND, Dba, Dpan, FS, PATH, SQL, badge, debug, def, echo, freeze, glob, got, help, info, isa, lets, rpr, semver_cmp, semver_satisfies, type_of, types, urge, validate, validate_list_of, warn, whisper;
-
   //###########################################################################################################
+  var CND, FS, L, PATH, alert, badge, debug, echo, help, info, log, rpr, test, urge, warn, whisper;
+
   CND = require('cnd');
 
   rpr = CND.rpr;
 
-  badge = 'DEPENDENCY-ANALYZER';
+  badge = 'DPAN/TESTS/MAIN';
+
+  log = CND.get_logger('plain', badge);
+
+  info = CND.get_logger('info', badge);
+
+  whisper = CND.get_logger('whisper', badge);
+
+  alert = CND.get_logger('alert', badge);
 
   debug = CND.get_logger('debug', badge);
 
   warn = CND.get_logger('warn', badge);
 
-  info = CND.get_logger('info', badge);
-
-  urge = CND.get_logger('urge', badge);
-
   help = CND.get_logger('help', badge);
 
-  whisper = CND.get_logger('whisper', badge);
+  urge = CND.get_logger('urge', badge);
 
   echo = CND.echo.bind(CND);
 
   //...........................................................................................................
-  types = new (require('intertype')).Intertype();
-
-  ({isa, type_of, validate, validate_list_of} = types.export());
-
-  // { to_width }              = require 'to-width'
-  SQL = String.raw;
-
-  ({lets, freeze} = require('letsfreezethat'));
-
-  ({Dba} = require('icql-dba'));
-
-  def = Object.defineProperty;
-
-  glob = require('glob');
-
   PATH = require('path');
 
   FS = require('fs');
 
-  got = require('got');
+  test = require('../../../apps/guy-test');
 
-  semver_satisfies = require('semver/functions/satisfies');
+  //###########################################################################################################
+  L = this;
 
-  semver_cmp = require('semver/functions/cmp');
-
-  //===========================================================================================================
-  types.declare('dpan_constructor_cfg', {
-    tests: {
-      '@isa.object x': function(x) {
-        return this.isa.object(x);
-      },
-      'x.prefix is a prefix': function(x) {
-        if (!this.isa.text(x.prefix)) {
-          return false;
+  (function() {
+    var i, key, len, module, path, paths, value;
+    paths = FS.readdirSync(__dirname);
+    for (i = 0, len = paths.length; i < len; i++) {
+      path = paths[i];
+      if (!path.endsWith('.tests.js')) {
+        continue;
+      }
+      path = PATH.join(__dirname, path);
+      module = require(path);
+      for (key in module) {
+        value = module[key];
+        if (key.startsWith('_')) {
+          continue;
         }
-        if (x.prefix === '') {
-          return true;
+        if (L[key] != null) {
+          // debug '39838', path, key
+          throw new Error(`duplicate key ${rpr(key)}`);
         }
-        return /^[_a-z][_a-z0-9]*$/.test(x.prefix);
-      },
-      '@isa.boolean x.recreate': function(x) {
-        return this.isa.boolean(x.recreate);
+        L[key] = value.bind(L);
       }
     }
-  });
-
-  //-----------------------------------------------------------------------------------------------------------
-  types.defaults = {
-    dpan_constructor_cfg: {
-      dba: null,
-      prefix: 'dpan_',
-      db_path: PATH.resolve(PATH.join(__dirname, '../dpan.sqlite')),
-      recreate: false,
-      registry_url: 'https://registry.npmjs.org/'
-    }
-  };
-
-  //===========================================================================================================
-  Dpan = class Dpan {
-    //---------------------------------------------------------------------------------------------------------
-    constructor(cfg) {
-      var dba;
-      validate.dpan_constructor_cfg(this.cfg = {...types.defaults.dpan_constructor_cfg, ...cfg});
-      debug('^4877^', this.cfg);
-      //.......................................................................................................
-      dba = this.cfg.dba != null ? this.cfg.dba : new Dba();
-      def(this, 'dba', {
-        enumerable: false,
-        value: dba
-      });
-      delete this.cfg.dba;
-      this.cfg = freeze(this.cfg);
-      //.......................................................................................................
-      if (this.cfg.db_path != null) {
-        this.dba.open({
-          path: this.cfg.db_path
-        });
-      }
-      if (this.cfg.recreate) {
-        //.......................................................................................................
-        this._clear_db();
-      }
-      this._create_db_structure();
-      this._compile_sql();
-      this._create_sql_functions();
-      return void 0;
-    }
-
-    //---------------------------------------------------------------------------------------------------------
-    _create_db_structure() {
-      /* TAINT unify name / pkg_name, version / pkg_version */
-      var prefix;
-      prefix = this.cfg.prefix;
-      this.dba.execute(SQL`create table if not exists ${prefix}pkgs (
-    pkg_name          text    not null references ${prefix}pkg_names    ( pkg_name    ),
-    pkg_version       text    not null references ${prefix}pkg_versions ( pkg_version ),
-    pkg_vname         text    generated always as ( pkg_name || '@' || pkg_version ) virtual not null unique,
-    description       text,
-    url               text,
-    fspath            text,
-  primary key ( pkg_name, pkg_version ) );
-create unique index if not exists ${prefix}pkgs_vname_idx on ${prefix}pkgs ( pkg_vname );
-create table if not exists ${prefix}deps (
-    pkg_vname         text    not null references ${prefix}pkgs ( pkg_vname ),
-    dep_vname         text    not null references ${prefix}pkgs ( pkg_vname ),
-  primary key ( pkg_vname, dep_vname ) );
-create table if not exists ${prefix}pkg_names (
-    pkg_name          text not null primary key );
-create table if not exists ${prefix}pkg_versions (
-    pkg_version       text not null primary key );`);
-      return null;
-    }
-
-    //---------------------------------------------------------------------------------------------------------
-    _clear_db() {
-      /* TAINT should be a method of ICQL/DB */
-      var prefix;
-      prefix = this.cfg.prefix;
-      this.dba.execute(SQL`drop index if exists ${prefix}pkgs_vname_idx;
-drop table if exists ${prefix}deps;
-drop table if exists ${prefix}pkgs;
-drop table if exists ${prefix}pkg_names;
-drop table if exists ${prefix}pkg_versions;`);
-      return null;
-    }
-
-    //---------------------------------------------------------------------------------------------------------
-    _compile_sql() {
-      var prefix;
-      prefix = this.cfg.prefix;
-      this.sql = {
-        add_pkg_version: SQL`insert into ${prefix}pkg_versions ( pkg_version )
-  values ( $pkg_version )
-  on conflict do nothing;`,
-        add_pkg_name: SQL`insert into ${prefix}pkg_names ( pkg_name )
-  values ( $pkg_name )
-  on conflict do nothing;`,
-        add_pkg: SQL`insert into ${prefix}pkgs ( pkg_name, pkg_version, description, url, fspath )
-  values ( $pkg_name, $pkg_version, $description, $url, $fspath )
-  on conflict do nothing;`
-      };
-      return null;
-    }
-
-    //---------------------------------------------------------------------------------------------------------
-    _create_sql_functions() {
-      return this.dba.create_function({
-        name: 'semver_satisfies',
-        call: (version, pattern) => {
-          if (semver_satisfies(version, pattern)) {
-            return 1;
-          }
-          return 0;
-        }
-      });
-    }
-
-  };
+    return test(L, {
+      timeout: 5000
+    });
+  })();
 
 }).call(this);
 
