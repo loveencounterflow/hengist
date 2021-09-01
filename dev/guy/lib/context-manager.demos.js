3b8930da2fded5351ab194e486738d4fbf46653c
flow <flow@enceladus.orbit>
2021-09-01 12:37:32 +0200

    update

--- dev/guy/lib/context-manager.demos.js
+++ dev/guy/lib/context-manager.demos.js
@@ -1,7 +1,6 @@
 (function() {
   //###########################################################################################################
-  var CND, Context_manager_1, Context_manager_2, FS, H, PATH, alert, badge, debug, demo_1, echo, help, info, isa, log, rpr, test, type_of, types, urge, validate, validate_list_of, warn, whisper,
-    boundMethodCheck = function(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new Error('Bound instance method accessed before binding'); } };
+  var CND, Context_manager, FS, H, PATH, alert, badge, debug, demo_1, echo, help, info, isa, log, rpr, test, type_of, types, urge, validate, validate_list_of, warn, whisper;
 
   PATH = require('path');
 
@@ -44,56 +43,41 @@
   //===========================================================================================================
   // CLASS DEFINITION
   //-----------------------------------------------------------------------------------------------------------
-  Context_manager_1 = class Context_manager_1 extends Function {
-    //---------------------------------------------------------------------------------------------------------
-    constructor() {
-      super();
-      //---------------------------------------------------------------------------------------------------------
-      this.mymethod = this.mymethod.bind(this);
-      Object.setPrototypeOf(this.mymethod, Context_manager_1.prototype);
-      // Object.setPrototypeOf @mymethod, @constructor.prototype
-      this.id = 1234;
-      return this.mymethod;
-    }
-
-    mymethod(...P) {
-      var k;
-      boundMethodCheck(this, Context_manager_1);
-      // debug '^4554^', type_of @
-      debug('^4554^', (function() {
-        var results;
-        results = [];
-        for (k in this) {
-          results.push(k);
-        }
-        return results;
-      }).call(this));
-      debug('^4554^', this.id);
-      return debug('^4554^', this.frobulate);
-    }
-
-    frobulate(n) {
-      return n ** 2;
-    }
-
-  };
-
-  //-----------------------------------------------------------------------------------------------------------
-  Context_manager_2 = class Context_manager_2 extends Function {
+  Context_manager = class Context_manager extends Function {
     //---------------------------------------------------------------------------------------------------------
     constructor(f) {
+      var manager;
       super();
-      this.f = f.bind(this);
-      // Object.setPrototypeOf f, Context_manager_2.prototype
-      // Object.setPrototypeOf @f, @constructor.prototype
+      f = f.bind(this);
+      manager = (...P) => {
+        var R;
+        this.enter(...P);
+        try {
+          R = f(...P);
+        } finally {
+          this.exit(...P);
+        }
+        return R;
+      };
       this.id = 4567;
-      // f.__call__ = @f
-      // return @f
-      return void 0;
+      return manager;
     }
 
+    //---------------------------------------------------------------------------------------------------------
     frobulate(n) {
       return n ** 2;
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    enter(...P) {
+      debug('^701^', "enter()", P);
+      return 1;
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    exit(...P) {
+      debug('^701^', "exit()", P);
+      return 1;
     }
 
   };
@@ -102,20 +86,13 @@
   // DEMOS
   //-----------------------------------------------------------------------------------------------------------
   demo_1 = function() {
-    (() => {
-      var cm_1;
-      cm_1 = new Context_manager_1();
-      cm_1();
-      whisper('------------------');
-      return cm_1.mymethod();
-    })();
-    whisper('=================');
     return (() => {
       var cm_2;
-      cm_2 = new Context_manager_2(function() {
-        var k;
-        info('^4554^', this.id);
-        info('^4554^', (function() {
+      cm_2 = new Context_manager(function(...P) {
+        var k, p;
+        info('^4554^', 'kernel', P);
+        whisper('^4554^', this.id);
+        whisper('^4554^', (function() {
           var results;
           results = [];
           for (k in this) {
@@ -123,17 +100,24 @@
           }
           return results;
         }).call(this));
-        info('^4554^', this.frobulate);
-        return 'othermethod';
+        whisper('^4554^', this.frobulate);
+        whisper('^4554^', this.frobulate(42));
+        return ((function() {
+          var i, len, results;
+          results = [];
+          for (i = 0, len = P.length; i < len; i++) {
+            p = P[i];
+            results.push(rpr(p));
+          }
+          return results;
+        })()).join('|');
       });
-      cm_2();
-      whisper('------------------');
-      urge(typeof cm_2.f === "function" ? cm_2.f() : void 0);
-      // urge cm_2.__call__?()
-      return urge(typeof cm_2.frobulate === "function" ? cm_2.frobulate(3) : void 0);
+      return urge(cm_2('a', 'b', 'c'));
     })();
   };
 
+  // whisper '------------------'
+  // urge cm_2.f?()
   // urge cm_2.frobulate? 3
 
   //###########################################################################################################
