e11dc48fb2e8a73faed4f2b7804baffb8dfbe71b
flow <flow@enceladus.orbit>
2021-09-21 12:56:29 +0200

    update

--- dev/dbay/lib/subselects-with-udfs.demos.js
+++ dev/dbay/lib/subselects-with-udfs.demos.js
@@ -63,6 +63,7 @@
 
   guy = require('../../../apps/guy');
 
+  //-----------------------------------------------------------------------------------------------------------
   cfg = {
     // verbose:    true
     verbose: false,
@@ -74,11 +75,14 @@
       sf: [null], // [ true, false, ]                         ### sf                    ###
       ft: [null], // [ 'none', 'scalar', 'table', 'sqlite', ] ### function_type         ###
       un: [true, false]
+    },
+    /* use_nested_statement  */results: {
+      not_applicable: Symbol('not_applicable')
     }
   };
 
   //-----------------------------------------------------------------------------------------------------------
-  /* use_nested_statement  */  prepare_db = function(db) {
+  prepare_db = function(db) {
     var i, idx, j, len, len1, n, nrx, nry, ref, ref1, word;
     db.sqlt1.exec(SQL`create table x ( word text, nrx );`);
     db.sqlt1.exec(SQL`create table y ( word text, nry );`);
@@ -214,6 +218,24 @@
     }
     try {
       //.........................................................................................................
+      if (ut) {
+        if (!un) {
+          if (ut) {
+            return {
+              result: cfg.results.not_applicable,
+              error: "(need nested stms for tx:1)"
+            };
+          }
+        }
+        if (!sc) {
+          if (ut) {
+            return {
+              result: cfg.results.not_applicable,
+              error: "(need single conn for tx:1)"
+            };
+          }
+        }
+      }
       _begin_transaction(ut, sqlt_a, sqlt_b);
       //.......................................................................................................
       if (un/* use_nested_statement */) {
@@ -283,6 +305,11 @@
                   fingerprint = {uu, sc, ut, uw, sf, ft, un};
                   kenning = get_kenning(fingerprint);
                   ({result, error} = ff(db, count, fingerprint));
+                  switch (result) {
+                    case cfg.results.not_applicable:
+                      whisper('^450^', count, kenning, "N/A", error);
+                      continue;
+                  }
                   is_ok = equals(result, matcher);
                   info('^450^', CND.blue(count, kenning), CND.truth(is_ok), CND.red(error != null ? error : ''));
                   if (!is_ok) {
