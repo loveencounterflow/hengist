c016f7e3bbeb9bc8a5e6df3f557d67ca96fe085b
flow <flow@enceladus.orbit>
2021-10-05 20:07:53 +0200

    `dev/guy`: add benchmarks, patched `n-readlines` (https://raw.githubusercontent.com/nacholibre/node-readlines/master/readlines.js)

---
+++ dev/guy/lib/readlines.benchmarks.js
@@ -0,0 +1,197 @@
+(function() {
+  'use strict';
+  var BM, CND, DATA, FS, PATH, alert, badge, data_cache, debug, echo, freeze, gcfg, help, info, jr, log, rpr, test, urge, warn, whisper;
+
+  //###########################################################################################################
+  CND = require('cnd');
+
+  rpr = CND.rpr;
+
+  badge = 'IN-MEMORY-SQL';
+
+  log = CND.get_logger('plain', badge);
+
+  info = CND.get_logger('info', badge);
+
+  whisper = CND.get_logger('whisper', badge);
+
+  alert = CND.get_logger('alert', badge);
+
+  debug = CND.get_logger('debug', badge);
+
+  warn = CND.get_logger('warn', badge);
+
+  help = CND.get_logger('help', badge);
+
+  urge = CND.get_logger('urge', badge);
+
+  echo = CND.echo.bind(CND);
+
+  //...........................................................................................................
+  PATH = require('path');
+
+  FS = require('fs');
+
+  DATA = require('../../../lib/data-providers-nocache');
+
+  test = require('guy-test');
+
+  ({jr} = CND);
+
+  BM = require('../../../lib/benchmarks');
+
+  data_cache = null;
+
+  gcfg = {
+    verbose: false
+  };
+
+  ({freeze} = require('letsfreezethat'));
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.intertext_splitlines = function(cfg) {
+    return new Promise((resolve) => {
+      var SL;
+      SL = require('intertext-splitlines');
+      //.........................................................................................................
+      resolve(() => {
+        return new Promise((resolve) => {
+          var buffer, bytes_read, count, ctx, fd, line, path, ref, ref1;
+          count = 0;
+          ctx = SL.new_context({
+            splitter: '\n',
+            decode: true,
+            skip_empty_last: true,
+            keep_newlines: true
+          });
+          path = cfg.paths[cfg.size];
+          fd = FS.openSync(path);
+          while (true) {
+            buffer = Buffer.alloc(cfg.chunk_size);
+            bytes_read = FS.readSync(fd, buffer);
+            if (bytes_read === 0) {
+              break;
+            }
+            if (bytes_read < cfg.chunk_size) {
+              buffer = buffer.slice(0, bytes_read);
+            }
+            ref = SL.walk_lines(ctx, buffer);
+            for (line of ref) {
+              count++;
+              if (cfg.show) {
+                debug('^888-1^', {count, bytes_read, line});
+              }
+            }
+          }
+          ref1 = SL.flush(ctx);
+          for (line of ref1) {
+            count++;
+            if (cfg.show) {
+              debug('^888-2^', {count, line});
+            }
+          }
+          return resolve(count);
+        });
+      });
+      return null;
+    });
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this._n_readlines = function(cfg) {
+    return new Promise((resolve) => {
+      var Readlines;
+      if (cfg.use_patched) {
+        Readlines = require('n-readlines');
+      } else {
+        Readlines = require('../n-readlines-patched');
+      }
+      //.........................................................................................................
+      resolve(() => {
+        return new Promise((resolve) => {
+          var count, line, path, readlines, readlines_cfg;
+          count = 0;
+          path = cfg.paths[cfg.size];
+          readlines_cfg = {
+            readChunk: cfg.chunk_size,
+            newLineCharacter: '\n' // nl
+          };
+          readlines = new Readlines(path, readlines_cfg);
+          while ((line = readlines.next()) !== false) {
+            count++;
+            line = line.toString('utf-8');
+            if (cfg.show) {
+              debug('^888-1^', {count, line});
+            }
+          }
+          return resolve(count);
+        });
+      });
+      return null;
+    });
+  };
+
+  this.n_readlines = function(cfg) {
+    return this._n_readlines({
+      ...cfg,
+      use_patched: false
+    });
+  };
+
+  this.n_readlines_patched = function(cfg) {
+    return this._n_readlines({
+      ...cfg,
+      use_patched: true
+    });
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.run_benchmarks = async function() {
+    var _, bench, cfg, i, j, len, ref, ref1, repetitions, test_name, test_names;
+    gcfg.verbose = true;
+    gcfg.verbose = false;
+    bench = BM.new_benchmarks();
+    cfg = {
+      // size:         'small'
+      size: 'big1',
+      // chunk_size:   1 * 1024
+      chunk_size: 4 * 1024,
+      // chunk_size:   8 * 1024
+      // chunk_size:   16 * 1024
+      // chunk_size:   64 * 1024
+      paths: {
+        small: PATH.resolve(PATH.join(__filename)),
+        big1: '/usr/share/dict/american-english'
+      }
+    };
+    cfg.show = cfg.size === 'small';
+    repetitions = 5;
+    test_names = ['n_readlines', 'n_readlines_patched', 'intertext_splitlines'];
+    if (global.gc != null) {
+      global.gc();
+    }
+    data_cache = null;
+    for (_ = i = 1, ref = repetitions; (1 <= ref ? i <= ref : i >= ref); _ = 1 <= ref ? ++i : --i) {
+      whisper('-'.repeat(108));
+      ref1 = CND.shuffle(test_names);
+      for (j = 0, len = ref1.length; j < len; j++) {
+        test_name = ref1[j];
+        if (global.gc != null) {
+          global.gc();
+        }
+        await BM.benchmark(bench, cfg, false, this, test_name);
+      }
+    }
+    return BM.show_totals(bench);
+  };
+
+  //###########################################################################################################
+  if (require.main === module) {
+    (async() => {
+      return (await this.run_benchmarks());
+    })();
+  }
+
+}).call(this);
+
+//# sourceMappingURL=readlines.benchmarks.js.map
---
+++ dev/guy/src/readlines.benchmarks.coffee
@@ -0,0 +1,114 @@
+
+
+'use strict'
+
+############################################################################################################
+CND                       = require 'cnd'
+rpr                       = CND.rpr
+badge                     = 'IN-MEMORY-SQL'
+log                       = CND.get_logger 'plain',     badge
+info                      = CND.get_logger 'info',      badge
+whisper                   = CND.get_logger 'whisper',   badge
+alert                     = CND.get_logger 'alert',     badge
+debug                     = CND.get_logger 'debug',     badge
+warn                      = CND.get_logger 'warn',      badge
+help                      = CND.get_logger 'help',      badge
+urge                      = CND.get_logger 'urge',      badge
+echo                      = CND.echo.bind CND
+#...........................................................................................................
+PATH                      = require 'path'
+FS                        = require 'fs'
+DATA                      = require '../../../lib/data-providers-nocache'
+test                      = require 'guy-test'
+{ jr }                    = CND
+BM                        = require '../../../lib/benchmarks'
+data_cache                = null
+gcfg                      = { verbose: false, }
+{ freeze }                = require 'letsfreezethat'
+
+
+#-----------------------------------------------------------------------------------------------------------
+@intertext_splitlines = ( cfg ) -> new Promise ( resolve ) =>
+  SL = require 'intertext-splitlines'
+  #.........................................................................................................
+  resolve => new Promise ( resolve ) =>
+    count = 0
+    ctx   = SL.new_context
+      splitter:         '\n'
+      decode:           true
+      skip_empty_last:  true
+      keep_newlines:    true
+    path        = cfg.paths[ cfg.size ]
+    fd          = FS.openSync path
+    loop
+      buffer     = Buffer.alloc cfg.chunk_size
+      bytes_read = FS.readSync fd, buffer
+      break if bytes_read is 0
+      buffer = buffer.slice 0, bytes_read if bytes_read < cfg.chunk_size
+      for line from SL.walk_lines ctx, buffer
+        count++
+        debug '^888-1^', { count, bytes_read, line, } if cfg.show
+    for line from SL.flush ctx
+      count++
+      debug '^888-2^', { count, line, } if cfg.show
+    resolve count
+  return null
+
+#-----------------------------------------------------------------------------------------------------------
+@_n_readlines = ( cfg ) -> new Promise ( resolve ) =>
+  if cfg.use_patched then Readlines = require 'n-readlines'
+  else                    Readlines = require '../n-readlines-patched'
+  #.........................................................................................................
+  resolve => new Promise ( resolve ) =>
+    count         = 0
+    path          = cfg.paths[ cfg.size ]
+    readlines_cfg =
+      readChunk:          cfg.chunk_size
+      newLineCharacter:   '\n'      # nl
+    readlines = new Readlines path, readlines_cfg
+    while ( line = readlines.next() ) isnt false
+      count++
+      line = line.toString 'utf-8'
+      debug '^888-1^', { count, line, } if cfg.show
+    resolve count
+  return null
+
+@n_readlines          = ( cfg ) -> @_n_readlines { cfg..., use_patched: false, }
+@n_readlines_patched  = ( cfg ) -> @_n_readlines { cfg..., use_patched: true, }
+
+#-----------------------------------------------------------------------------------------------------------
+@run_benchmarks = ->
+  gcfg.verbose  = true
+  gcfg.verbose  = false
+  bench         = BM.new_benchmarks()
+  cfg           =
+    # size:         'small'
+    size:         'big1'
+    # chunk_size:   1 * 1024
+    chunk_size:   4 * 1024
+    # chunk_size:   8 * 1024
+    # chunk_size:   16 * 1024
+    # chunk_size:   64 * 1024
+    paths:
+      small:  PATH.resolve PATH.join __filename
+      big1:   '/usr/share/dict/american-english'
+  cfg.show      = cfg.size is 'small'
+  repetitions   = 5
+  test_names    = [
+    'n_readlines'
+    'n_readlines_patched'
+    'intertext_splitlines'
+    ]
+  global.gc() if global.gc?
+  data_cache = null
+  for _ in [ 1 .. repetitions ]
+    whisper '-'.repeat 108
+    for test_name in CND.shuffle test_names
+      global.gc() if global.gc?
+      await BM.benchmark bench, cfg, false, @, test_name
+  BM.show_totals bench
+
+
+############################################################################################################
+if require.main is module then do =>
+  await @run_benchmarks()
