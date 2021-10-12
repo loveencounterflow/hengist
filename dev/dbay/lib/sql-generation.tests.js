3a7812db22811c3466b4c7cf9af8db35be92754c
flow <flow@enceladus.orbit>
2021-10-12 17:22:23 +0200

    update

--- dev/dbay/lib/sql-generation.tests.js
+++ dev/dbay/lib/sql-generation.tests.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  var CND, H, PATH, SQL, badge, debug, echo, get_Sqlgen, help, info, isa, r, rpr, test, type_of, types, urge, validate, validate_list_of, warn, whisper;
+  var CND, H, PATH, SQL, badge, debug, echo, help, info, isa, r, rpr, test, type_of, types, urge, validate, validate_list_of, warn, whisper;
 
   //###########################################################################################################
   CND = require('cnd');
@@ -38,149 +38,35 @@
 
   r = String.raw;
 
-  get_Sqlgen = function() {
-    var DBay, Sqlgen;
-    ({DBay} = require(H.dbay_path));
-    types = require(PATH.join(H.dbay_path, 'lib/types'));
-    //-----------------------------------------------------------------------------------------------------------
-    types.declare('dbay_create_insert_cfg', {
-      tests: {
-        "@isa.object x": function(x) {
-          return this.isa.object(x);
-        },
-        "@isa.dbay_usr_schema x.schema": function(x) {
-          return this.isa.dbay_usr_schema(x.schema);
-        },
-        "@isa.dbay_name x.name": function(x) {
-          return this.isa.dbay_name(x.name);
-        },
-        "?@isa_list_of.nonempty_text x.exclude": function(x) {
-          if (x.exclude == null) {
-            true;
-          }
-          return this.isa_list_of.nonempty_text(x.exclude);
-        },
-        "?@isa_list_of.nonempty_text x.include": function(x) {
-          if (x.include == null) {
-            true;
-          }
-          return this.isa_list_of.nonempty_text(x.include);
-        },
-        "x.exclude, x.include may not appear together": function(x) {
-          return (x.exclude == null) || (x.include == null);
-        },
-        "x.on_conflict in [ 'nothing', 'sql', ]": function(x) {
-          var ref;
-          return (ref = x.on_conflict) === 'nothing' || ref === 'sql';
-        },
-        "@isa_list_of.nonempty_text x.fields": function(x) {
-          return this.isa_list_of.nonempty_text(x.fields);
-        }
-      }
-    });
-    // #-----------------------------------------------------------------------------------------------------------
-    // types.declare 'dbay_fields_of_cfg', tests:
-    //   "@isa.object x":                          ( x ) -> @isa.object x
-    //   # "@isa.ic_schema x.schema":                ( x ) -> @isa.ic_schema x.schema
-    //   # "@isa.nonempty_text x.name":              ( x ) -> @isa_optional.ic_name x.name
-
-      //===========================================================================================================
-    Sqlgen = class Sqlgen extends DBay {
-      //---------------------------------------------------------------------------------------------------------
-      // C
-
-        //---------------------------------------------------------------------------------------------------------
-      constructor(cfg) {
-        super(cfg);
-        return void 0;
-      }
-
-      //---------------------------------------------------------------------------------------------------------
-      prepare_insert(cfg) {
-        return this.prepare(this.create_insert(cfg));
-      }
-
-      create_insert(cfg) {
-        var I, L, R, V;
-        this.types.validate.dbay_create_insert_cfg((cfg = {...this.constructor.C.defaults.dbay_create_insert_cfg, ...cfg}));
-        ({L, I, V} = this.sql);
-        R = [];
-        R.push(`insert into ${I(cfg.schema)}.${I(cfg.table)}`);
-        return R.join(' ');
-      }
-
-      //---------------------------------------------------------------------------------------------------------
-      _get_fields(schema, name) {
-        var R, d, ref, schema_i, type;
-        // @types.validate.dbay_fields_of_cfg ( cfg = { @constructor.C.defaults.dbay_fields_of_cfg..., cfg..., } )
-        ({name, schema} = cfg);
-        schema_i = this.sql.I(schema);
-        R = {};
-        ref = this.query(SQL`select * from ${schema_i}.pragma_table_info( $name );`, {name});
-        for (d of ref) {
-          // { cid: 0, name: 'id', type: 'integer', notnull: 1, dflt_value: null, pk: 1 }
-          type = d.type === '' ? null : d.type;
-          R[d.name] = {
-            idx: d.cid,
-            type: type,
-            name: d.name,
-            optional: !d.notnull,
-            default: d.dflt_value,
-            is_pk: !!d.pk
-          };
-        }
-        return R;
-      }
-
-    };
-    return Sqlgen;
-  };
-
   //-----------------------------------------------------------------------------------------------------------
-  this["_DBAY Sqlgen demo"] = function(T, done) {
-    var Sqlgen, Tbl, db, dtab, schema;
+  this["DBAY Sqlgen demo"] = function(T, done) {
+    var DBay, Tbl, db, dtab, schema;
     // T?.halt_on_error()
-    // { DBay }          = require H.dbay_path
-    // db                = new DBay()
-    Sqlgen = get_Sqlgen();
-    db = new Sqlgen();
+    ({DBay} = require(H.dbay_path));
+    db = new DBay();
     ({Tbl} = require('../../../apps/icql-dba-tabulate'));
     dtab = new Tbl({
       dba: db
     });
     schema = 'main';
     //.........................................................................................................
+    if (T != null) {
+      T.throws(/object 'main.cities' does not exist/, () => {
+        return db._get_field_names('main', 'cities');
+      });
+    }
+    //.........................................................................................................
     db(function() {
-      var _, row;
+      var sql;
       db(SQL`create table cities (
   id      integer not null primary key,
   name    text    not null,
   country text    not null )`);
-      debug('^334^', db.create_insert({
+      sql = db.create_insert({
         schema,
-        table: 'cities'
-      }));
-      echo(dtab._tabulate(db(SQL`select type, name from sqlite_schema;`)));
-      echo(dtab._tabulate(db(SQL`select * from ${schema}.pragma_table_info( $name );`, {
-        name: 'cities'
-      })));
-      debug('^33443^', db._get_fields({
-        schema,
-        name: 'cities'
-      }));
-      return echo(dtab._tabulate((function() {
-        var ref, results;
-        ref = db._get_fields({
-          schema,
-          name: 'cities'
-        });
-        results = [];
-        for (_ in ref) {
-          row = ref[_];
-          results.push(row);
-        }
-        return results;
-      })()));
+        into: 'cities'
+      });
+      return T != null ? T.eq(sql, `insert into "main"."cities" ( "id", "name", "country" ) values ( $id, $name, $country );`) : void 0;
     });
     return typeof done === "function" ? done() : void 0;
   };
@@ -188,10 +74,13 @@
   //###########################################################################################################
   if (module === require.main) {
     (() => {
-      // test @, { timeout: 10e3, }
-      return this["_DBAY Sqlgen demo"]();
+      return test(this, {
+        timeout: 10e3
+      });
     })();
   }
+
+  // @[ "_DBAY Sqlgen demo" ]()
 
 }).call(this);
 
