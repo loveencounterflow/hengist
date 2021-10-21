ced4790f1a1f562922ab03baa4d724e756476cf7
flow <flow@enceladus.orbit>
2021-10-21 22:05:55 +0200

    [dbay-rustybuzz] update

--- dev/dbay-rustybuzz/lib/demos.js
+++ dev/dbay-rustybuzz/lib/demos.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  var CND, FS, H, PATH, RBW, SQL, badge, debug, echo, equals, guy, help, info, isa, rpr, type_of, types, urge, validate, validate_list_of, warn, whisper;
+  var CND, FS, H, PATH, RBW, SQL, badge, debug, echo, equals, guy, help, info, isa, newlinyfy, rpr, type_of, types, urge, validate, validate_list_of, warn, whisper;
 
   //###########################################################################################################
   CND = require('cnd');
@@ -42,11 +42,16 @@
 
   H = require('./helpers');
 
+  //-----------------------------------------------------------------------------------------------------------
+  newlinyfy = function(text) {
+    return ((Array.from(text)).join('\n')) + '\n';
+  };
+
   //===========================================================================================================
 
   //-----------------------------------------------------------------------------------------------------------
   this.demo_load_font_outlines = function() {
-    var DBay, Drb, Tbl, bbox, db, drb, dtab, font_idx, fontnick, fspath, gid, outline, pd, schema;
+    var DBay, Drb, Tbl, bbox, db, drb, dtab, font_idx, fontnick, fspath, gid, gids, outline, pd, schema, text;
     ({DBay} = require(H.dbay_path));
     ({Drb} = require(H.drb_path));
     ({Tbl} = require('../../../apps/icql-dba-tabulate'));
@@ -58,8 +63,9 @@
     });
     dtab = new Tbl({db});
     schema = 'drb';
-    fontnick = 'jzr';
-    fspath = PATH.resolve(PATH.join(__dirname, '../../../', 'assets/jizura-fonts/jizura3b.ttf'));
+    // fontnick = 'jzr';   fspath = PATH.resolve PATH.join __dirname, '../../../', 'assets/jizura-fonts/jizura3b.ttf'
+    fontnick = 'djvs';
+    fspath = PATH.resolve(PATH.join(__dirname, '../../../', 'assets/jizura-fonts/DejaVuSerif.ttf'));
     drb.register_fontnick({fontnick, fspath});
     echo(dtab._tabulate(db(SQL`select * from ${schema}.outlines order by fontnick, gid;`)));
     echo(dtab._tabulate(db(SQL`select * from ${schema}.fontnicks order by fontnick;`)));
@@ -70,6 +76,37 @@
     font_idx = 0;
     urge('^290^', outline = JSON.parse(drb.RBW.glyph_to_svg_pathdata(font_idx, gid)));
     urge('^290^', ({bbox, pd} = drb.get_single_outline({fontnick, gid})));
+    text = newlinyfy("sample text");
+    help('^290^', drb.RBW.shape_text({
+      format: 'json',
+      text,
+      0: 0
+    }));
+    help('^290^', drb.RBW.shape_text({
+      format: 'rusty',
+      text,
+      0: 0
+    }));
+    help('^290^', gids = drb.RBW.shape_text({
+      format: 'short',
+      text,
+      0: 0
+    }));
+    help('^290^', gids.split('|'));
+    // help '^290^', gids.match   /\|[^|]+\|[^|]+/g
+    help('^290^', gids = gids.replace(/\|([0-9]+:)[^|]+\|[^|]+/g, '$1'));
+    help('^290^', gids = gids.slice(0, gids.length - 2));
+    help('^290^', gids = gids.split(':'));
+    help('^290^', gids = (function() {
+      var i, len, results;
+      results = [];
+      for (i = 0, len = gids.length; i < len; i++) {
+        gid = gids[i];
+        results.push(parseInt(gid));
+      }
+      return results;
+    })());
+    // help '^290^', arrangement.replace /^\|[^|]+\|[^|]+/g
     return null;
   };
 
