a7ecf72074065089a876a534b8a1c0c3423be4e1
flow <flow@enceladus.orbit>
2021-10-25 23:51:33 +0200

    [dbay-rustybuzz] add test for `codepoints` mixin

---
+++ dev/dbay-rustybuzz/lib/codepoints.tests.js
@@ -0,0 +1,80 @@
+(function() {
+  'use strict';
+  var CND, H, PATH, SQL, badge, debug, echo, equals, guy, help, info, isa, rpr, test, type_of, types, urge, validate, validate_list_of, warn, whisper;
+
+  //###########################################################################################################
+  CND = require('cnd');
+
+  rpr = CND.rpr;
+
+  badge = 'DBAY-RUSTYBUZZ/CODEPOINTS/BASIC';
+
+  debug = CND.get_logger('debug', badge);
+
+  warn = CND.get_logger('warn', badge);
+
+  info = CND.get_logger('info', badge);
+
+  urge = CND.get_logger('urge', badge);
+
+  help = CND.get_logger('help', badge);
+
+  whisper = CND.get_logger('whisper', badge);
+
+  echo = CND.echo.bind(CND);
+
+  //...........................................................................................................
+  test = require('../../../apps/guy-test');
+
+  PATH = require('path');
+
+  // FS                        = require 'fs'
+  H = require('./helpers');
+
+  types = new (require('intertype')).Intertype();
+
+  ({isa, equals, type_of, validate, validate_list_of} = types.export());
+
+  SQL = String.raw;
+
+  guy = require('../../../apps/guy');
+
+  // MMX                       = require '../../../apps/multimix/lib/cataloguing'
+
+  //-----------------------------------------------------------------------------------------------------------
+  this["DRB get_unicode_codepoints()"] = function(T, done) {
+    var DBay, Drb;
+    // ### explicit path, explicitly temporary ###
+    // T?.halt_on_error()
+    // { DBay }            = require H.dbay_path
+    ({DBay} = require(H.dbay_path));
+    ({Drb} = require(H.drb_path));
+    (() => {      // path                = PATH.resolve DBay.C.autolocation, 'drb-23842847.sqlite'
+      // DH                  = require PATH.join H.dbay_path, 'lib/helpers'
+      //.........................................................................................................
+      var db, drb, result;
+      db = new DBay();
+      drb = new Drb({
+        db,
+        temporary: true
+      });
+      result = drb.get_unicode_codepoints();
+      /* as of NodeJS v16.9.1 with  Unicode 13 (?) */
+      if (T != null) {
+        T.ok(result.length >= 143_439);
+      }
+      return null;
+    })();
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //###########################################################################################################
+  if (require.main === module) {
+    (() => {
+      return test(this);
+    })();
+  }
+
+}).call(this);
+
+//# sourceMappingURL=codepoints.tests.js.map
