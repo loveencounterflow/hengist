1ea86e0e0ce314759d77e90acc62d08950fc8dcd
flow <flow@enceladus.orbit>
2021-12-04 08:42:34 +0100

    [dbay-rustybuzz] add demo for AD chains visualization

---
+++ dev/dbay-rustybuzz/lib/demo-ad-chain.js
@@ -0,0 +1,198 @@
+(function() {
+  'use strict';
+  var CND, DBay, Drb, FS, H, PATH, SQL, badge, debug, echo, equals, guy, help, info, isa, rpr, to_width, type_of, types, urge, validate, validate_list_of, warn, whisper, width_of;
+
+  //###########################################################################################################
+  CND = require('cnd');
+
+  rpr = CND.rpr;
+
+  badge = 'DBAY-RUSTYBUZZ/DEMO-AD-CHAIN';
+
+  debug = CND.get_logger('debug', badge);
+
+  warn = CND.get_logger('warn', badge);
+
+  info = CND.get_logger('info', badge);
+
+  urge = CND.get_logger('urge', badge);
+
+  help = CND.get_logger('help', badge);
+
+  whisper = CND.get_logger('whisper', badge);
+
+  echo = CND.echo.bind(CND);
+
+  //...........................................................................................................
+  PATH = require('path');
+
+  FS = require('fs');
+
+  H = require('./helpers');
+
+  types = new (require('intertype')).Intertype();
+
+  ({isa, equals, type_of, validate, validate_list_of} = types.export());
+
+  SQL = String.raw;
+
+  guy = require('../../../apps/guy');
+
+  // MMX                       = require '../../../apps/multimix/lib/cataloguing'
+  ({DBay} = require(H.dbay_path));
+
+  ({Drb} = require(H.drb_path));
+
+  ({width_of, to_width} = require('to-width'));
+
+  //===========================================================================================================
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.demo_ad_chain_1 = function(cfg) {
+    var ad, b1, b1_b2, b2, chrs, collector, db, drb, gid, hborder, i, idx, j, len, len1, line, ref, ref1, width;
+    // defaults        = { fontnick: 'b42', fspath: null, gid_1: 1, gid_2: 100, }
+    // cfg             = { defaults..., cfg..., }
+    // { fontnick
+    //   fspath
+    //   gid_1
+    //   gid_2    }    = cfg
+    // { Tbl, }        = require '../../../apps/icql-dba-tabulate'
+    db = new DBay({
+      path: '/dev/shm/typesetting-1.sqlite'
+    });
+    drb = new Drb({
+      db,
+      rebuild: false,
+      path: '/dev/shm/typesetting-2.sqlite'
+    });
+    // dtab            = new Tbl { db, }
+    collector = [[], [], [], [], []];
+    ref = db(SQL`select
+    *
+  from ads
+  where true
+    and ( alt = 1 )
+  order by b1
+  limit 50;`, {});
+    for (ad of ref) {
+      ({gid, chrs, b1, b2} = ad);
+      chrs = chrs.replace('\xad', '¬');
+      chrs = chrs.replace('\x20', '␣');
+      // chrs    = ' ' + chrs + ' '
+      // chrs    = to_width chrs, 5, { align: 'center', }
+      width = (width_of(chrs)) + 3 * 2;
+      hborder = '─'.repeat(width);
+      gid = to_width(gid.toString(), width, {
+        align: 'center'
+      });
+      b1_b2 = `${b1}, ${b2}`;
+      b1_b2 = to_width(b1_b2, width, {
+        align: 'center'
+      });
+      ref1 = [`  ${gid}  `, ` ┌${hborder}┐ `, `─┼─▶ ${chrs} ◀─┼─`, ` └${hborder}┘ `, `  ${b1_b2}  `];
+      for (idx = i = 0, len = ref1.length; i < len; idx = ++i) {
+        line = ref1[idx];
+        collector[idx].push(line);
+      }
+    }
+    for (j = 0, len1 = collector.length; j < len1; j++) {
+      line = collector[j];
+      echo(to_width(line.join(''), 200, {
+        align: 'center',
+        ellipsis: ''
+      }));
+    }
+    return null;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.demo_ad_chain_2 = function(cfg) {
+    var ad, b, b1, chrs, collector, db, drb, gid, h, i, len, line, ref, width;
+    db = new DBay({
+      path: '/dev/shm/typesetting-1.sqlite'
+    });
+    drb = new Drb({
+      db,
+      rebuild: false,
+      path: '/dev/shm/typesetting-2.sqlite'
+    });
+    collector = [[], [], [], [], []];
+    ref = db(SQL`select
+    *
+  from ads
+  where true
+    and ( alt > 1 )
+  order by b1
+  limit 50;`, {});
+    for (ad of ref) {
+      ({gid, chrs, b1} = ad);
+      b = ' ' + b1.toString();
+      chrs = chrs.replace('\xad', '¬');
+      chrs = chrs.replace('\x20', '␣');
+      gid = gid.toString();
+      width = Math.max(1, width_of(b), width_of(chrs), width_of(gid));
+      b = to_width(b, width, {
+        align: 'left'
+      });
+      chrs = to_width(chrs, width, {
+        align: 'right'
+      });
+      gid = to_width(gid, width, {
+        align: 'right'
+      });
+      h = '─'.repeat(width);
+      collector[0].push(b + ' ');
+      collector[1].push('┬' + h);
+      collector[2].push('│' + chrs);
+      collector[3].push('│' + gid);
+      collector[4].push('┴' + h);
+    }
+    for (i = 0, len = collector.length; i < len; i++) {
+      line = collector[i];
+      echo(to_width(line.join(''), 120, {
+        align: 'left'
+      }));
+    }
+    return null;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.demo_ad_chain_3 = function(cfg) {
+    var db, drb;
+    db = new DBay({
+      path: '/dev/shm/typesetting-1.sqlite'
+    });
+    drb = new Drb({
+      db,
+      rebuild: false,
+      path: '/dev/shm/typesetting-2.sqlite'
+    });
+    echo(drb.render_ad_chain({
+      format: 'compact',
+      b: 125,
+      context: 10
+    }));
+    return null;
+  };
+
+  /*
+
+  b       10 11 12 14 15  16 17  18 19
+           ┬──┬──┬──┬──┬───┬──┬───┬──┬───
+  chrs     │a │f │ ¬│ f│ i │r │ m │ ␣│
+  gid      │23|85| 3|-1|176|40|180| 3│
+           ┴──┴──┴──┴──┴───┴──┴───┴──┴───
+
+  */
+  //###########################################################################################################
+  if (require.main === module) {
+    (() => {
+      // @demo_ad_chain_1()
+      // @demo_ad_chain_2()
+      return this.demo_ad_chain_3();
+    })();
+  }
+
+}).call(this);
+
+//# sourceMappingURL=demo-ad-chain.js.map
---
+++ dev/dbay-rustybuzz/src/demo-ad-chain.coffee
@@ -0,0 +1,157 @@
+
+'use strict'
+
+
+############################################################################################################
+CND                       = require 'cnd'
+rpr                       = CND.rpr
+badge                     = 'DBAY-RUSTYBUZZ/DEMO-AD-CHAIN'
+debug                     = CND.get_logger 'debug',     badge
+warn                      = CND.get_logger 'warn',      badge
+info                      = CND.get_logger 'info',      badge
+urge                      = CND.get_logger 'urge',      badge
+help                      = CND.get_logger 'help',      badge
+whisper                   = CND.get_logger 'whisper',   badge
+echo                      = CND.echo.bind CND
+#...........................................................................................................
+PATH                      = require 'path'
+FS                        = require 'fs'
+H                         = require './helpers'
+types                     = new ( require 'intertype' ).Intertype
+{ isa
+  equals
+  type_of
+  validate
+  validate_list_of }      = types.export()
+SQL                       = String.raw
+guy                       = require '../../../apps/guy'
+# MMX                       = require '../../../apps/multimix/lib/cataloguing'
+{ DBay }                  = require H.dbay_path
+{ Drb }                   = require H.drb_path
+{ width_of
+  to_width }              = require 'to-width'
+
+
+
+#===========================================================================================================
+#
+#-----------------------------------------------------------------------------------------------------------
+@demo_ad_chain_1 = ( cfg ) ->
+  # defaults        = { fontnick: 'b42', fspath: null, gid_1: 1, gid_2: 100, }
+  # cfg             = { defaults..., cfg..., }
+  # { fontnick
+  #   fspath
+  #   gid_1
+  #   gid_2    }    = cfg
+  # { Tbl, }        = require '../../../apps/icql-dba-tabulate'
+  db              = new DBay { path: '/dev/shm/typesetting-1.sqlite', }
+  drb             = new Drb { db, rebuild: false, path: '/dev/shm/typesetting-2.sqlite', }
+  # dtab            = new Tbl { db, }
+  collector       = [
+    []
+    []
+    []
+    []
+    []
+    ]
+  for ad from db SQL"""
+    select
+        *
+      from ads
+      where true
+        and ( alt = 1 )
+      order by b1
+      limit 50;
+    """, {}
+    { gid
+      chrs
+      b1
+      b2  } = ad
+    chrs    = chrs.replace '\xad', '¬'
+    chrs    = chrs.replace '\x20', '␣'
+    # chrs    = ' ' + chrs + ' '
+    # chrs    = to_width chrs, 5, { align: 'center', }
+    width   = ( width_of chrs ) + 3 * 2
+    hborder = '─'.repeat width
+    gid     = to_width gid.toString(), width, { align: 'center', }
+    b1_b2   = "#{b1}, #{b2}"
+    b1_b2   = to_width b1_b2, width, { align: 'center', }
+    for line, idx in [
+      "  #{gid}  "
+      " ┌#{hborder}┐ "
+      "─┼─▶ #{chrs} ◀─┼─"
+      " └#{hborder}┘ "
+      "  #{b1_b2}  " ]
+      collector[ idx ].push line
+  for line in collector
+    echo to_width ( line.join '' ), 200, { align: 'center', ellipsis: '', }
+  return null
+
+
+
+#-----------------------------------------------------------------------------------------------------------
+@demo_ad_chain_2 = ( cfg ) ->
+  db              = new DBay { path: '/dev/shm/typesetting-1.sqlite', }
+  drb             = new Drb { db, rebuild: false, path: '/dev/shm/typesetting-2.sqlite', }
+  collector       = [
+    []
+    []
+    []
+    []
+    []
+    ]
+  for ad from db SQL"""
+    select
+        *
+      from ads
+      where true
+        and ( alt > 1 )
+      order by b1
+      limit 50;
+    """, {}
+    { gid
+      chrs
+      b1  } = ad
+    b       = ' ' + b1.toString()
+    chrs    = chrs.replace '\xad', '¬'
+    chrs    = chrs.replace '\x20', '␣'
+    gid     = gid.toString()
+    width   = Math.max 1, ( width_of b ), ( width_of chrs ), ( width_of gid )
+    b       = to_width b,     width, { align: 'left', }
+    chrs    = to_width chrs,  width, { align: 'right', }
+    gid     = to_width gid,   width, { align: 'right', }
+    h       = '─'.repeat width
+    collector[ 0 ].push b + ' '
+    collector[ 1 ].push '┬' + h
+    collector[ 2 ].push '│' + chrs
+    collector[ 3 ].push '│' + gid
+    collector[ 4 ].push '┴' + h
+  for line in collector
+    echo to_width ( line.join '' ), 120, { align: 'left', }
+  return null
+
+#-----------------------------------------------------------------------------------------------------------
+@demo_ad_chain_3 = ( cfg ) ->
+  db              = new DBay { path: '/dev/shm/typesetting-1.sqlite', }
+  drb             = new Drb { db, rebuild: false, path: '/dev/shm/typesetting-2.sqlite', }
+  echo drb.render_ad_chain { format: 'compact', b: 125, context: 10, }
+  return null
+
+###
+
+b       10 11 12 14 15  16 17  18 19
+         ┬──┬──┬──┬──┬───┬──┬───┬──┬───
+chrs     │a │f │ ¬│ f│ i │r │ m │ ␣│
+gid      │23|85| 3|-1|176|40|180| 3│
+         ┴──┴──┴──┴──┴───┴──┴───┴──┴───
+
+###
+
+
+############################################################################################################
+if require.main is module then do =>
+  # @demo_ad_chain_1()
+  # @demo_ad_chain_2()
+  @demo_ad_chain_3()
+
+
