6d897093925562423fcc3db785a4981c06c3bbb1
flow <flow@enceladus.orbit>
2021-12-28 11:16:15 +0100

    [dbay-rustybuzz] update

--- dev/dbay-rustybuzz/lib/helpers.js
+++ dev/dbay-rustybuzz/lib/helpers.js
@@ -122,7 +122,7 @@
         fontnick = 'eg8i';
         fspath = 'EBGaramond08-Italic.otf';
         chrs = "abcdefghijklm";
-        text = "affirm こちらはいい affluent";
+        text = "affirmこちらはいいaffluent";
         break;
       case 'specials-eg8i':
         fontnick = 'eg8i';
