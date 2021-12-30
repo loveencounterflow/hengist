f4a9a758dc9cd66ba5b7865fb0b4dd7d5e9faa44
flow <flow@enceladus.orbit>
2021-12-30 13:14:51 +0100

    [snippets] update

--- dev/snippets/lib/demo-svg-with-paper-js.js
+++ dev/snippets/lib/demo-svg-with-paper-js.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  var CND, FS, PAPER, badge, circle, debug, dom, echo, from_pt, help, info, radius, rectangle, rpr, svg, to_pt, union_pth, urge, warn, whisper;
+  var CND, FS, PAPER, badge, debug, echo, help, info, p1_pth, p2_pth, rpr, union_pth, urge, warn, whisper;
 
   //###########################################################################################################
   CND = require('cnd');
@@ -29,64 +29,58 @@
 
   PAPER.setup(new PAPER.Size(300, 600));
 
-  // stops   = [ ( new PAPER.Color 1, 1, 0, 0 ), 'red', 'black', ]
-  radius = PAPER.view.bounds.width * 0.4;
+  // # stops   = [ ( new PAPER.Color 1, 1, 0, 0 ), 'red', 'black', ]
+  // radius  = PAPER.view.bounds.width * 0.4
+  // from_pt = new PAPER.Point PAPER.view.center.x, 300
+  // to_pt   = from_pt.add radius, 0
 
-  from_pt = new PAPER.Point(PAPER.view.center.x, 300);
+  // circle = new PAPER.Path.Circle
+  //   center:       from_pt
+  //   radius:       radius * 0.7
+  //   fillColor:    'red'
+  //   strokeColor:  'black'
 
-  to_pt = from_pt.add(radius, 0);
+  // rectangle = new PAPER.Path.Rectangle
+  //   from:         PAPER.view.bounds.leftCenter
+  //   to:           PAPER.view.bounds.bottomRight
+  //   fillColor:    'red',
+  //   strokeColor:  'black'
 
-  circle = new PAPER.Path.Circle({
-    center: from_pt,
-    radius: radius * 0.7,
-    fillColor: 'red',
-    strokeColor: 'black'
-  });
+  // debug ( ( circle.exportSVG    { asString: false, } ) ).getAttribute 'd'
+  // debug ( ( rectangle.exportSVG { asString: false, } ) ).getAttribute 'd'
 
-  rectangle = new PAPER.Path.Rectangle({
-    from: PAPER.view.bounds.leftCenter,
-    to: PAPER.view.bounds.bottomRight,
-    fillColor: 'red',
-    strokeColor: 'black'
-  });
+  // p1_pth    = PAPER.PathItem.create 'M66,300c0,-46.39192 37.60808,-84 84,-84c46.39192,0 84,37.60808 84,84c0,46.39192 -37.60808,84 -84,84c-46.39192,0 -84,-37.60808 -84,-84z'
+  // rectangle = PAPER.PathItem.create 'M0,600v-300h300v300z'
+  p1_pth = PAPER.PathItem.create('M 40.49 201.605 L 60.313 201.605 L 60.313 403.212 L 40.49 403.212 L 40.49 201.605 Z');
 
-  rectangle.rotate(45).scale(0.7);
+  p2_pth = PAPER.PathItem.create('M 0 371.912 L 100.803 371.912 L 100.803 403.212 L 0 403.212 L 0 371.912 Z');
 
-  // svg = PAPER.project.exportSVG { asString: true, }
+  // p2_pth.rotate(45).scale(0.7)
   // console.log(svg)
   // fs.writeFileSync( '/tmp/paper1.svg', svg )
-  union_pth = rectangle.unite(circle);
+  union_pth = p2_pth.unite(p1_pth);
 
-  rectangle.remove();
+  p2_pth.remove();
 
-  circle.remove();
+  p1_pth.remove();
 
-  // debug '^35345^', ( k for k of circle )
-  debug('^35345^', circle.exportSVG({
+  // debug '^35345^', ( k for k of p1_pth )
+  debug('^35345^', p1_pth.exportSVG({
     asString: true
   }));
 
-  urge('^35345^', union_pth.exportSVG({
-    asString: true,
-    precision: 0
-  }));
-
-  svg = PAPER.project.exportSVG({
-    asString: true,
-    precision: 0
-  });
-
-  info(svg);
-
-  FS.writeFileSync('/tmp/paper2.svg', svg);
-
-  dom = PAPER.project.exportSVG({
+  urge('^35345^', (union_pth.exportSVG({
     asString: false,
     precision: 0
-  });
+  })).getAttribute('d'));
 
-  // debug '^445645^', ( k for k of dom )
-  help('^5345348^', (dom.getElementsByTagName('path'))[0].getAttribute('d'));
+  // svg = PAPER.project.exportSVG { asString: true, precision: 0, }
+// info svg
+// FS.writeFileSync '/tmp/paper2.svg', svg
+
+  // dom = PAPER.project.exportSVG { asString: false, precision: 0, }
+// # debug '^445645^', ( k for k of dom )
+// help '^5345348^', ( dom.getElementsByTagName 'path' )[ 0 ].getAttribute 'd'
 
 }).call(this);
 
