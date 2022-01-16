ccb6325193d8917efc5f65f7211e3e35824ba556
flow <flow@enceladus.orbit>
2022-01-16 18:37:48 +0100

    [dbay-mirage] update

--- dev/dbay-mirage/lib/demo-separate-data-tables.js
+++ dev/dbay-mirage/lib/demo-separate-data-tables.js
@@ -47,7 +47,7 @@
 
   //-----------------------------------------------------------------------------------------------------------
   this.demo_datamill = function(cfg) {
-    var DBay, Mrg, db, dsk, lnr, mrg, path, ref1, txt, x;
+    var DBay, Mrg, db, dsk, mrg, oln, path, ref1, txt, x;
     ({DBay} = require('../../../apps/dbay'));
     ({Mrg} = require('../../../apps/dbay-mirage/lib/main2'));
     db = new DBay();
@@ -62,33 +62,33 @@
     db.setv('allow_change_on_mirror', 1);
     db(mrg.sql.insert_lnpart, {
       dsk,
-      lnr: 2,
+      oln: 2,
       trk: 2,
       pce: 1,
       txt: `something`
     });
     mrg.deactivate({
       dsk,
-      lnr: 2,
+      oln: 2,
       trk: 2
     });
     db(mrg.sql.insert_lnpart, {
       dsk,
-      lnr: 2,
+      oln: 2,
       trk: 3,
       pce: 1,
       txt: `<div>`
     });
     db(mrg.sql.insert_lnpart, {
       dsk,
-      lnr: 2,
+      oln: 2,
       trk: 3,
       pce: 2,
       txt: `inserted content`
     });
     db(mrg.sql.insert_lnpart, {
       dsk,
-      lnr: 2,
+      oln: 2,
       trk: 3,
       pce: 3,
       txt: `</div>`
@@ -96,13 +96,13 @@
     //.........................................................................................................
     db.setv('dsk', dsk);
     H.tabulate('mrg_datasources', db(SQL`select * from mrg_datasources;`));
-    H.tabulate('mrg_mirror', db(SQL`select * from mrg_mirror order by dsk, lnr, trk, pce;`));
-    H.tabulate('mrg_lines', db(SQL`select * from mrg_lines  order by dsk, lnr;`));
+    H.tabulate('mrg_mirror', db(SQL`select * from mrg_mirror order by dsk, oln, trk, pce;`));
+    H.tabulate('mrg_lines', db(SQL`select * from mrg_lines  order by dsk, oln;`));
     H.banner(`lines of ${dsk}`);
     ref1 = mrg.walk_line_rows({dsk});
     for (x of ref1) {
-      ({lnr, txt} = x);
-      urge('^474^', lnr, rpr(txt));
+      ({oln, txt} = x);
+      urge('^474^', oln, rpr(txt));
     }
     return null;
   };
@@ -168,7 +168,7 @@
 
   //-----------------------------------------------------------------------------------------------------------
   this.demo_htmlish = function(cfg) {
-    var DBay, Mrg, db, dsk, lnr1, lnr2, mrg, par, path, prefix, ref1, txt, x;
+    var DBay, Mrg, db, dsk, mrg, path, prefix;
     ({DBay} = require('../../../apps/dbay'));
     ({Mrg} = require('../../../apps/dbay-mirage/lib/main2'));
     prefix = 'mrg';
@@ -181,53 +181,79 @@
     mrg.register_dsk({dsk, path});
     mrg.refresh_datasource({dsk});
     //.........................................................................................................
-    // db.setv 'allow_change_on_mirror', 1
     db(mrg.sql.insert_lnpart, {
       dsk,
-      lnr: 2,
+      oln: 2,
       trk: 2,
       pce: 1,
       txt: `something`
     });
     mrg.deactivate({
       dsk,
-      lnr: 2,
+      oln: 2,
       trk: 2
     });
     db(mrg.sql.insert_lnpart, {
       dsk,
-      lnr: 2,
+      oln: 2,
       trk: 3,
       pce: 1,
       txt: `<div>`
     });
     db(mrg.sql.insert_lnpart, {
       dsk,
-      lnr: 2,
+      oln: 2,
       trk: 3,
       pce: 2,
       txt: `inserted content`
     });
     db(mrg.sql.insert_lnpart, {
       dsk,
-      lnr: 2,
+      oln: 2,
       trk: 3,
       pce: 3,
       txt: `</div>`
     });
-    //.........................................................................................................
-    H.tabulate(`${prefix}_mirror`, db(SQL`select * from ${prefix}_mirror order by dsk, lnr, trk, pce;`));
-    H.tabulate(`${prefix}_mirror`, db(SQL`select * from ${prefix}_paragraph_linenumbers;`));
-    H.tabulate(`${prefix}_parmirror`, db(SQL`select * from ${prefix}_parmirror;`));
-    H.tabulate("mrg.walk_line_rows()", mrg.walk_line_rows({dsk}));
-    H.tabulate("mrg.walk_par_rows()", mrg.walk_par_rows({dsk}));
-    ref1 = mrg.walk_par_rows({dsk});
-    // return null
-    //.........................................................................................................
-    for (x of ref1) {
-      ({par, lnr1, lnr2, txt} = x);
-      H.tabulate(`${par} (${lnr1}..${lnr2}) ${rpr(txt)}`, normalize_tokens(HTMLISH.parse(txt)));
+    try {
+      db.setv('allow_change_on_mirror', 1);
+    } finally {
+      // mrg.deactivate { dsk, oln: 10, trk: 1, }
+      db.setv('allow_change_on_mirror', 0);
     }
+    db(mrg.sql.insert_lnpart, {
+      dsk,
+      oln: 11,
+      trk: 2,
+      pce: 1,
+      txt: ''
+    });
+    db(mrg.sql.insert_lnpart, {
+      dsk,
+      oln: 11,
+      trk: 2,
+      pce: 2,
+      txt: `generated paragraph`
+    });
+    db(mrg.sql.insert_lnpart, {
+      dsk,
+      oln: 11,
+      trk: 2,
+      pce: 3,
+      txt: ''
+    });
+    // #.........................................................................................................
+    // for { par, lnr1, lnr2, txt, } from mrg.walk_par_rows { dsk, }
+    //   H.tabulate "#{par} (#{lnr1}..#{lnr2}) #{rpr txt}", normalize_tokens HTMLISH.parse txt
+    //.........................................................................................................
+    H.tabulate(`${prefix}_mirror`, db(SQL`select * from ${prefix}_mirror           order by dsk, oln, trk, pce;`));
+    // H.tabulate "#{prefix}_mirror (act)",  db SQL"select * from #{prefix}_mirror where act order by dsk, oln, trk, pce;"
+    H.tabulate(`${prefix}_rwnmirror`, db(SQL`select * from ${prefix}_rwnmirror;`));
+    H.tabulate(`${prefix}_parlnrs0`, db(SQL`select * from ${prefix}_parlnrs0;`));
+    H.tabulate(`${prefix}_parlnrs`, db(SQL`select * from ${prefix}_parlnrs;`));
+    H.tabulate(`${prefix}_parmirror`, db(SQL`select * from ${prefix}_parmirror;`));
+    // H.tabulate "mrg.walk_line_rows()",  mrg.walk_line_rows { dsk, }
+    // H.tabulate "mrg.walk_par_rows()",   mrg.walk_par_rows { dsk, }
+    //.........................................................................................................
     return null;
   };
 
