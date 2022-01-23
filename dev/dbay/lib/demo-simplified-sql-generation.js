2364c4254323d6f89378e2aa3a78ebc8fe3f7bfe
loveencounterflow <paragate@gmx.net>
2022-01-23 22:08:42 +0100

    [dbay] implementing DB structure analysis, DDL generation

---
+++ dev/dbay/lib/demo-simplified-sql-generation.js
@@ -0,0 +1,141 @@
+(function() {
+  'use strict';
+  var CND, DBay, DBay2, FS, GUY, H, PATH, SQL, badge, debug, echo, equals, freeze, help, info, isa, lets, rpr, to_width, type_of, types, urge, validate, validate_list_of, warn, whisper;
+
+  //###########################################################################################################
+  CND = require('cnd');
+
+  rpr = CND.rpr;
+
+  badge = 'DBAY-MIRAGE/DEMO';
+
+  debug = CND.get_logger('debug', badge);
+
+  warn = CND.get_logger('warn', badge);
+
+  info = CND.get_logger('info', badge);
+
+  urge = CND.get_logger('urge', badge);
+
+  help = CND.get_logger('help', badge);
+
+  whisper = CND.get_logger('whisper', badge);
+
+  echo = CND.echo.bind(CND);
+
+  //...........................................................................................................
+  PATH = require('path');
+
+  FS = require('fs');
+
+  types = new (require('intertype')).Intertype();
+
+  ({isa, equals, type_of, validate, validate_list_of} = types.export());
+
+  SQL = String.raw;
+
+  GUY = require('../../../apps/guy');
+
+  // { HDML }                  = require '../../../apps/hdml'
+  H = require('../../../lib/helpers');
+
+  ({lets, freeze} = GUY.lft);
+
+  ({to_width} = require('to-width'));
+
+  ({DBay} = require('../../../apps/dbay'));
+
+  DBay2 = class DBay2 extends DBay {
+    //---------------------------------------------------------------------------------------------------------
+    get_primary_keys(cfg) {
+      var R, i, len, row, schema;
+      ({schema} = this.cfg);
+      R = this.all_rows(SQL`select 
+    -- pk                          as nr,
+    $table                      as "table",
+    name                        as field, 
+    lower( type )               as type,
+    not "notnull"               as nullable
+  from ${schema}.pragma_table_info( $table )
+  where true 
+    and ( pk > 0 )
+  order by pk;`, cfg);
+      for (i = 0, len = R.length; i < len; i++) {
+        row = R[i];
+        row.nullable = !!row.nullable;
+      }
+      return R;
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    get_foreign_keys(cfg) {
+      var R, schema;
+      ({schema} = this.cfg);
+      R = this.all_rows(SQL`select 
+    $table                      as from_table,
+    "from"                      as from_field,
+    "table"                     as to_table,
+    coalesce( "to", "from" )    as to_field
+  from ${schema}.pragma_foreign_key_list( $table )
+  order by seq;`, cfg);
+      return R;
+    }
+
+  };
+
+  //===========================================================================================================
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.demo_simplified_sql_generation = function(cfg) {
+    var Mrg, db, mrg;
+    ({Mrg} = require('../../../apps/dbay-mirage'));
+    db = new DBay2();
+    mrg = new Mrg({db});
+    //.........................................................................................................
+    db(SQL`create table a ( 
+    foo float,
+    bar float,
+    baz float,
+    nr integer,
+  primary key ( nr ) );
+create table b ( 
+    idx integer not null, 
+    b text, 
+  primary key ( idx ), 
+  foreign key ( idx ) references a ( nr ) );`);
+    //.........................................................................................................
+    // H.tabulate 'sqlite_schema', db SQL"select * from sqlite_schema;"
+    H.tabulate("pragma_table_list()", db(SQL`select * from pragma_table_list();`));
+    H.tabulate("pragma_table_info( 'mrg_mirror' )", db(SQL`select * from pragma_table_info( 'mrg_mirror' );`));
+    H.tabulate("pragma_foreign_key_list( 'mrg_mirror' )", db(SQL`select * from pragma_foreign_key_list( 'mrg_mirror' );`));
+    H.tabulate("pragma_table_info( 'mrg_raw_mirror' )", db(SQL`select * from pragma_table_info( 'mrg_raw_mirror' );`));
+    H.tabulate("pragma_foreign_key_list( 'mrg_raw_mirror' )", db(SQL`select * from pragma_foreign_key_list( 'mrg_raw_mirror' );`));
+    H.tabulate("db.get_primary_keys { table: 'mrg_raw_mirror', }", db.get_primary_keys({
+      table: 'mrg_raw_mirror'
+    }));
+    H.tabulate("pragma_table_info( 'a' )", db(SQL`select * from pragma_table_info( 'a' );`));
+    H.tabulate("pragma_foreign_key_list( 'a' )", db(SQL`select * from pragma_foreign_key_list( 'a' );`));
+    H.tabulate("pragma_table_info( 'b' )", db(SQL`select * from pragma_table_info( 'b' );`));
+    H.tabulate("pragma_foreign_key_list( 'b' )", db(SQL`select * from pragma_foreign_key_list( 'b' );`));
+    H.tabulate("db.get_primary_keys { table: 'b', }", db.get_primary_keys({
+      table: 'b'
+    }));
+    H.tabulate("db.get_foreign_keys { table: 'b', }", db.get_foreign_keys({
+      table: 'b'
+    }));
+    H.tabulate("db.get_foreign_keys { table: 'mrg_raw_mirror', }", db.get_foreign_keys({
+      table: 'mrg_raw_mirror'
+    }));
+    return null;
+  };
+
+  //###########################################################################################################
+  if (module === require.main) {
+    (() => {
+      return this.demo_simplified_sql_generation();
+    })();
+  }
+
+}).call(this);
+
+//# sourceMappingURL=demo-simplified-sql-generation.js.map
