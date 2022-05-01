c9958037c3d61ba027bf063bbabab20aa0d20b6f
loveencounterflow <loveencounterflow@example.com>
2022-05-01 10:16:16 +0200

    [dbay-vogue] add tests

---
+++ dev/dbay-vogue/lib/construction.tests.js
@@ -0,0 +1,90 @@
+(function() {
+  'use strict';
+  var CND, badge, debug, echo, equals, guy, help, info, isa, rpr, test, type_of, types, urge, validate, validate_list_of, warn, whisper;
+
+  //###########################################################################################################
+  CND = require('cnd');
+
+  rpr = CND.rpr;
+
+  badge = 'DBAY-VOGUE/TESTS/CONSTRUCTION';
+
+  debug = CND.get_logger('debug', badge);
+
+  warn = CND.get_logger('warn', badge);
+
+  info = CND.get_logger('info', badge);
+
+  urge = CND.get_logger('urge', badge);
+
+  help = CND.get_logger('help', badge);
+
+  whisper = CND.get_logger('whisper', badge);
+
+  echo = CND.echo.bind(CND);
+
+  //...........................................................................................................
+  test = require('../../../apps/guy-test');
+
+  // PATH                      = require 'path'
+  // FS                        = require 'fs'
+  // H                         = require './helpers'
+  types = new (require('intertype')).Intertype();
+
+  ({isa, equals, type_of, validate, validate_list_of} = types.export());
+
+  guy = require('../../../apps/guy');
+
+  // MMX                       = require '../../../apps/multimix/lib/cataloguing'
+
+  //-----------------------------------------------------------------------------------------------------------
+  this["exported names"] = function(T, done) {
+    var VOGUE;
+    VOGUE = require('../../../apps/dbay-vogue');
+    if (T != null) {
+      T.eq(type_of(VOGUE.Vogue_db), 'class');
+    }
+    if (T != null) {
+      T.eq(type_of(VOGUE.Vogue_scraper), 'class');
+    }
+    if (T != null) {
+      T.eq(type_of(VOGUE.Vogue_server), 'class');
+    }
+    if (T != null) {
+      T.eq(type_of(VOGUE.Vogue), 'class');
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this["construction 1"] = function(T, done) {
+    var Vogue, Vogue_db, Vogue_scraper, vogue;
+    if (T != null) {
+      T.halt_on_error();
+    }
+    ({Vogue, Vogue_db, Vogue_scraper} = require('../../../apps/dbay-vogue'));
+    vogue = new Vogue();
+    if (T != null) {
+      T.eq(type_of(vogue.vdb), 'vogue_db');
+    }
+    if (T != null) {
+      T.ok(vogue.vdb.hub === vogue);
+    }
+    if (T != null) {
+      T.throws(/unable to set hub on a vogue/, function() {
+        return vogue._set_hub({});
+      });
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this["property `hub` only settable where licensed"] = function(T, done) {
+    var Vogue, Vogue_db, Vogue_scraper;
+    ({Vogue, Vogue_db, Vogue_scraper} = require('../../../apps/dbay-vogue'));
+    return typeof done === "function" ? done() : void 0;
+  };
+
+}).call(this);
+
+//# sourceMappingURL=construction.tests.js.map
---
+++ dev/dbay-vogue/src/construction.tests.coffee
@@ -0,0 +1,57 @@
+
+'use strict'
+
+
+############################################################################################################
+CND                       = require 'cnd'
+rpr                       = CND.rpr
+badge                     = 'DBAY-VOGUE/TESTS/CONSTRUCTION'
+debug                     = CND.get_logger 'debug',     badge
+warn                      = CND.get_logger 'warn',      badge
+info                      = CND.get_logger 'info',      badge
+urge                      = CND.get_logger 'urge',      badge
+help                      = CND.get_logger 'help',      badge
+whisper                   = CND.get_logger 'whisper',   badge
+echo                      = CND.echo.bind CND
+#...........................................................................................................
+test                      = require '../../../apps/guy-test'
+# PATH                      = require 'path'
+# FS                        = require 'fs'
+# H                         = require './helpers'
+types                     = new ( require 'intertype' ).Intertype
+{ isa
+  equals
+  type_of
+  validate
+  validate_list_of }      = types.export()
+guy                       = require '../../../apps/guy'
+# MMX                       = require '../../../apps/multimix/lib/cataloguing'
+
+
+#-----------------------------------------------------------------------------------------------------------
+@[ "exported names" ] = ( T, done ) ->
+  VOGUE = require '../../../apps/dbay-vogue'
+  T?.eq ( type_of VOGUE.Vogue_db      ), 'class'
+  T?.eq ( type_of VOGUE.Vogue_scraper ), 'class'
+  T?.eq ( type_of VOGUE.Vogue_server  ), 'class'
+  T?.eq ( type_of VOGUE.Vogue         ), 'class'
+  return done?()
+
+#-----------------------------------------------------------------------------------------------------------
+@[ "construction 1" ] = ( T, done ) ->
+  T?.halt_on_error()
+  { Vogue
+    Vogue_db
+    Vogue_scraper } = require '../../../apps/dbay-vogue'
+  vogue = new Vogue()
+  T?.eq ( type_of vogue.vdb ), 'vogue_db'
+  T?.ok vogue.vdb.hub is vogue
+  T?.throws /unable to set hub on a vogue/, -> vogue._set_hub {}
+  return done?()
+
+#-----------------------------------------------------------------------------------------------------------
+@[ "property `hub` only settable where licensed" ] = ( T, done ) ->
+  { Vogue
+    Vogue_db
+    Vogue_scraper } = require '../../../apps/dbay-vogue'
+  return done?()
