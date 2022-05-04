fedcc64ac04a53a353160a28beaed3e4020a8da6
loveencounterflow <loveencounterflow@example.com>
2022-05-05 00:11:39 +0200

    [dbay] add demo for concurrent inserts

---
+++ dev/dbay/lib/demo-concurrent-writes.js
@@ -0,0 +1,160 @@
+(function() {
+  'use strict';
+  var CND, GUY, H, PATH, X, after, badge, debug, demo_concurrent_writes, echo, equals, every, help, info, isa, rpr, sleep, type_of, types, urge, validate, validate_list_of, warn, whisper;
+
+  //###########################################################################################################
+  CND = require('cnd');
+
+  rpr = CND.rpr;
+
+  badge = 'DBAY/DEMOS/CONCURRENT-WRITES';
+
+  debug = CND.get_logger('debug', badge);
+
+  warn = CND.get_logger('warn', badge);
+
+  info = CND.get_logger('info', badge);
+
+  urge = CND.get_logger('urge', badge);
+
+  help = CND.get_logger('help', badge);
+
+  whisper = CND.get_logger('whisper', badge);
+
+  echo = CND.echo.bind(CND);
+
+  //...........................................................................................................
+  PATH = require('path');
+
+  H = require('../../../lib/helpers');
+
+  types = new (require('intertype')).Intertype();
+
+  ({isa, equals, type_of, validate, validate_list_of} = types.export());
+
+  GUY = require('../../../apps/guy');
+
+  X = require('../../../lib/helpers');
+
+  every = function(dts, f) {
+    return setInterval(f, dts * 1000);
+  };
+
+  after = function(dts, f) {
+    return setTimeout(f, dts * 1000);
+  };
+
+  sleep = function(dts) {
+    return new Promise((done) => {
+      return setTimeout(done, dts * 1000);
+    });
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  demo_concurrent_writes = async function() {
+    var DBay, SQL, count, insert_numbers, insert_one, insert_two, p1, p2, path;
+    // T?.halt_on_error()
+    ({DBay} = require('../../../apps/dbay'));
+    ({SQL} = DBay);
+    path = PATH.resolve(PATH.join(__dirname, '../../../dev-shm/concurrent-writes.sqlite'));
+    count = 0;
+    insert_numbers = null;
+    (() => {      //.........................................................................................................
+      var db;
+      db = new DBay({path});
+      db(() => {
+        return db(SQL`drop table if exists c;
+create table c (
+    count integer not null,
+    src   text    not null,
+    n     integer not null,
+    s     integer not null,
+  primary key ( src, n ) );`);
+      });
+      return insert_numbers = db.prepare_insert({
+        into: 'c',
+        returning: '*'
+      });
+    })();
+    //.........................................................................................................
+    insert_one = async() => {
+      var db, error, i, n, row, s, src;
+      db = new DBay({path});
+      db.begin_transaction();
+      urge('^603-1^', "start one");
+      await sleep(0.1);
+      src = 'one';
+      for (n = i = 1; i <= 10; n = ++i) {
+        count++;
+        s = n ** 2;
+        row = {count, src, n, s};
+        try {
+          row = insert_numbers.get(row);
+        } catch (error1) {
+          error = error1;
+          warn('^603-1^', error.message, row);
+        }
+        help('^603-one^', row);
+        await sleep(0.1);
+      }
+      db.commit_transaction();
+      return null;
+    };
+    //.........................................................................................................
+    insert_two = async() => {
+      var db, error, i, n, row, s, src;
+      db = new DBay({path});
+      db.begin_transaction();
+      urge('^603-1^', "start two");
+      await sleep(0.1);
+      src = 'two';
+      for (n = i = 1; i <= 10; n = ++i) {
+        count++;
+        s = n ** 2;
+        row = {count, src, n, s};
+        try {
+          row = insert_numbers.get(row);
+        } catch (error1) {
+          error = error1;
+          warn('^603-2^', error.message, row);
+        }
+        help('^603-two^', row);
+        await sleep(0.1);
+      }
+      db.rollback_transaction();
+      return null;
+    };
+    //.........................................................................................................
+    p1 = new Promise((resolve) => {
+      return after(0.5, async() => {
+        await insert_one();
+        return resolve();
+      });
+    });
+    //.........................................................................................................
+    p2 = new Promise((resolve) => {
+      return after(0.5, async() => {
+        await insert_two();
+        return resolve();
+      });
+    });
+    await Promise.all([p1, p2]);
+    (() => {      //.........................................................................................................
+      var db;
+      db = new DBay({path});
+      return H.tabulate("c", db(SQL`select * from c order by src, n;`));
+    })();
+    //.........................................................................................................
+    return null;
+  };
+
+  //###########################################################################################################
+  if (require.main === module) {
+    (async() => {
+      return (await demo_concurrent_writes());
+    })();
+  }
+
+}).call(this);
+
+//# sourceMappingURL=demo-concurrent-writes.js.map
---
+++ dev/dbay/src/demo-concurrent-writes.coffee
@@ -0,0 +1,113 @@
+
+'use strict'
+
+
+############################################################################################################
+CND                       = require 'cnd'
+rpr                       = CND.rpr
+badge                     = 'DBAY/DEMOS/CONCURRENT-WRITES'
+debug                     = CND.get_logger 'debug',     badge
+warn                      = CND.get_logger 'warn',      badge
+info                      = CND.get_logger 'info',      badge
+urge                      = CND.get_logger 'urge',      badge
+help                      = CND.get_logger 'help',      badge
+whisper                   = CND.get_logger 'whisper',   badge
+echo                      = CND.echo.bind CND
+#...........................................................................................................
+PATH                      = require 'path'
+H                         = require '../../../lib/helpers'
+types                     = new ( require 'intertype' ).Intertype
+{ isa
+  equals
+  type_of
+  validate
+  validate_list_of }      = types.export()
+GUY                       = require '../../../apps/guy'
+X                         = require '../../../lib/helpers'
+every                     = ( dts, f ) ->                         setInterval f,    dts * 1000
+after                     = ( dts, f ) ->                         setTimeout  f,    dts * 1000
+sleep                     = ( dts    ) -> new Promise ( done ) => setTimeout  done, dts * 1000
+
+
+#-----------------------------------------------------------------------------------------------------------
+demo_concurrent_writes = ->
+  # T?.halt_on_error()
+  { DBay }            = require '../../../apps/dbay'
+  { SQL  }            = DBay
+  path                = ( PATH.resolve PATH.join __dirname, '../../../dev-shm/concurrent-writes.sqlite' )
+  count               = 0
+  insert_numbers      = null
+  #.........................................................................................................
+  do =>
+    db                  = new DBay { path, }
+    db =>
+      db SQL"""
+        drop table if exists c;
+        create table c (
+            count integer not null,
+            src   text    not null,
+            n     integer not null,
+            s     integer not null,
+          primary key ( src, n ) );
+        """
+    insert_numbers = db.prepare_insert { into: 'c', returning: '*', }
+  #.........................................................................................................
+  insert_one = =>
+    db                  = new DBay { path, }
+    db.begin_transaction()
+    urge '^603-1^', "start one"
+    await sleep 0.1
+    src = 'one'
+    for n in [ 1 .. 10 ]
+      count++
+      s   = n ** 2
+      row = { count, src, n, s, }
+      try
+        row = insert_numbers.get row
+      catch error
+        warn '^603-1^', error.message, row
+      help '^603-one^', row
+      await sleep 0.1
+    db.commit_transaction()
+    return null
+  #.........................................................................................................
+  insert_two = =>
+    db                  = new DBay { path, }
+    db.begin_transaction()
+    urge '^603-1^', "start two"
+    await sleep 0.1
+    src = 'two'
+    for n in [ 1 .. 10 ]
+      count++
+      s   = n ** 2
+      row = { count, src, n, s, }
+      try
+        row = insert_numbers.get row
+      catch error
+        warn '^603-2^', error.message, row
+      help '^603-two^', row
+      await sleep 0.1
+    db.rollback_transaction()
+    return null
+  #.........................................................................................................
+  p1 = new Promise ( resolve ) =>
+    after 0.5, =>
+      await insert_one()
+      resolve()
+  #.........................................................................................................
+  p2 = new Promise ( resolve ) =>
+    after 0.5, =>
+      await insert_two()
+      resolve()
+  await Promise.all [ p1, p2, ]
+  #.........................................................................................................
+  do =>
+    db                  = new DBay { path, }
+    H.tabulate "c", db SQL"select * from c order by src, n;"
+  #.........................................................................................................
+  return null
+
+
+############################################################################################################
+if require.main is module then do =>
+  await demo_concurrent_writes()
