94f95a5593f0b05de1cd44ae9cd614617be6bbf2
loveencounterflow <mail@somewhere.com>
2022-06-19 16:43:11 +0200

    [intertype] move demo code to proper project; add tests for Intertype NG

---
+++ dev/intertype/lib/_ng.test.js
@@ -0,0 +1,132 @@
+(function() {
+  'use strict';
+  var CND, H, alert, badge, debug, echo, help, info, log, njs_path, praise, rpr, test, urge, warn, whisper;
+
+  //###########################################################################################################
+  // njs_util                  = require 'util'
+  njs_path = require('path');
+
+  // njs_fs                    = require 'fs'
+  //...........................................................................................................
+  CND = require('cnd');
+
+  rpr = CND.rpr.bind(CND);
+
+  badge = 'INTERTYPE/tests/basics';
+
+  log = CND.get_logger('plain', badge);
+
+  info = CND.get_logger('info', badge);
+
+  whisper = CND.get_logger('whisper', badge);
+
+  alert = CND.get_logger('alert', badge);
+
+  debug = CND.get_logger('debug', badge);
+
+  warn = CND.get_logger('warn', badge);
+
+  help = CND.get_logger('help', badge);
+
+  urge = CND.get_logger('urge', badge);
+
+  praise = CND.get_logger('praise', badge);
+
+  echo = CND.echo.bind(CND);
+
+  //...........................................................................................................
+  test = require('guy-test');
+
+  // { intersection_of }       = require '../../../apps/intertype/lib/helpers'
+  H = require('../../../lib/helpers');
+
+  //-----------------------------------------------------------------------------------------------------------
+  this["isa"] = function(T, done) {
+    var Intertype, jto, types;
+    ({Intertype} = require('../../../apps/intertype'));
+    types = new Intertype();
+    jto = (x) => {
+      return ((Object.prototype.toString.call(x)).slice(8, -1)).toLowerCase().replace(/\s+/g, '');
+    };
+    types.declare('null', {
+      test: function(x) {
+        return x === null;
+      }
+    });
+    types.declare('array', {
+      collection: true,
+      test: function(x) {
+        return (jto(x)) === 'array';
+      }
+    });
+    /* @isa 'empty', 'collection', x */
+    // types.declare 'empty_array',                  test: ( x ) -> ( @isa 'array', x ) and x.length is 0
+    types.declare('list', {
+      test: function(x) {
+        return this.isa('array', x);
+      }
+    });
+    types.declare('integer', {
+      numeric: true,
+      test: function(x) {
+        return this.isa('array', x);
+      }
+    });
+    //.........................................................................................................
+    if (T != null) {
+      T.eq(types.isa('null', null), true);
+    }
+    if (T != null) {
+      T.eq(types.isa('optional', 'null', null), true);
+    }
+    if (T != null) {
+      T.eq(types.isa('optional', 'null', void 0), true);
+    }
+    if (T != null) {
+      T.eq(types.isa('null', void 0), false);
+    }
+    if (T != null) {
+      T.eq(types.isa('array', []), true);
+    }
+    if (T != null) {
+      T.eq(types.isa('list', []), true);
+    }
+    if (T != null) {
+      T.eq(types.isa('empty', 'array', []), true);
+    }
+    if (T != null) {
+      T.eq(types.isa('optional', 'empty', 'array', []), true);
+    }
+    //.........................................................................................................
+    if (T != null) {
+      T.throws(/'optional' cannot be a hedge in declarations/, () => {
+        return types.declare('optional', 'integer', {
+          test: function() {}
+        });
+      });
+    }
+    // for type, declaration of types._types
+    //   debug '^34234^', type, declaration
+    H.tabulate('types._types', (function*() {
+      var _, ref, results, type;
+      ref = types._types;
+      results = [];
+      for (_ in ref) {
+        type = ref[_];
+        results.push((yield type));
+      }
+      return results;
+    })());
+    //.........................................................................................................
+    done();
+    return null;
+  };
+
+  //###########################################################################################################
+  if (module.parent == null) {
+    test(this);
+  }
+
+}).call(this);
+
+//# sourceMappingURL=_ng.test.js.map
---
+++ dev/intertype/src/_ng.test.coffee
@@ -0,0 +1,63 @@
+
+'use strict'
+
+
+############################################################################################################
+# njs_util                  = require 'util'
+njs_path                  = require 'path'
+# njs_fs                    = require 'fs'
+#...........................................................................................................
+CND                       = require 'cnd'
+rpr                       = CND.rpr.bind CND
+badge                     = 'INTERTYPE/tests/basics'
+log                       = CND.get_logger 'plain',     badge
+info                      = CND.get_logger 'info',      badge
+whisper                   = CND.get_logger 'whisper',   badge
+alert                     = CND.get_logger 'alert',     badge
+debug                     = CND.get_logger 'debug',     badge
+warn                      = CND.get_logger 'warn',      badge
+help                      = CND.get_logger 'help',      badge
+urge                      = CND.get_logger 'urge',      badge
+praise                    = CND.get_logger 'praise',    badge
+echo                      = CND.echo.bind CND
+#...........................................................................................................
+test                      = require 'guy-test'
+# { intersection_of }       = require '../../../apps/intertype/lib/helpers'
+H                         = require '../../../lib/helpers'
+
+
+
+#-----------------------------------------------------------------------------------------------------------
+@[ "isa" ] = ( T, done ) ->
+  { Intertype }   = require '../../../apps/intertype'
+  types           = new Intertype()
+  jto = ( x ) => ( ( Object::toString.call x ).slice 8, -1 ).toLowerCase().replace /\s+/g, ''
+  types.declare 'null',                           test: ( x ) -> x is null
+  types.declare 'array',       collection: true,  test: ( x ) -> ( jto x ) is 'array'
+  ### @isa 'empty', 'collection', x ###
+  # types.declare 'empty_array',                  test: ( x ) -> ( @isa 'array', x ) and x.length is 0
+  types.declare 'list',                           test: ( x ) -> @isa 'array', x
+  types.declare 'integer',      numeric: true,    test: ( x ) -> @isa 'array', x
+  #.........................................................................................................
+  T?.eq ( types.isa 'null',                         null          ), true
+  T?.eq ( types.isa 'optional', 'null',             null          ), true
+  T?.eq ( types.isa 'optional', 'null',             undefined     ), true
+  T?.eq ( types.isa 'null',                         undefined     ), false
+  T?.eq ( types.isa 'array',                        []            ), true
+  T?.eq ( types.isa 'list',                         []            ), true
+  T?.eq ( types.isa 'empty', 'array',               []            ), true
+  T?.eq ( types.isa 'optional', 'empty', 'array',   []            ), true
+  #.........................................................................................................
+  T?.throws /'optional' cannot be a hedge in declarations/, => types.declare 'optional', 'integer', test: ->
+  # for type, declaration of types._types
+  #   debug '^34234^', type, declaration
+  H.tabulate 'types._types', ( -> yield type for _, type of types._types )()
+  #.........................................................................................................
+  done()
+  return null
+
+
+
+############################################################################################################
+unless module.parent?
+  test @
