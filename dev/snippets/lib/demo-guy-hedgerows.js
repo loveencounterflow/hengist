2557564440232e0f4a0a4b6b8838dd8fc55f15ad
loveencounterflow <mail@somewhere.com>
2022-08-21 10:46:40 +0200

    [snippets] update

--- dev/snippets/lib/demo-guy-hedgerows.js
+++ dev/snippets/lib/demo-guy-hedgerows.js
@@ -43,52 +43,36 @@
       this.state = {
         hedges: null
       };
-      R = this._get_top_proxy(this.cfg);
+      R = this._get_hedge_proxy(true, cfg.target);
       return R;
     }
 
     //---------------------------------------------------------------------------------------------------------
-    _get_top_proxy(cfg) {
+    _get_hedge_proxy(is_top, owner) {
       var R, dsc;
       dsc = {
+        //-----------------------------------------------------------------------------------------------------
         get: (target, key) => {
-          var R, hedges, sub_target;
-          this.state.hedges = [key];
+          var R, hedges, sub_owner;
+          if (is_top) {
+            this.state.hedges = [key];
+          } else {
+            this.state.hedges.push(key);
+          }
           if ((R = target[key]) !== void 0) {
+            //...................................................................................................
             return R;
           }
           hedges = [...this.state.hedges];
-          sub_target = (...P) => {
+          //...................................................................................................
+          sub_owner = (...P) => {
             return this.cfg.target(hedges, ...P);
           };
-          return target[key] != null ? target[key] : target[key] = this._get_sub_proxy({
-            target: sub_target
-          });
+          return target[key] != null ? target[key] : target[key] = this._get_hedge_proxy(false, sub_owner);
         }
       };
-      return R = new Proxy(cfg.target, dsc);
-    }
-
-    //---------------------------------------------------------------------------------------------------------
-    _get_sub_proxy(cfg) {
-      var R, dsc;
-      dsc = {
-        get: (target, key) => {
-          var R, hedges, sub_target;
-          this.state.hedges.push(key);
-          if ((R = target[key]) !== void 0) {
-            return R;
-          }
-          hedges = [...this.state.hedges];
-          sub_target = (...P) => {
-            return this.cfg.target(hedges, ...P);
-          };
-          return target[key] != null ? target[key] : target[key] = this._get_sub_proxy({
-            target: sub_target
-          });
-        }
-      };
-      return R = new Proxy(cfg.target, dsc);
+      //.......................................................................................................
+      return R = new Proxy(owner, dsc);
     }
 
   };
@@ -96,9 +80,9 @@
   //###########################################################################################################
   if (module === require.main) {
     (() => {
-      var error, h;
+      var _isa, error;
       //---------------------------------------------------------------------------------------------------------
-      isa = function(hedges, x) {
+      _isa = function(hedges, x) {
         var arity;
         if ((arity = arguments.length) !== 2) {
           throw new Error(`^387^ expected 2 arguments, got ${arity}`);
@@ -107,13 +91,13 @@
         return true;
       };
       //---------------------------------------------------------------------------------------------------------
-      info('^450-6^', h = new Hedge({
-        target: isa
+      info('^450-6^', isa = new Hedge({
+        target: _isa
       }));
       //.........................................................................................................
       info('^450-12^', (function() {
         try {
-          return h(1);
+          return isa(1);
         } catch (error1) {
           error = error1;
           return warn(rvr(error.message));
@@ -121,21 +105,20 @@
       })());
       info('^450-12^', (function() {
         try {
-          return h(1, 2, 3);
+          return isa(1, 2, 3);
         } catch (error1) {
           error = error1;
           return warn(rvr(error.message));
         }
       })());
       //.........................................................................................................
-      info('^450-12^', h(['one'], 1));
-      info('^450-12^', h.one(1));
-      info('^450-13^', h.one.two(2));
-      info('^450-14^', h.one.two.three(3));
-      info('^450-14^', h.one.two.three(3));
-      info('^450-15^', h.one.two.three.four(4));
-      info('^450-16^', h(['one', 'two', 'three', 'four', 'five'], 5));
-      info('^450-16^', h.one.two.three.four.five(5));
+      info('^450-12^', isa(['one'], 1));
+      info('^450-12^', isa.one(1));
+      info('^450-13^', isa.one.two(2));
+      info('^450-14^', isa.one.two.three(3));
+      info('^450-15^', isa.one.two.three.four(4));
+      info('^450-16^', isa(['one', 'two', 'three', 'four', 'five'], 5));
+      info('^450-16^', isa.one.two.three.four.five(5));
       //---------------------------------------------------------------------------------------------------------
       return null;
     })();
