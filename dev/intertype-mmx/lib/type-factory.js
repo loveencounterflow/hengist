77ccc24e03887d50fde4093ff2341cacaaa2861b
loveencounterflow <mail@somewhere.com>
2022-09-25 21:52:06 +0200

    [intertype-mmx letsfreezethat metteur] update

--- dev/intertype-mmx/lib/type-factory.js
+++ dev/intertype-mmx/lib/type-factory.js
@@ -140,7 +140,7 @@
   //           try
   //             return f x
   //           catch error
-  //             throw error if @hub.cfg.errors is 'throw' or error instanceof E.Intertype_error
+  //             throw error if @hub.cfg.errors or error instanceof E.Intertype_error
   //             @hub.state.error = error
   //           return false
   //         @hub.push_hedgeresult [ '▲nt2', @hub.state.isa_depth, dsc.name, x, R ]
--- dev/metteur/lib/basic.test.js
+++ dev/metteur/lib/basic.test.js
@@ -108,23 +108,257 @@
     MTR = require('../../../apps/metteur');
     template = "helo {name}.";
     tpl = new MTR.Template({template});
-    result = tpl.fill({
+    tpl.fill_all({
       name: "world"
     });
+    result = tpl.peek();
     if (T != null) {
       T.eq(result, "helo world.");
     }
     return typeof done === "function" ? done() : void 0;
   };
 
+  //-----------------------------------------------------------------------------------------------------------
+  this.mtr_template_rejects_nonstring_values = function(T, done) {
+    var MTR, template, tpl;
+    MTR = require('../../../apps/metteur');
+    template = "the answer is {answer}.";
+    tpl = new MTR.Template({template});
+    if (T != null) {
+      T.throws(/expected text, got a float/, function() {
+        var e;
+        try {
+          return tpl.fill_all({
+            answer: 42
+          });
+        } catch (error) {
+          e = error;
+          warn(rvr(e.message));
+          throw e;
+        }
+      });
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.mtr_template_rejects_missing_keys = function(T, done) {
+    var MTR, template, tpl;
+    MTR = require('../../../apps/metteur');
+    template = "the answer is {answer}.";
+    tpl = new MTR.Template({template});
+    if (T != null) {
+      T.throws(/unknown key 'answer'/, function() {
+        var e;
+        try {
+          return tpl.fill_all({});
+        } catch (error) {
+          e = error;
+          warn(rvr(e.message));
+          throw e;
+        }
+      });
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.mtr_template_accepts_custom_braces = function(T, done) {
+    var MTR, close, open, result, template, tpl;
+    MTR = require('../../../apps/metteur');
+    template = "the answer is ❰answer❱.";
+    open = '❰';
+    close = '❱';
+    tpl = new MTR.Template({template, open, close});
+    if (T != null) {
+      T.eq(tpl.cfg.open, '❰');
+    }
+    if (T != null) {
+      T.eq(tpl.cfg.close, '❱');
+    }
+    if (T != null) {
+      T.eq(tpl._cfg, {
+        open: '❰',
+        close: '❱',
+        rx: /❰(?<key>[^❱]*)❱/g
+      });
+    }
+    tpl.fill_all({
+      answer: "42"
+    });
+    result = tpl.peek();
+    if (T != null) {
+      T.eq(result, "the answer is 42.");
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.mtr_template_honours_triple_dots = function(T, done) {
+    var MTR, close, open, template, tpl;
+    MTR = require('../../../apps/metteur');
+    //.........................................................................................................
+    template = "the numbers are ❰...numbers❱ in ascending and ❰numbers...❱ in descending order.";
+    open = '❰';
+    close = '❱';
+    tpl = new MTR.Template({template, open, close});
+    tpl.fill_all({
+      numbers: "1"
+    });
+    if (T != null) {
+      T.eq(tpl.peek(), "the numbers are 1 in ascending and 1 in descending order.");
+    }
+    tpl.fill_all({
+      numbers: " 2 "
+    });
+    if (T != null) {
+      T.eq(tpl.peek(), "the numbers are 1 2  in ascending and  2 1 in descending order.");
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.mtr_template_finish_clears_segments = function(T, done) {
+    var MTR, close, open, template, tpl;
+    MTR = require('../../../apps/metteur');
+    //.........................................................................................................
+    template = "the answers are ❰...answer❱.";
+    open = '❰';
+    close = '❱';
+    tpl = new MTR.Template({template, open, close});
+    tpl.fill_all({
+      answer: "42"
+    });
+    tpl.fill_all({
+      answer: " and 108"
+    });
+    if (T != null) {
+      T.eq(tpl.finish(), "the answers are 42 and 108.");
+    }
+    if (T != null) {
+      T.eq(tpl.finish(), "the answers are .");
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.mtr_template_peek_returns_current_state = function(T, done) {
+    var MTR, close, open, template, tpl;
+    MTR = require('../../../apps/metteur');
+    //.........................................................................................................
+    template = "the {{{what}}} are {{{...answer}}}.";
+    open = '{{{';
+    close = '}}}';
+    tpl = new MTR.Template({template, open, close});
+    if (T != null) {
+      T.eq(tpl.peek(), "the  are .");
+    }
+    tpl.fill_all({
+      what: "answers",
+      answer: "42"
+    });
+    if (T != null) {
+      T.eq(tpl.peek(), "the answers are 42.");
+    }
+    tpl.fill_some({
+      answer: " and 108"
+    });
+    if (T != null) {
+      T.eq(tpl.peek(), "the answers are 42 and 108.");
+    }
+    if (T != null) {
+      T.eq(tpl.finish(), "the answers are 42 and 108.");
+    }
+    if (T != null) {
+      T.eq(tpl.peek(), "the  are .");
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.mtr_template_fill_some_targets_some_fields = function(T, done) {
+    var MTR, template, tpl;
+    MTR = require('../../../apps/metteur');
+    //.........................................................................................................
+    template = "I have {count1} {fruit1} and {count2} {fruit2}.";
+    tpl = new MTR.Template({template});
+    tpl.fill_some({
+      count1: "1"
+    });
+    if (T != null) {
+      T.eq(tpl.peek(), "I have 1  and  .");
+    }
+    tpl.fill_some({
+      count2: "2"
+    });
+    if (T != null) {
+      T.eq(tpl.peek(), "I have 1  and 2 .");
+    }
+    if (T != null) {
+      T.eq(tpl.finish(), "I have 1  and 2 .");
+    }
+    if (T != null) {
+      T.eq(tpl.peek(), "I have   and  .");
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.mtr_template_class_has_prop_misfit = function(T, done) {
+    var MTR, facets, template, tpl;
+    MTR = require('../../../apps/metteur');
+    //.........................................................................................................
+    template = "I have {count1} {fruit1} and {count2} {fruit2}.";
+    tpl = new MTR.Template({template});
+    facets = {
+      count1: "1",
+      fruit1: MTR.Template.misfit,
+      count2: "13",
+      fruit2: "bananas"
+    };
+    tpl.fill_some(facets);
+    if (T != null) {
+      T.eq(tpl.peek(), "I have 1  and 13 bananas.");
+    }
+    if (T != null) {
+      T.throws(/unknown key 'fruit1'/, function() {
+        var e;
+        try {
+          return tpl.fill_all(facets);
+        } catch (error) {
+          e = error;
+          warn(rvr(e.message));
+          throw e;
+        }
+      });
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.mtr_template_demo_splitting = function(T, done) {
+    var MTR, template, tpl;
+    MTR = require('../../../apps/metteur');
+    //.........................................................................................................
+    template = "I have {count1} {fruit1...} and {count2} {fruit2} and that's {what}.";
+    tpl = new MTR.Template({template});
+    debug('^4534^', tpl._segments);
+    debug('^4534^', tpl._mark_idxs);
+    debug('^4534^', tpl._idx_directions);
+    return typeof done === "function" ? done() : void 0;
+  };
+
   //###########################################################################################################
   if (module === require.main) {
     (() => {
+      // @mtr_template_cfg_prop_rpr()
+      // test @mtr_template_and_cfg_are_strict
+      // @mtr_template_demo_splitting()
+      // test @mtr_template_accepts_custom_braces
+      // test @mtr_template_honours_triple_dots
       return test(this);
     })();
   }
-
-  // @mtr_template_and_cfg_are_strict()
 
 }).call(this);
 
