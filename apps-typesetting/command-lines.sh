f1ddc2776d0081b544bf0bf5075c5dafd794c37d
loveencounterflow <mail@somewhere.com>
2022-10-15 11:09:08 +0200

    [apps-typesetting] add memo with command lines for typesetting

---
+++ apps-typesetting/command-lines.sh
@@ -0,0 +1,35 @@
+
+cd ~/jzr/hengist/apps-typesetting && live-server --wait=0 --no-browser --watch=html+svg-demos/demo-typeset-sample-page.output.html,html+svg-demos/overriding-svg-element-styles-with-css.html,html+svg-demos/demo-def-use-transform.html | prefix-datetime
+nodexh ~/jzr/hengist/dev/dbay-mirage/lib/demo-html-in-sql.js
+
+~/jzr/nodexh/bin/nodexh ~/3rd-party-repos/harfbuzzjs/examples/hbjs.example.js
+~/jzr/nodexh/bin/nodexh ~/3rd-party-repos/harfbuzzjs/examples/hbjs.example.node.js
+~/jzr/nodexh/bin/nodexh ~/jzr/hengist/dev/glyphshapes-and-typesetting-with-harfbuzz/lib/main.js
+~/jzr/nodexh/bin/nodexh ~/jzr/hengist/dev/glyphshapes-and-typesetting-with-harfbuzz/lib/main.tests.js
+~/jzr/nodexh/bin/nodexh ~/jzr/glyphshapes-and-typesetting-with-harfbuzz/lib/demo-harfbuzz.js
+~/jzr/nodexh/bin/nodexh ~/jzr/glyphshapes-and-typesetting-with-harfbuzz/lib/demo-opentypejs.js
+~/jzr/nodexh/bin/nodexh ~/jzr/glyphshapes-and-typesetting-with-harfbuzz/lib/demo-fontkit.js
+~/jzr/nodexh/bin/nodexh ~/jzr/glyphshapes-and-typesetting-with-harfbuzz/lib/demo-harfbuzzjs.js
+~/jzr/nodexh/bin/nodexh ~/jzr/glyphshapes-and-typesetting-with-harfbuzz/lib/demo-fonteditorcore.js
+~/jzr/nodexh/bin/nodexh ~/jzr/hengist/dev/glyphshapes-and-typesetting-with-harfbuzz/lib/textshaping.benchmarks.js
+~/jzr/nodexh/bin/nodexh ~/jzr/hengist/dev/glyphshapes-and-typesetting-with-harfbuzz/lib/rustybuzz-wasm-text-shaping-call-arities.benchmarks.js
+~/jzr/nodexh/bin/nodexh ~/jzr/hengist/dev/intertext/lib/demo-slab-based-typesetting.js
+nodexh ~/jzr/datamill/lib/experiments/generate-pdf.js
+nodexh ~/jzr/datamill/lib/main.js
+nodexh ~/jzr/interplot/lib/experiments/harfbuzzjs-fontmetrics.js
+nodexh ~/jzr/glyphshapes-and-typesetting-with-harfbuzz/lib/demo-fonteditorcore.js
+nodexh ~/jzr/glyphshapes-and-typesetting-with-harfbuzz/lib/demo-harfbuzz.js
+nodexh ~/jzr/glyphshapes-and-typesetting-with-harfbuzz/lib/demo-opentypejs.js
+nodexh ~/jzr/glyphshapes-and-typesetting-with-harfbuzz/lib/demo-fontkit.js
+nodexh ~/jzr/hengist/dev/glyphshapes-and-typesetting-with-harfbuzz/lib/basics.test.js
+nodexh ~/jzr/hengist/dev/glyphshapes-and-typesetting-with-harfbuzz/lib/main.tests.js
+nodexh ~/jzr/hengist/dev/glyphshapes-and-typesetting-with-harfbuzz/lib/rustybuzz-wasm-text-shaping-call-arities.benchmarks.js
+nodexh ~/jzr/hengist/dev/glyphshapes-and-typesetting-with-harfbuzz/lib/textshaping.benchmarks.js
+nodexh ~/jzr/hengist/dev/glyphshapes-and-typesetting-with-harfbuzz/lib/compact-single-file-svg-from-text.js
+time wasm-pack build --debug --target nodejs && trash pkg/.gitignore && nodexh ~/jzr/hengist/dev/dbay-rustybuzz/lib/demo-typeset-sample-page.js
+nodexh ~/jzr/hengist/apps-typesetting/rustybuzz-wasm/demo-nodejs-using-wasm/lib/main.js
+trash /dev/shm/typesetting-2.sqlite && nodexh ~/jzr/hengist/dev/dbay-rustybuzz/lib/demo-typeset-sample-page.js
+trash /dev/shm/typesetting-2.sqlite && node ~/jzr/hengist/dev/dbay-rustybuzz/lib/demo-typeset-sample-page.js
+time wasm-pack build --target nodejs && trash pkg/.gitignore && nodexh ~/jzr/hengist/dev/dbay-rustybuzz/lib/demo-typeset-sample-page.js
+node ~/jzr/hengist/dev/dbay-rustybuzz/lib/demo-typeset-sample-page.js
+nodexh ~/jzr/hengist/dev/dbay-rustybuzz/lib/demo-typeset-sample-page.js
