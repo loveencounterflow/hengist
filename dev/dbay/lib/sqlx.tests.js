7d005c86af60d77a1dde0e33d07f22ac29d6016d
loveencounterflow <mail@somewhere.com>
2022-10-16 11:55:30 +0200

    [dbay] update

--- dev/dbay/lib/sqlx.tests.js
+++ dev/dbay/lib/sqlx.tests.js
@@ -1,28 +1,14 @@
 (function() {
   'use strict';
-  var CND, H, PATH, X, badge, debug, echo, equals, guy, help, info, isa, rpr, test, type_of, types, urge, validate, validate_list_of, warn, whisper,
+  var DBay_sqlx, GUY, H, PATH, alert, debug, echo, equals, help, info, inspect, isa, log, plain, praise, rpr, test, type_of, types, urge, validate, validate_list_of, warn, whisper,
     splice = [].splice;
 
   //###########################################################################################################
-  CND = require('cnd');
+  GUY = require('guy');
 
-  rpr = CND.rpr;
+  ({alert, debug, help, info, plain, praise, urge, warn, whisper} = GUY.trm.get_loggers('DBAY/sqlx'));
 
-  badge = 'DBAY/TESTS/CONSTRUCTION';
-
-  debug = CND.get_logger('debug', badge);
-
-  warn = CND.get_logger('warn', badge);
-
-  info = CND.get_logger('info', badge);
-
-  urge = CND.get_logger('urge', badge);
-
-  help = CND.get_logger('help', badge);
-
-  whisper = CND.get_logger('whisper', badge);
-
-  echo = CND.echo.bind(CND);
+  ({rpr, inspect, echo, log} = GUY.trm);
 
   //...........................................................................................................
   test = require('../../../apps/guy-test');
@@ -36,33 +22,20 @@
 
   ({isa, equals, type_of, validate, validate_list_of} = types.export());
 
-  guy = require('../../../apps/guy');
+  // X                         = require '../../../lib/helpers'
 
-  X = require('../../../lib/helpers');
+    //===========================================================================================================
+  DBay_sqlx = class DBay_sqlx extends (require(H.dbay_path)).DBay {
+    //---------------------------------------------------------------------------------------------------------
+    constructor(...P) {
+      super(...P);
+      GUY.props.hide(this, '_sqlx_declarations', {});
+      GUY.props.hide(this, '_sqlx_cmd_re', null);
+      return void 0;
+    }
 
-  //-----------------------------------------------------------------------------------------------------------
-  this.dbay_sqlx_function = function(T, done) {
-    var DBay, E, SQL, Tbl, db, dtab;
-    // T?.halt_on_error()
-    E = require('../../../apps/dbay/lib/errors');
-    ({DBay} = require(H.dbay_path));
-    ({SQL} = DBay);
-    ({DBay} = require(H.dbay_path));
-    db = new DBay();
-    ({Tbl} = require('../../../apps/icql-dba-tabulate'));
-    dtab = new Tbl({
-      dba: db
-    });
-    //.........................................................................................................
-    E.DBay_sqlx_error = class DBay_sqlx_error extends E.DBay_error {
-      constructor(ref, message) {
-        super(ref, message);
-      }
-
-    };
-    //.........................................................................................................
-    db.definitions = {};
-    db.declare = function(sqlx) {
+    //---------------------------------------------------------------------------------------------------------
+    declare(sqlx) {
       var body, current_idx, match, name, name_re, parameters, parameters_re, ref1;
       this.types.validate.nonempty_text(sqlx);
       parameters_re = null;
@@ -86,21 +59,66 @@
         throw new E.DBay_sqlx_error('^dbay/sqlx@3^', `syntax error: parentheses are obligatory but missing in ${rpr(sqlx)}`);
       }
       // parameters              = []
+      //.......................................................................................................
       current_idx = (ref1 = parameters_re != null ? parameters_re.lastIndex : void 0) != null ? ref1 : name_re.lastIndex;
       body = sqlx.slice(current_idx).replace(/\s*;\s*$/, '');
-      this.definitions[name] = {name, parameters, body};
+      this._sqlx_declare({name, parameters, body});
       //.......................................................................................................
       return null;
-    };
-    //.........................................................................................................
-    db.resolve = function(sqlx) {
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    _sqlx_get_cmd_re() {
+      var R, name, names;
+      if ((R = this._sqlx_cmd_re) != null) {
+        return R;
+      }
+      names = (Object.keys(this._sqlx_declarations)).sort(function(a, b) {
+        a = (Array.from(a)).length;
+        b = (Array.from(b)).length;
+        if (a > b) {
+          return +1;
+        }
+        if (a < b) {
+          return -1;
+        }
+        return 0;
+      });
+      names = ((function() {
+        var i, len, results;
+        results = [];
+        for (i = 0, len = names.length; i < len; i++) {
+          name = names[i];
+          results.push(this._escape_literal_for_regex(name));
+        }
+        return results;
+      }).call(this)).join('|');
+      return this._sqlx_cmd_re = RegExp(`(?<=\\W|^)(?<name>${names})(?=\\W|$)`, "g");
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    /* thx to https://stackoverflow.com/a/6969486/7568091 and
+     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping */
+    _escape_literal_for_regex(literal) {
+      return literal.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    _sqlx_declare(cfg) {
+      this._sqlx_cmd_re = null;
+      this._sqlx_declarations[cfg.name] = cfg;
+      return null;
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    resolve(sqlx) {
       this.types.validate.nonempty_text(sqlx);
       return sqlx.replace(/(?<name>@[^\s^(]+)\(\s*(?<values>[^)]*?)\s*\)/g, (...P) => {
         var R, call_arity, definition, definition_arity, groups, i, idx, len, name, parameter, ref1, ref2, value, values;
         ref1 = P, [...P] = ref1, [groups] = splice.call(P, -1);
         ({name, values} = groups);
         values = values.split(/\s*,\s*/);
-        if ((definition = this.definitions[name]) == null) {
+        if ((definition = this._sqlx_declarations[name]) == null) {
           throw new E.DBay_sqlx_error('^dbay/sqlx@4^', `unknown name ${rpr(name)}`);
         }
         if ((call_arity = values.length) !== (definition_arity = definition.parameters.length)) {
@@ -116,6 +134,27 @@
         }
         return R;
       });
+    }
+
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.dbay_sqlx_function = function(T, done) {
+    var E, SQL, Tbl, db, dtab;
+    // T?.halt_on_error()
+    E = require('../../../apps/dbay/lib/errors');
+    ({SQL} = DBay_sqlx);
+    db = new DBay_sqlx();
+    ({Tbl} = require('../../../apps/icql-dba-tabulate'));
+    dtab = new Tbl({
+      dba: db
+    });
+    //.........................................................................................................
+    E.DBay_sqlx_error = class DBay_sqlx_error extends E.DBay_error {
+      constructor(ref, message) {
+        super(ref, message);
+      }
+
     };
     //.........................................................................................................
     db(function() {
@@ -132,6 +171,8 @@
       var sql, sqlx;
       db.declare(SQL`@max( @a, @b ) = case when @a > @b then @a else @b end;`);
       sqlx = SQL`select @max( 3, 2 ) as the_bigger_the_better;`;
+      debug('^87-1^', db._sqlx_get_cmd_re());
+      debug('^87-1^', [...(sqlx.matchAll(db._sqlx_get_cmd_re()))]);
       sql = db.resolve(sqlx);
       help(rpr(sqlx));
       info(rpr(sql));
