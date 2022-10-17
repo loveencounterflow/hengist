ddfebb60babc23965e6e203a26d5d9a8091a3ffc
loveencounterflow <mail@somewhere.com>
2022-10-17 16:43:27 +0200

    [dbay] update

--- dev/dbay/lib/sqlx.tests.js
+++ dev/dbay/lib/sqlx.tests.js
@@ -29,6 +29,8 @@
 
   E = require('../../../apps/dbay/lib/errors');
 
+  equals = (require('util')).isDeepStrictEqual;
+
   //===========================================================================================================
   DBay_sqlx = class DBay_sqlx extends (require(H.dbay_path)).DBay {
     //---------------------------------------------------------------------------------------------------------
@@ -41,7 +43,7 @@
 
     //---------------------------------------------------------------------------------------------------------
     declare(sqlx) {
-      var body, current_idx, match, name, name_re, parameters, parameters_re, ref1;
+      var arity, body, current_idx, match, name, name_re, parameters, parameters_re, ref1;
       this.types.validate.nonempty_text(sqlx);
       parameters_re = null;
       //.......................................................................................................
@@ -59,6 +61,9 @@
         }
         ({parameters} = match.groups);
         parameters = parameters.split(/\s*,\s*/);
+        if (equals(parameters, [''])) {
+          parameters = [];
+        }
       } else {
         /* extension for declaration, call w/out parentheses left for later */
         // throw new E.DBay_sqlx_error '^dbay/sqlx@3^', "syntax error: parentheses are obligatory but missing in #{rpr sqlx}"
@@ -67,7 +72,8 @@
       //.......................................................................................................
       current_idx = (ref1 = parameters_re != null ? parameters_re.lastIndex : void 0) != null ? ref1 : name_re.lastIndex;
       body = sqlx.slice(current_idx).replace(/\s*;\s*$/, '');
-      this._sqlx_declare({name, parameters, body});
+      arity = parameters.length;
+      this._sqlx_declare({name, parameters, arity, body});
       //.......................................................................................................
       return null;
     }
@@ -98,7 +104,7 @@
         }
         return results;
       }).call(this)).join('|');
-      return this._sqlx_cmd_re = RegExp(`(?<=\\W|^)(?<name>${names})(?=\\W|$)`, "g");
+      return this._sqlx_cmd_re = RegExp(`(?<=\\W|^)(?<name>${names})(?=\\W|$)(?<tail>.*)$`, "g");
     }
 
     //---------------------------------------------------------------------------------------------------------
@@ -120,65 +126,67 @@
 
     //---------------------------------------------------------------------------------------------------------
     resolve(sqlx) {
+      var count, sql_after, sql_before;
       this.types.validate.nonempty_text(sqlx);
-      return sqlx.replace(this._sqlx_get_cmd_re(), (..._matches) => {
-        var _sqlx, call_arity, definition, groups, i, idx, len, match, matches, name, ref1, tail;
-        ref1 = _matches, [..._matches] = ref1, [idx, _sqlx, groups] = splice.call(_matches, -3);
-        // debug '^546^', rpr sqlx[ idx ... idx + groups.name.length ]
-        ({name} = groups);
-        tail = sqlx.slice(idx + name.length);
-        //.....................................................................................................
-        if ((definition = this._sqlx_declarations[name]) == null) {
-          /* NOTE should never happen as we always re-compile pattern from declaration keys */
-          throw new E.DBay_sqlx_error('^dbay/sqlx@4^', `unknown name ${rpr(name)}`);
+      sql_before = sqlx;
+      count = 0;
+      while (true) {
+        if (count++ > 10_000/* NOTE to avoid deadlock, just in case */) {
+          //.......................................................................................................
+          break;
         }
-        //.....................................................................................................
-        if (tail.startsWith('(')) {
-          debug('^87-1^', rpr(tail));
-          matches = new_xregex.matchRecursive(tail, '\\(', '\\)', '', {
-            escapeChar: '\\',
-            unbalanced: 'skip-lazy',
-            valueNames: ['outside', 'before', 'between', 'after']
-          });
-          for (i = 0, len = matches.length; i < len; i++) {
-            match = matches[i];
-            if (!(match.name === 'between')) {
-              continue;
-            }
-            debug('^87-2^', rpr(match.value));
-            break;
+        sql_after = sql_before.replace(this._sqlx_get_cmd_re(), (..._matches) => {
+          /* ------------------------------------- */
+          /* TAINT this part to be done with lexer */
+          var R, _sqlx, after, before, between, call_arity, declaration, groups, i, idx, len, matches, name, parameter, ref1, ref2, tail, value, values;
+          ref1 = _matches, [..._matches] = ref1, [idx, _sqlx, groups] = splice.call(_matches, -3);
+          // debug '^546^', rpr sqlx[ idx ... idx + groups.name.length ]
+          ({name, tail} = groups);
+          //.....................................................................................................
+          if ((declaration = this._sqlx_declarations[name]) == null) {
+            /* NOTE should never happen as we always re-compile pattern from declaration keys */
+            throw new E.DBay_sqlx_error('^dbay/sqlx@4^', `unknown name ${rpr(name)}`);
           }
-        } else {
-          call_arity = 0;
+          //.....................................................................................................
+          if (tail.startsWith('(')) {
+            matches = new_xregex.matchRecursive(tail, '\\(', '\\)', '', {
+              escapeChar: '\\',
+              unbalanced: 'skip-lazy',
+              valueNames: ['outside', 'before', 'between', 'after']
+            });
+            [before, between, after] = matches;
+            tail = tail.slice(after.end);
+            values = between.value.trim();
+            values = between.value.trim();
+            values = values.split(/\s*,\s*/);
+            if (equals(values, [''])) {
+              values = [];
+            }
+            call_arity = values.length;
+          } else {
+            call_arity = 0;
+          }
+          //.....................................................................................................
+          if (call_arity !== declaration.arity) {
+            throw new E.DBay_sqlx_error('^dbay/sqlx@5^', `expected ${declaration.arity} argument(s), got ${call_arity}`);
+          }
+          //.....................................................................................................
+          R = declaration.body;
+          ref2 = declaration.parameters;
+          for (idx = i = 0, len = ref2.length; i < len; idx = ++i) {
+            parameter = ref2[idx];
+            value = values[idx];
+            R = R.replace(RegExp(`${parameter}`, "g"), value);
+          }
+          return R + tail;
+        });
+        if (sql_after === sql_before) {
+          break;
         }
-        // #.....................................................................................................
-        // unless ( call_arity = values.length ) is ( definition_arity = definition.parameters.length )
-        //   throw new E.DBay_sqlx_error '^dbay/sqlx@5^', "expected #{definition_arity} arguments, got #{call_arity}"
-        // #.....................................................................................................
-        // debug '^546^', groups
-        return '*';
-      });
-      return sqlx.replace(/(?<name>@[^\s^(]+)\(\s*(?<values>[^)]*?)\s*\)/g, (...P) => {
-        var R, call_arity, definition, definition_arity, groups, i, idx, len, name, parameter, ref1, ref2, value, values;
-        ref1 = P, [...P] = ref1, [groups] = splice.call(P, -1);
-        ({name, values} = groups);
-        values = values.split(/\s*,\s*/);
-        if ((definition = this._sqlx_declarations[name]) == null) {
-          throw new E.DBay_sqlx_error('^dbay/sqlx@4^', `unknown name ${rpr(name)}`);
-        }
-        if ((call_arity = values.length) !== (definition_arity = definition.parameters.length)) {
-          throw new E.DBay_sqlx_error('^dbay/sqlx@5^', `expected ${definition_arity} arguments, got ${call_arity}`);
-        }
-        //.....................................................................................................
-        R = definition.body;
-        ref2 = definition.parameters;
-        for (idx = i = 0, len = ref2.length; i < len; idx = ++i) {
-          parameter = ref2[idx];
-          value = values[idx];
-          R = R.replace(RegExp(`${parameter}`, "g"), value);
-        }
-        return R;
-      });
+        sql_before = sql_after;
+      }
+      //.......................................................................................................
+      return sql_after;
     }
 
   };
@@ -268,6 +276,7 @@
       info(rpr(sql));
       return T != null ? T.eq(sql, SQL`select 'a' || 'b' as c1, 'c' || 'd' as c2;`) : void 0;
     });
+    return typeof done === "function" ? done() : void 0;
     //.........................................................................................................
     db(function() {
       var sql, sqlx;
@@ -329,13 +338,11 @@
   if (require.main === module) {
     (() => {
       // test @
-      // @dbay_sqlx_lexer()
-      return this.dbay_sql_lexer();
+      // @dbay_sql_lexer()
+      this.dbay_sqlx_function();
+      return test(this.dbay_sqlx_function);
     })();
   }
-
-  // @dbay_sqlx_function()
-// test @dbay_sqlx_function
 
 }).call(this);
 
