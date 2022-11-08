55c1bb4ec9297d8d28ed25101b5030e726fc46b2
loveencounterflow <mail@somewhere.com>
2022-11-08 19:31:05 +0100

    [moonriver] update

--- dev/moonriver/lib/demo-ng.js
+++ dev/moonriver/lib/demo-ng.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  var Collector, GUY, Pipeline, Segment, UTIL, alert, debug, echo, help, info, inspect, isa, log, plain, praise, rpr, type_of, types, urge, validate, validate_optional, warn, whisper;
+  var GUY, Pipeline, Reporting_collector, Segment, UTIL, alert, debug, def, echo, help, hide, info, inspect, isa, log, nameit, plain, praise, rpr, type_of, types, urge, validate, validate_optional, warn, whisper;
 
   //###########################################################################################################
   GUY = require('guy');
@@ -16,15 +16,27 @@
 
   UTIL = require('node:util');
 
+  ({hide, def} = GUY.props);
+
+  nameit = function(name, f) {
+    return def(f, 'name', {
+      value: name
+    });
+  };
+
   //===========================================================================================================
   Segment = class Segment {
     //---------------------------------------------------------------------------------------------------------
     constructor(cfg) {
-      var ref, ref1, send;
+      var name, ref, ref1, send;
       this.input = (ref = cfg.input) != null ? ref : [];
       this.output = (ref1 = cfg.output) != null ? ref1 : [];
-      GUY.props.hide(this, 'transform', cfg.transform.bind(this));
-      /* binding is optional */      GUY.props.hide(this, '_send', send = (d) => {
+      name = cfg.transform.name;
+      if (name === '') {
+        name = 'ƒ';
+      }
+      hide(this, 'transform', nameit(name, cfg.transform.bind(this)));
+      /* binding is optional */      hide(this, '_send', send = (d) => {
         this.output.push(d);
         return d/* 'inner' send method */;
       });
@@ -47,17 +59,23 @@
       return 0;
     }
 
+    //---------------------------------------------------------------------------------------------------------
+    [UTIL.inspect.custom]() {
+      return this.toString();
+    }
+
+    toString() {
+      return `${rpr(this.input)} ▶ ${this.transform.name} ▶ ${rpr(this.output)}`;
+    }
+
   };
 
-  //---------------------------------------------------------------------------------------------------------
-  // unshift: ->
-
-    //===========================================================================================================
-  Collector = class Collector {
+  //===========================================================================================================
+  Reporting_collector = class Reporting_collector {
     //---------------------------------------------------------------------------------------------------------
-    constructor(host) {
-      GUY.props.hide(this, 'host', host);
-      GUY.props.hide(this, 'd', []);
+    constructor(callback) {
+      hide(this, 'callback', callback);
+      hide(this, 'd', []);
       GUY.props.def(this, 'length', {
         get: function() {
           return this.d.length;
@@ -68,22 +86,22 @@
 
     //---------------------------------------------------------------------------------------------------------
     push(d) {
-      this.host._on_change_datacount(+1);
+      this.callback(+1);
       return this.d.push(d);
     }
 
     unshift(d) {
-      this.host._on_change_datacount(+1);
+      this.callback(+1);
       return this.d.unshift(d);
     }
 
     pop() {
-      this.host._on_change_datacount(-1);
+      this.callback(-1);
       return this.d.pop();
     }
 
     shift() {
-      this.host._on_change_datacount(-1);
+      this.callback(-1);
       return this.d.shift();
     }
 
@@ -102,30 +120,40 @@
   Pipeline = class Pipeline {
     //---------------------------------------------------------------------------------------------------------
     constructor(cfg) {
+      var ref, ref1;
+      cfg = {...{}, ...cfg};
+      // cfg             = types.create.mr_pipeline_cfg cfg
       this.datacount = 0;
-      this.input = new Collector(this);
-      this.output = new Collector(this);
-      this.segments = [];
+      this.input = this._new_collector();
+      this.output = [];
+      this./* pipeline output buffer does not participate in datacount */segments = [];
+      this.on_before_step = (ref = cfg.on_before_step) != null ? ref : null;
+      this.on_after_step = (ref1 = cfg.on_after_step) != null ? ref1 : null;
       return void 0;
     }
 
     //---------------------------------------------------------------------------------------------------------
-    _on_change_datacount(delta) {
-      return this.datacount += delta;
+    _new_collector() {
+      return new Reporting_collector((delta) => {
+        return this.datacount += delta;
+      });
     }
 
     //---------------------------------------------------------------------------------------------------------
     push(transform) {
-      var R, count, input, output, prv_segment;
-      output = this.output;
+      var R, count, input, prv_segment;
       if ((count = this.segments.length) === 0) {
         input = this.input;
       } else {
         prv_segment = this.segments[count - 1];
-        prv_segment.output = new Collector(this);
+        prv_segment.output = this._new_collector();
         input = prv_segment.output;
       }
-      R = new Segment({input, transform, output});
+      R = new Segment({
+        input,
+        transform,
+        output: this.output
+      });
       this.segments.push(R);
       return R;
     }
@@ -137,27 +165,40 @@
     }
 
     //---------------------------------------------------------------------------------------------------------
-    * walk() {
-      var i, len, ref, segment;
-      while (true) {
-        ref = this.segments;
-        for (i = 0, len = ref.length; i < len; i++) {
-          segment = ref[i];
-          debug('^56-1^', this);
-          segment.process();
-          debug('^56-1^', this);
-          debug('^56-1^', {
-            output: this.output,
-            datacount: this.datacount
-          });
-          while (this.output.length > 0) {
-            yield this.output.shift();
-          }
+    process() {
+      var i, len, ref, segment, segment_idx;
+      ref = this.segments;
+      for (segment_idx = i = 0, len = ref.length; i < len; segment_idx = ++i) {
+        segment = ref[segment_idx];
+        if (this.on_before_step != null) {
+          this.on_before_step(segment_idx);
         }
-        debug('^56-1^', {
-          output: this.output,
-          datacount: this.datacount
-        });
+        segment.process();
+        if (this.on_after_step != null) {
+          this.on_after_step(segment_idx);
+        }
+      }
+      return null;
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    run() {
+      var d, ref, results;
+      ref = this.walk();
+      results = [];
+      for (d of ref) {
+        results.push(d);
+      }
+      return results;
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    * walk() {
+      while (true) {
+        this.process();
+        while (this.output.length > 0) {
+          yield this.output.shift();
+        }
         if (this.datacount < 1) {
           break;
         }
@@ -168,6 +209,7 @@
     //---------------------------------------------------------------------------------------------------------
     show() {
       var i, len, ref, segment;
+      /* TAINT return string, do not output */
       echo({
         input: this.input
       });
@@ -191,28 +233,55 @@
   //###########################################################################################################
   if (module === require.main) {
     (() => {
-      var d, p, ref;
-      p = new Pipeline();
-      p.push(function(d, send) {
-        return send(d * 2);
+      var on_after_step, on_before_step, p, plus_2, times_2, times_3;
+      on_before_step = function(sidx) {
+        var datacount;
+        ({datacount} = this);
+        info('-'.repeat(108));
+        info({sidx, datacount});
+        return this.show();
+      };
+      on_after_step = function(sidx) {
+        var datacount;
+        ({datacount} = this);
+        urge({sidx, datacount});
+        return this.show();
+      };
+      on_before_step = null;
+      on_after_step = null;
+      p = new Pipeline({on_before_step, on_after_step});
+      p.push(times_2 = function(d, send) {
+        if (isa.float(d)) {
+          send('(');
+          send(d * 2);
+          return send(')');
+        } else {
+          return send(d);
+        }
       });
-      p.push(function(d, send) {
-        return send(d + 2);
+      p.push(plus_2 = function(d, send) {
+        if (isa.float(d)) {
+          send('[');
+          send(d + 2);
+          return send(']');
+        } else {
+          return send(d);
+        }
       });
-      debug(p.input.host);
-      debug(p.input.host === p);
+      p.push(times_3 = function(d, send) {
+        if (isa.float(d)) {
+          send('{');
+          send(d * 3);
+          return send('}');
+        } else {
+          return send(d);
+        }
+      });
       p.send(1);
       p.send(2);
       p.send(3);
-      p.show();
-      ref = p.walk();
-      // info '^97-3^', p.segments[ 0 ].process()
-      // p.show()
-      // info '^97-3^', p.segments[ 1 ].process()
-      // p.show()
-      for (d of ref) {
-        urge('^97-4^', d);
-      }
+      // urge '^97-4^', d for d from p.walk()
+      urge('^97-4^', p.run());
       return null;
     })();
   }
