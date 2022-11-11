c8d1dfa2d59203b8f794105e3135dece493d137b
loveencounterflow <mail@somewhere.com>
2022-11-11 22:36:46 +0100

    [moonriver] update

--- dev/moonriver/lib/demo-ng.js
+++ dev/moonriver/lib/demo-ng.js
@@ -185,16 +185,21 @@
 
   //-----------------------------------------------------------------------------------------------------------
   demo_5 = async function() {
-    var Async_pipeline, FS, p, show_2;
+    var Async_pipeline, FS, T, p, show_2;
     echo('—————————————————————————————————————————————');
     FS = require('node:fs');
-    ({Async_pipeline} = require('../../../apps/moonriver'));
+    ({
+      Async_pipeline,
+      transforms: T
+    } = require('../../../apps/moonriver'));
     p = new Async_pipeline();
-    p.push(FS.createReadStream(__filename));
+    p.push(FS.createReadStream(__filename, {
+      highWaterMark: 50
+    }));
+    p.push(T.$split_lines());
     p.push(show_2 = function(d) {
       return whisper('Ⅱ', rpr(d));
     });
-    // p.push show_2 = ( d ) -> whisper 'Ⅲ', rpr d
     info('^24-7^', p);
     await p.run();
     return null;
@@ -203,36 +208,12 @@
   //###########################################################################################################
   if (module === require.main) {
     (async() => {
-      var FS, Receiver, SL, ctx, d, line, rcv, ref, source;
-      // demo_1()
-      // demo_2()
-      // await demo_3a()
-      // await demo_3b()
-      // demo_4()
-      // demo_5()
-      ({Receiver} = require('../../../apps/jfee'));
-      SL = require('../../../apps/intertext-splitlines');
-      FS = require('node:fs');
-      source = FS.createReadStream(__filename, {
-        highWaterMark: 10 //, { encoding: 'utf-8', }
-      });
-      rcv = Receiver.from_readstream(source, {
-        bare: true
-      });
-      ctx = SL.new_context();
-      // # source.on 'readable', -> debug '^29-2^', rpr source.read 10
-      // source.on 'data',   ( data ) -> debug '^29-3^', 'data'; debug '^29-4^', rpr data[ .. 100 ]
-      // source.on 'end',    -> debug '^29-5^', 'end';   @has_finished = true
-      // source.on 'close',  -> debug '^29-6^', 'close'; @has_finished = true
-      debug('^29-6^', rcv);
-      for await (d of rcv) {
-        debug('^29-6^', '.');
-        ref = SL.walk_lines(ctx, d);
-        for await (line of ref) {
-          debug(rpr(line)); // .toString()
-        }
-      }
-      return null;
+      demo_1();
+      demo_2();
+      await demo_3a();
+      await demo_3b();
+      demo_4();
+      return demo_5();
     })();
   }
 
