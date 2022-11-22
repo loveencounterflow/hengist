bc63339c345452d0e78fe158a37489ffa77021e9
loveencounterflow <mail@somewhere.com>
2022-11-22 20:57:26 +0100

    [moonriver] add tests for multi-pipeline processing

---
+++ dev/moonriver/lib/test-multi-pipeline-processing.js
@@ -0,0 +1,99 @@
+(function() {
+  'use strict';
+  var GUY, H, alert, debug, echo, equals, guy, help, info, inspect, isa, log, plain, praise, rpr, test, type_of, types, urge, validate, warn, whisper,
+    modulo = function(a, b) { return (+a % (b = +b) + b) % b; };
+
+  //###########################################################################################################
+  GUY = require('guy');
+
+  ({alert, debug, help, info, plain, praise, urge, warn, whisper} = GUY.trm.get_loggers('MOONRIVER/TESTS/MULTI-PIPELINE-PROCESSING'));
+
+  ({rpr, inspect, echo, log} = GUY.trm);
+
+  //...........................................................................................................
+  test = require('../../../apps/guy-test');
+
+  types = new (require('../../../apps/intertype')).Intertype();
+
+  ({isa, equals, type_of, validate} = types);
+
+  guy = require('../../../apps/guy');
+
+  H = require('../../../lib/helpers');
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.walk_named_pipelines = function(T, done) {
+    var $, Pipeline, first, get_pipelines, last;
+    ({Pipeline, $} = require('../../../apps/moonriver'));
+    first = Symbol('first');
+    last = Symbol('last');
+    //.........................................................................................................
+    get_pipelines = function() {
+      var p_1, p_2, show;
+      p_1 = new Pipeline();
+      p_2 = new Pipeline();
+      p_1.push([0, 1, 2, 3, 4, 5]);
+      p_1.push($({first, last}, function(d, send) {
+        return send(d);
+      }));
+      p_1.push(show = function(d) {
+        return whisper('input', d);
+      });
+      p_1.push((function() {
+        var count;
+        count = 0;
+        return function(d, send) {
+          count++;
+          if (modulo(count, 2) === 0) {
+            return p_2.send(d);
+          }
+          return send(d);
+        };
+      })());
+      p_1.push(show = function(d) {
+        return urge('p_1', d);
+      });
+      p_2.push(show = function(d) {
+        return warn('p_2', d);
+      });
+      return {p_1, p_2};
+    };
+    (function() {      //.........................................................................................................
+      var d, p_1, p_2, ref, result;
+      ({p_1, p_2} = get_pipelines());
+      result = {
+        even: [],
+        odd: []
+      };
+      ref = Pipeline.walk_named_pipelines({
+        odd: p_1,
+        even: p_2
+      });
+      for (d of ref) {
+        info(d);
+        result[d.name].push(d.data);
+      }
+      if (T != null) {
+        T.eq(result, {
+          even: [0, 2, 4, last],
+          odd: [first, 1, 3, 5]
+        });
+      }
+      return null;
+    })();
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //###########################################################################################################
+  if (require.main === module) {
+    (() => {
+      // @walk_named_pipelines_1()
+      // @walk_named_pipelines()
+      // test @walk_named_pipelines
+      return test(this);
+    })();
+  }
+
+}).call(this);
+
+//# sourceMappingURL=test-multi-pipeline-processing.js.map
---
+++ dev/moonriver/src/test-multi-pipeline-processing.coffee
@@ -0,0 +1,71 @@
+
+'use strict'
+
+
+############################################################################################################
+GUY                       = require 'guy'
+{ alert
+  debug
+  help
+  info
+  plain
+  praise
+  urge
+  warn
+  whisper }               = GUY.trm.get_loggers 'MOONRIVER/TESTS/MULTI-PIPELINE-PROCESSING'
+{ rpr
+  inspect
+  echo
+  log     }               = GUY.trm
+#...........................................................................................................
+test                      = require '../../../apps/guy-test'
+types                     = new ( require '../../../apps/intertype' ).Intertype
+{ isa
+  equals
+  type_of
+  validate              } = types
+guy                       = require '../../../apps/guy'
+H                         = require '../../../lib/helpers'
+
+
+#-----------------------------------------------------------------------------------------------------------
+@walk_named_pipelines = ( T, done ) ->
+  { Pipeline, \
+    $,              } = require '../../../apps/moonriver'
+  first               = Symbol 'first'
+  last                = Symbol 'last'
+  #.........................................................................................................
+  get_pipelines = ->
+    p_1                 = new Pipeline()
+    p_2                 = new Pipeline()
+    p_1.push [ 0 .. 5 ]
+    p_1.push $ { first, last, }, ( d, send ) -> send d
+    p_1.push show = ( d ) -> whisper 'input', d
+    p_1.push do ->
+      count = 0
+      return ( d, send ) ->
+        count++
+        if count %% 2 is 0 then return p_2.send d
+        send d
+    p_1.push show = ( d ) -> urge 'p_1', d
+    p_2.push show = ( d ) -> warn 'p_2', d
+    return { p_1, p_2, }
+  #.........................................................................................................
+  do ->
+    { p_1, p_2, } = get_pipelines()
+    result        = { even: [], odd: [], }
+    for d from Pipeline.walk_named_pipelines { odd: p_1, even: p_2, }
+      info d
+      result[ d.name ].push d.data
+    T?.eq result, { even: [ 0, 2, 4, last ], odd: [ first, 1, 3, 5 ] }
+    return null
+  #.........................................................................................................
+  done?()
+
+
+############################################################################################################
+if require.main is module then do =>
+  # @walk_named_pipelines_1()
+  # @walk_named_pipelines()
+  # test @walk_named_pipelines
+  test @
