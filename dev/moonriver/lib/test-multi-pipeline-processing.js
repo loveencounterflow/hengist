be287872c7f9a56a68a667c4a958ea90f3a779c0
loveencounterflow <mail@somewhere.com>
2022-11-23 08:21:18 +0100

    [moonriver] update

--- dev/moonriver/lib/test-multi-pipeline-processing.js
+++ dev/moonriver/lib/test-multi-pipeline-processing.js
@@ -84,13 +84,84 @@
     return typeof done === "function" ? done() : void 0;
   };
 
+  //-----------------------------------------------------------------------------------------------------------
+  this.async_walk_named_pipelines = async function(T, done) {
+    var $, Async_pipeline, defer, first, get_pipelines, last;
+    ({Async_pipeline, $} = require('../../../apps/moonriver'));
+    first = Symbol('first');
+    last = Symbol('last');
+    ({defer} = GUY.async);
+    //.........................................................................................................
+    get_pipelines = function() {
+      var p_1, p_2, show;
+      p_1 = new Async_pipeline();
+      p_2 = new Async_pipeline();
+      p_1.push([0, 1, 2, 3, 4, 5]);
+      p_1.push(show = function(d) {
+        return whisper('input', d);
+      });
+      p_1.push($({first, last}, async function(d, send) {
+        return (await defer(function() {
+          return send(d);
+        }));
+      }));
+      p_1.push(show = function(d) {
+        return whisper('input', d);
+      });
+      p_1.push((function() {
+        var count;
+        count = 0;
+        return async function(d, send) {
+          return (await defer(function() {
+            count++;
+            if (modulo(count, 2) === 0) {
+              return p_2.send(d);
+            }
+            return send(d);
+          }));
+        };
+      })());
+      p_1.push(show = function(d) {
+        return urge('p_1', d);
+      });
+      p_2.push(show = function(d) {
+        return warn('p_2', d);
+      });
+      return {p_1, p_2};
+    };
+    await (async function() {      //.........................................................................................................
+      var d, p_1, p_2, ref, result;
+      ({p_1, p_2} = get_pipelines());
+      result = {
+        even: [],
+        odd: []
+      };
+      ref = Async_pipeline.walk_named_pipelines({
+        odd: p_1,
+        even: p_2
+      });
+      for await (d of ref) {
+        info(d);
+        result[d.name].push(d.data);
+      }
+      if (T != null) {
+        T.eq(result, {
+          even: [0, 2, 4, last],
+          odd: [first, 1, 3, 5]
+        });
+      }
+      return null;
+    })();
+    return typeof done === "function" ? done() : void 0;
+  };
+
   //###########################################################################################################
   if (require.main === module) {
-    (() => {
+    (async() => {
       // @walk_named_pipelines_1()
-      // @walk_named_pipelines()
+      // await @async_walk_named_pipelines()
       // test @walk_named_pipelines
-      return test(this);
+      return (await test(this));
     })();
   }
 
