eb5940d164f52ed7e5e3fc14a2a74a7dc30938f3
loveencounterflow <mail@somewhere.com>
2022-12-29 18:51:22 +0100

    [datamill-v2] update

--- dev/datamill-v2/lib/test-document.js
+++ dev/datamill-v2/lib/test-document.js
@@ -215,6 +215,74 @@
     return typeof done === "function" ? done() : void 0;
   };
 
+  //-----------------------------------------------------------------------------------------------------------
+  this.doc_walk_concatenated_lines_of_files = function(T, done) {
+    var Document, SQL;
+    ({SQL} = require('../../../apps/dbay'));
+    ({Document} = require('../../../apps/datamill-v2/lib/document'));
+    //.........................................................................................................
+    GUY.temp.with_directory(function({
+        path: home_parent
+      }) {
+      var doc, doc_file_id, doc_file_path, file, files, home, i, len, result, source_path, target_path;
+      home = PATH.resolve(home_parent, 'dmd');
+      FS.mkdirSync(home);
+      doc = new Document({home});
+      result = [];
+      debug('^34-5^', {doc});
+      files = [
+        {
+          doc_file_id: '3p',
+          doc_file_path: 'datamill/three-paragraphs.txt'
+        },
+        {
+          doc_file_id: '3n',
+          doc_file_path: 'datamill/file-with-3-lines-no-eofnl.txt'
+        },
+        {
+          doc_file_id: '1n',
+          doc_file_path: 'datamill/file-with-single-nl.txt'
+        }
+      ];
+      for (i = 0, len = files.length; i < len; i++) {
+        ({doc_file_id, doc_file_path} = files[i]);
+        source_path = PATH.resolve(__dirname, '../../../assets/', doc_file_path);
+        doc_file_path = PATH.basename(doc_file_path);
+        target_path = PATH.resolve(home, doc_file_path);
+        FS.cpSync(source_path, target_path);
+        file = doc.add_file({doc_file_id, doc_file_path});
+        result.push(file);
+      }
+      (function() {
+        var matcher;
+        doc._delete_file('layout');
+        matcher = doc.db.all_rows(SQL`select
+    dense_rank() over w as doc_file_nr,
+    *
+  from doc_raw_lines
+  window w as ( order by doc_file_id )
+  order by doc_file_id, doc_line_nr;`);
+        H.tabulate("matcher", matcher);
+        return T != null ? T.eq([...doc.walk_raw_lines()], matcher) : void 0;
+      })();
+      (function() {
+        var matcher;
+        matcher = doc.db.all_rows(SQL`select 1 as doc_file_nr, * from doc_raw_lines where doc_file_id = '1n'
+union all
+select 2 as doc_file_nr, * from doc_raw_lines where doc_file_id = '3n'
+union all
+select 3 as doc_file_nr, * from doc_raw_lines where doc_file_id = '3p'
+order by doc_file_nr, doc_line_nr;`);
+        urge('^9856^', matcher);
+        H.tabulate("matcher", matcher);
+        return T != null ? T.eq([...(doc.walk_raw_lines('1n', '3n', '3p'))], matcher) : void 0;
+      })();
+      //.......................................................................................................
+      return null;
+    });
+    return typeof done === "function" ? done() : void 0;
+  };
+
   //###########################################################################################################
   if (require.main === module) {
     (() => {
@@ -227,9 +295,12 @@
       // test @doc_add_and_read_file
       // @doc_paragraphs()
       // test @doc_paragraphs
-      return test(this);
+      this.doc_walk_concatenated_lines_of_files();
+      return test(this.doc_walk_concatenated_lines_of_files);
     })();
   }
+
+  // test @
 
 }).call(this);
 
