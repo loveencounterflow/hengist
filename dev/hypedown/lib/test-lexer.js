20d28b206e80f76a9c6b8ac2b46c657626939593
loveencounterflow <mail@somewhere.com>
2023-03-27 12:12:29 +0200

    [hypedown] update

--- dev/hypedown/lib/test-lexer.js
+++ dev/hypedown/lib/test-lexer.js
@@ -64,38 +64,28 @@
   this.lex_codespans_and_single_star = async function(T, done) {
     var Hypedown_lexer, error, i, len, matcher, probe, probes_and_matchers;
     ({Hypedown_lexer} = require('../../../apps/hypedown'));
-    probes_and_matchers = [["`abc`", "<code>abc</code>\n"]];
+    probes_and_matchers = [['`', "c:$border'',c:start'`',c:nl'\\n'", null], ['``', "c:$border'',c:start'``',c:nl'\\n'", null], ['``````', "c:$border'',c:start'``````',c:nl'\\n'", null], ['`abc`', "c:$border'',c:start'`',c:text'abc',c:stop'`',p:$border'',p:nl'\\n'", null], ['*abc*', "p:other'*abc*',p:nl'\\n'", null], ['*foo* `*bar*` baz', "p:other'*foo* ',c:$border'',c:start'`',c:text'*bar*',c:stop'`',p:$border'',p:ws' ',p:other'baz',p:nl'\\n'", null], ['*foo* ``*bar*`` baz', "p:other'*foo* ',c:$border'',c:start'``',c:text'*bar*',c:stop'``',p:$border'',p:ws' ',p:other'baz',p:nl'\\n'", null], ['*foo* ````*bar*```` baz', "p:other'*foo* ',c:$border'',c:start'````',c:text'*bar*',c:stop'````',p:$border'',p:ws' ',p:other'baz',p:nl'\\n'", null], ['helo `world`!', "p:other'helo ',c:$border'',c:start'`',c:text'world',c:stop'`',p:$border'',p:other'!',p:nl'\\n'", null], ['foo\n\nbar\n\nbaz', "p:other'foo',p:nl'\\n',p:nl'\\n',p:other'bar',p:nl'\\n',p:nl'\\n',p:other'baz',p:nl'\\n'", null], ['*foo* ``*bar*``` baz', "p:other'*foo* ',c:$border'',c:start'``',c:text'*bar*',c:text'```',c:text' baz',c:nl'\\n'", null], ['*foo* ```*bar*`` baz', "p:other'*foo* ',c:$border'',c:start'```',c:text'*bar*',c:text'``',c:text' baz',c:nl'\\n'", null], ['abc ``d e f g\nh`` xyz', "p:other'abc ',c:$border'',c:start'``',c:text'd e f g',c:nl'\\n',c:text'h',c:stop'``',p:$border'',p:ws' ',p:other'xyz',p:nl'\\n'", null]];
 //.........................................................................................................
-// [ "*abc*", "<i>abc</i>\n", ]
-// [ '*foo* `*bar*` baz', '<i>foo</i> <code>*bar*</code> baz\n', null ]
-// [ '*foo* ``*bar*`` baz', '<i>foo</i> <code>*bar*</code> baz\n', null ]
-// [ '*foo* ````*bar*```` baz', '<i>foo</i> <code>*bar*</code> baz\n', null ]
-// [ 'helo `world`!', 'helo <code>world</code>!\n', null ]
-// [ 'foo\n\nbar\n\nbaz', 'foo\n\nbar\n\nbaz\n', null ]
-// [ '*foo* ``*bar*``` baz', '<i>foo</i> <code>*bar*</code> baz\n', null ] ### TAINT preliminary, lack of STOP token ###
-// [ '*foo* ```*bar*`` baz', '<i>foo</i> <code>*bar*`` baz</code>\n', null ] ### TAINT preliminary, lack of STOP token ###
     for (i = 0, len = probes_and_matchers.length; i < len; i++) {
       [probe, matcher, error] = probes_and_matchers[i];
       await T.perform(probe, matcher, error, function() {
         return new Promise(function(resolve, reject) {
-          var d, lexer, ref, result, t, tokens;
+          var d, lexer, ref, result, short_tokens, tokens;
           lexer = new Hypedown_lexer();
           tokens = [];
+          short_tokens = [];
           ref = lexer.walk(probe);
           for (d of ref) {
             tokens.push(d);
-          }
-          result = ((function() {
-            var j, len1, results;
-            results = [];
-            for (j = 0, len1 = tokens.length; j < len1; j++) {
-              t = tokens[j];
-              if (t.mk !== 'plain:nl') {
-                results.push(`${t.mk}${rpr(t.value)}`);
-              }
+            switch (d.mode) {
+              case 'plain':
+                short_tokens.push(`p:${d.tid}${rpr(d.value)}`);
+                break;
+              case 'cspan':
+                short_tokens.push(`c:${d.tid}${rpr(d.value)}`);
             }
-            return results;
-          })()).join(',');
+          }
+          result = short_tokens.join(',');
           // H.tabulate "#{rpr probe} -> #{rpr result}", tokens
           // H.tabulate "#{rpr probe} -> #{rpr result}", ( t for t in tokens when not t.$stamped )
           return resolve(result);
