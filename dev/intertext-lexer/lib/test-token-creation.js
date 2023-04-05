e111fc0246a6dac67d6c603f2731869f1875ea33
loveencounterflow <mail@somewhere.com>
2023-04-05 10:32:53 +0200

    [intertext-lexer] add tests for token creation from dataclass

---
+++ dev/intertext-lexer/lib/test-token-creation.js
@@ -0,0 +1,103 @@
+(function() {
+  'use strict';
+  var DATOM, GUY, H, H2, PATH, SQL, after, alert, debug, echo, equals, guy, help, info, inspect, isa, lets, log, new_datom, plain, praise, rpr, stamp, test, type_of, types, urge, validate, validate_list_of, warn, whisper,
+    indexOf = [].indexOf;
+
+  //###########################################################################################################
+  GUY = require('guy');
+
+  ({alert, debug, help, info, plain, praise, urge, warn, whisper} = GUY.trm.get_loggers('INTERTEXT-LEXER/TESTS/BASICS'));
+
+  ({rpr, inspect, echo, log} = GUY.trm);
+
+  //...........................................................................................................
+  test = require('../../../apps/guy-test');
+
+  PATH = require('path');
+
+  // FS                        = require 'fs'
+  types = new (require('intertype')).Intertype();
+
+  ({isa, equals, type_of, validate, validate_list_of} = types.export());
+
+  SQL = String.raw;
+
+  guy = require('../../../apps/guy');
+
+  H = require('../../../lib/helpers');
+
+  H2 = require('./helpers');
+
+  after = (dts, f) => {
+    return new Promise(function(resolve) {
+      return setTimeout((function() {
+        return resolve(f());
+      }), dts * 1000);
+    });
+  };
+
+  ({DATOM} = require('../../../apps/datom'));
+
+  ({new_datom, lets, stamp} = DATOM);
+
+  //===========================================================================================================
+  // TESTS
+  //-----------------------------------------------------------------------------------------------------------
+  this.token_creation_with_dataclass = function(T, done) {
+    var Interlex, lexer;
+    // T?.halt_on_error()
+    ({Interlex} = require('../../../apps/intertext-lexer'));
+    lexer = new Interlex();
+    (function() {      //.........................................................................................................
+      var error;
+      if (T != null) {
+        T.ok(indexOf.call(Object.keys(lexer.types.registry), 'Token') >= 0);
+      }
+      try {
+        lexer.types.create.Token();
+      } catch (error1) {
+        error = error1;
+        warn('^93-1^', GUY.trm.reverse(error.message));
+      }
+      if (T != null) {
+        T.throws(/not a valid/, function() {
+          return lexer.types.create.Token();
+        });
+      }
+      return null;
+    })();
+    (function() {      //.........................................................................................................
+      var d, e;
+      debug('^93-1^', lexer.types.create.Token({
+        $key: 'foo:bar'
+      }));
+      d = lexer.types.create.Token({
+        $key: 'plain:p:start',
+        lnr1: 123
+      });
+      e = d.set_mode('tag');
+      debug('^93-1^', d);
+      debug('^93-1^', e);
+      return null;
+    })();
+    if (typeof done === "function") {
+      done();
+    }
+    return null;
+  };
+
+  //###########################################################################################################
+  if (require.main === module) {
+    (() => {
+      // test @lex_tags
+      return test(this);
+    })();
+  }
+
+  // @token_creation_with_dataclass()
+// test @lex_tags_with_rpr
+// test @parse_md_stars_markup
+
+}).call(this);
+
+//# sourceMappingURL=test-token-creation.js.map
---
+++ dev/intertext-lexer/src/test-token-creation.coffee
@@ -0,0 +1,76 @@
+
+'use strict'
+
+
+############################################################################################################
+GUY                       = require 'guy'
+{ alert
+  debug
+  help
+  info
+  plain
+  praise
+  urge
+  warn
+  whisper }               = GUY.trm.get_loggers 'INTERTEXT-LEXER/TESTS/BASICS'
+{ rpr
+  inspect
+  echo
+  log     }               = GUY.trm
+#...........................................................................................................
+test                      = require '../../../apps/guy-test'
+PATH                      = require 'path'
+# FS                        = require 'fs'
+types                     = new ( require 'intertype' ).Intertype
+{ isa
+  equals
+  type_of
+  validate
+  validate_list_of }      = types.export()
+SQL                       = String.raw
+guy                       = require '../../../apps/guy'
+H                         = require '../../../lib/helpers'
+H2                        = require './helpers'
+after                     = ( dts, f  ) => new Promise ( resolve ) -> setTimeout ( -> resolve f() ), dts * 1000
+{ DATOM }                 = require '../../../apps/datom'
+{ new_datom
+  lets
+  stamp     }             = DATOM
+
+
+
+
+#===========================================================================================================
+# TESTS
+#-----------------------------------------------------------------------------------------------------------
+@token_creation_with_dataclass = ( T, done ) ->
+  # T?.halt_on_error()
+  { Interlex } = require '../../../apps/intertext-lexer'
+  lexer = new Interlex()
+  #.........................................................................................................
+  do ->
+    T?.ok 'Token' in Object.keys lexer.types.registry
+    try lexer.types.create.Token() catch error then warn '^93-1^', GUY.trm.reverse error.message
+    T?.throws /not a valid/, -> lexer.types.create.Token()
+    return null
+  #.........................................................................................................
+  do ->
+    debug '^93-1^', lexer.types.create.Token { $key: 'foo:bar', }
+    d = lexer.types.create.Token { $key: 'plain:p:start', lnr1: 123, }
+    e = d.set_mode 'tag'
+    debug '^93-1^', d
+    debug '^93-1^', e
+    return null
+  #.........................................................................................................
+  done?()
+  return null
+
+############################################################################################################
+if require.main is module then do =>
+  # test @lex_tags
+  test @
+  # @token_creation_with_dataclass()
+  # test @lex_tags_with_rpr
+  # test @parse_md_stars_markup
+
+
