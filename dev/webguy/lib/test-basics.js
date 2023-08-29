8b2ca9f3deb2f9d834341525e7c986ff69993803
loveencounterflow <mail@somewhere.com>
2023-08-29 14:20:52 +0200

    [hengist webguy] start tests for `webguy`

---
+++ dev/webguy/lib/test-basics.js
@@ -0,0 +1,65 @@
+(function() {
+  'use strict';
+  var CND, badge, debug, echo, help, info, jr, rpr, test, urge, warn, whisper;
+
+  //###########################################################################################################
+  CND = require('cnd');
+
+  rpr = CND.rpr;
+
+  badge = 'WEBGUY/TESTS';
+
+  debug = CND.get_logger('debug', badge);
+
+  warn = CND.get_logger('warn', badge);
+
+  info = CND.get_logger('info', badge);
+
+  urge = CND.get_logger('urge', badge);
+
+  help = CND.get_logger('help', badge);
+
+  whisper = CND.get_logger('whisper', badge);
+
+  echo = CND.echo.bind(CND);
+
+  //...........................................................................................................
+  test = require('guy-test');
+
+  jr = JSON.stringify;
+
+  // #-----------------------------------------------------------------------------------------------------------
+  // @[ "_XEMITTER: _" ] = ( T, done ) ->
+  //   { DATOM }                 = require '../../../apps/datom'
+  //   { new_datom
+  //     select }                = DATOM
+  // { Djehuti }               = require '../../../apps/intertalk'
+  //   #.........................................................................................................
+  //   probes_and_matchers = [
+  //     [['^foo', { time: 1500000, value: "msg#1", }],{"time":1500000,"value":"msg#1","$key":"^foo"},null]
+  //     ]
+  //   #.........................................................................................................
+  //   for [ probe, matcher, error, ] in probes_and_matchers
+  //     await T.perform probe, matcher, error, -> return new Promise ( resolve, reject ) ->
+  //       [ key, value, ] = probe
+  //       resolve new_datom key, value
+  //   done()
+  //   return null
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.first = function(T, done) {
+    //.........................................................................................................
+    done();
+    return null;
+  };
+
+  //###########################################################################################################
+  if (require.main === module) {
+    (() => {
+      return test(this);
+    })();
+  }
+
+}).call(this);
+
+//# sourceMappingURL=test-basics.js.map
---
+++ dev/webguy/src/test-basics.coffee
@@ -0,0 +1,52 @@
+
+
+'use strict'
+
+
+############################################################################################################
+CND                       = require 'cnd'
+rpr                       = CND.rpr
+badge                     = 'WEBGUY/TESTS'
+debug                     = CND.get_logger 'debug',     badge
+warn                      = CND.get_logger 'warn',      badge
+info                      = CND.get_logger 'info',      badge
+urge                      = CND.get_logger 'urge',      badge
+help                      = CND.get_logger 'help',      badge
+whisper                   = CND.get_logger 'whisper',   badge
+echo                      = CND.echo.bind CND
+#...........................................................................................................
+test                      = require 'guy-test'
+jr                        = JSON.stringify
+
+# #-----------------------------------------------------------------------------------------------------------
+# @[ "_XEMITTER: _" ] = ( T, done ) ->
+#   { DATOM }                 = require '../../../apps/datom'
+#   { new_datom
+#     select }                = DATOM
+  # { Djehuti }               = require '../../../apps/intertalk'
+#   #.........................................................................................................
+#   probes_and_matchers = [
+#     [['^foo', { time: 1500000, value: "msg#1", }],{"time":1500000,"value":"msg#1","$key":"^foo"},null]
+#     ]
+#   #.........................................................................................................
+#   for [ probe, matcher, error, ] in probes_and_matchers
+#     await T.perform probe, matcher, error, -> return new Promise ( resolve, reject ) ->
+#       [ key, value, ] = probe
+#       resolve new_datom key, value
+#   done()
+#   return null
+
+#-----------------------------------------------------------------------------------------------------------
+@first = ( T, done ) ->
+  #.........................................................................................................
+  done()
+  return null
+
+
+
+############################################################################################################
+if require.main is module then do =>
+  test @
+
+
+
