2f204a097fb0846c0f619281cfbb3558450125c4
loveencounterflow <mail@somewhere.com>
2023-09-12 11:37:04 +0200

    [webguy] update

--- dev/webguy/lib/test-basics.js
+++ dev/webguy/lib/test-basics.js
@@ -1,32 +1,22 @@
 (function() {
   'use strict';
-  var CND, badge, debug, echo, help, info, jr, rpr, test, urge, warn, whisper;
+  var GUY, alert, debug, echo, help, info, inspect, isa, jr, log, plain, praise, rpr, test, types, urge, warn, whisper;
 
   //###########################################################################################################
-  CND = require('cnd');
+  GUY = require('guy');
 
-  rpr = CND.rpr;
+  ({alert, debug, help, info, plain, praise, urge, warn, whisper} = GUY.trm.get_loggers('webguy/tests/basics'));
 
-  badge = 'WEBGUY/TESTS';
-
-  debug = CND.get_logger('debug', badge);
-
-  warn = CND.get_logger('warn', badge);
-
-  info = CND.get_logger('info', badge);
-
-  urge = CND.get_logger('urge', badge);
-
-  help = CND.get_logger('help', badge);
-
-  whisper = CND.get_logger('whisper', badge);
-
-  echo = CND.echo.bind(CND);
+  ({rpr, inspect, echo, log} = GUY.trm);
 
   //...........................................................................................................
   test = require('guy-test');
 
   jr = JSON.stringify;
+
+  types = new (require('intertype-newest')).Intertype();
+
+  ({isa} = types);
 
   // #-----------------------------------------------------------------------------------------------------------
   // @[ "_XEMITTER: _" ] = ( T, done ) ->
@@ -176,10 +166,126 @@
     return null;
   };
 
+  //===========================================================================================================
+  // TIME
+  //-----------------------------------------------------------------------------------------------------------
+  this.time_exports = function(T, done) {
+    var WGUY;
+    WGUY = require('../../../apps/webguy');
+    //.........................................................................................................
+    if (T != null) {
+      T.ok(WGUY.time instanceof WGUY.time.Time);
+    }
+    if (T != null) {
+      T.ok(isa.function(WGUY.time.stamp_f));
+    }
+    if (T != null) {
+      T.ok(isa.function(WGUY.time.stamp_s));
+    }
+    if (T != null) {
+      T.ok(isa.function(WGUY.time.monostamp_f2));
+    }
+    if (T != null) {
+      T.ok(isa.function(WGUY.time.monostamp_s2));
+    }
+    if (T != null) {
+      T.ok(isa.function(WGUY.time.monostamp_s1));
+    }
+    if (typeof done === "function") {
+      done();
+    }
+    return null;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.time_datatypes = async function(T, done) {
+    var WGUY, delta_ms, stamp_f_matcher, stamp_f_result;
+    WGUY = require('../../../apps/webguy');
+    //.........................................................................................................
+    if (T != null) {
+      T.ok(isa.float(WGUY.time.stamp_f()));
+    }
+    if (T != null) {
+      T.ok(isa.nonempty.text(WGUY.time.stamp_s(0)));
+    }
+    if (T != null) {
+      T.ok(isa.nonempty.text(WGUY.time.stamp_s()));
+    }
+    if (T != null) {
+      T.ok(isa.list.of.float(WGUY.time.monostamp_f2()));
+    }
+    if (T != null) {
+      T.eq(WGUY.time.monostamp_f2().length, 2);
+    }
+    if (T != null) {
+      T.eq(WGUY.time.stamp_s(0), '0000000000000.000');
+    }
+    if (T != null) {
+      T.eq(WGUY.time.monostamp_s1(45678), '0000000045678.000:000');
+    }
+    if (T != null) {
+      T.eq(WGUY.time.monostamp_s1(45678, 123), '0000000045678.000:123');
+    }
+    if (T != null) {
+      T.eq(WGUY.time.monostamp_s2(45678), ['0000000045678.000', '000']);
+    }
+    if (T != null) {
+      T.eq(WGUY.time.monostamp_s2(45678, 123), ['0000000045678.000', '123']);
+    }
+    stamp_f_matcher = Date.now();
+    delta_ms = 500;
+    await GUY.async.sleep(delta_ms / 1000);
+    stamp_f_result = WGUY.time.monostamp_f2()[0];
+    debug('^223^', {stamp_f_matcher, stamp_f_result});
+    if (T != null) {
+      T.ok((stamp_f_matcher - delta_ms * 2 < stamp_f_result && stamp_f_result < stamp_f_matcher + delta_ms * 2));
+    }
+    if (typeof done === "function") {
+      done();
+    }
+    return null;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.time_stamp = function(T, done) {
+    var WGUY, ts_f, ts_s_1, ts_s_2;
+    WGUY = require('../../../apps/webguy');
+    //.........................................................................................................
+    debug('^2342^', ts_f = WGUY.time.stamp_f());
+    debug('^2342^', ts_s_1 = WGUY.time.stamp_s(ts_f));
+    debug('^2342^', ts_s_2 = WGUY.time.stamp_s());
+    if (T != null) {
+      T.eq((ts_f.toFixed(3)) === ts_s_1);
+    }
+    if (typeof done === "function") {
+      done();
+    }
+    return null;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.time_monostamp = function(T, done) {
+    var WGUY, ts_f;
+    WGUY = require('../../../apps/webguy');
+    //.........................................................................................................
+    debug('^2342^', ts_f = WGUY.time.monostamp_f2());
+    debug('^2342^', ts_f = WGUY.time.monostamp_s2());
+    debug('^2342^', ts_f = WGUY.time.monostamp_s1());
+    debug('^2342^', ts_f = WGUY.time.monostamp_s1(':'));
+    if (typeof done === "function") {
+      done();
+    }
+    return null;
+  };
+
   //###########################################################################################################
   if (require.main === module) {
     (() => {
-      return test(this);
+      // test @
+      // test @time_exports
+      // @time_stamp()
+      // @time_monostamp()
+      return test(this.time_datatypes);
     })();
   }
 
