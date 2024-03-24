606145d4db5da94cf6520f2fef9aafcd9e89c13c
loveencounterflow <flow@home>
2024-03-24 16:16:17 +0100

    [snippets] update

--- dev/snippets/lib/namespaces-with-hubs.js
+++ dev/snippets/lib/namespaces-with-hubs.js
@@ -1,6 +1,6 @@
 (async function() {
   'use strict';
-  var GUY, Host, alert, create_subsidiary, create_ties, debug, echo, get_host, help, hosts, info, inspect, is_subsidiary, log, plain, praise, rpr, subsidiaries, urge, walk_subsidiaries, warn, whisper;
+  var GUY, Host, SUBSIDIARY, Subsidiary_helpers, alert, debug, echo, help, info, inspect, log, plain, praise, rpr, urge, warn, whisper;
 
   //===========================================================================================================
   GUY = require('guy');
@@ -23,84 +23,89 @@
 
    */
   //===========================================================================================================
-  subsidiaries = new WeakSet();
+  Subsidiary_helpers = class Subsidiary_helpers {
+    //---------------------------------------------------------------------------------------------------------
+    constructor() {
+      this.subsidiaries = new WeakSet();
+      this.hosts = new WeakMap();
+    }
 
-  hosts = new WeakMap();
+    //---------------------------------------------------------------------------------------------------------
+    * walk_subsidiaries(host) {
+      var results, subsidiary, subsidiary_key;
+      results = [];
+      for (subsidiary_key in host) {
+        subsidiary = host[subsidiary_key];
+        if (this.is_subsidiary(subsidiary)) {
+          /* TAINT this loop should be changed so we catch all relevant objects, including from inherited classes */
+          results.push((yield {subsidiary_key, subsidiary}));
+        }
+      }
+      return results;
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    create(subsidiary) {
+      if (this.subsidiaries.has(subsidiary)) {
+        throw new Error("object already in use as subsidiary");
+      }
+      this.subsidiaries.add(subsidiary);
+      return subsidiary;
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    /* TAINT safeguard against non-object values */
+    is_subsidiary(x) {
+      return this.subsidiaries.has(x);
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    tie_host_and_subsidiary(cfg) {
+      /* TAINT use types, validate */
+      var template;
+      template = {
+        host: null,
+        subsidiary: null,
+        subsidiary_key: '$',
+        host_key: '_'
+      };
+      cfg = {...template, ...cfg};
+      debug('^340-1^', cfg);
+      /* TAINT shouldn't be necessary if done explicitly? */
+      if (!this.subsidiaries.has(cfg.subsidiary)) {
+        throw new Error("object isn't a subsidiary");
+      }
+      if (this.hosts.has(cfg.subsidiary)) {
+        throw new Error("subsidiary already has a host");
+      }
+      /* host->subsidiary is a standard containment/compository relationship and is expressed directly;
+         subsidiary-> host is a backlink that would create a circular reference which we avoid by using a
+         `WeakMap` instance, `@hosts`: */
+      Object.defineProperty(cfg.host, cfg.subsidiary_key, {
+        value: cfg.subsidiary
+      });
+      Object.defineProperty(cfg.subsidiary, cfg.host_key, {
+        get: () => {
+          return this.get_host(cfg.subsidiary);
+        }
+      });
+      this.hosts.set(cfg.subsidiary, cfg.host);
+      return cfg.subsidiary;
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    get_host(subsidiary) {
+      var R;
+      if ((R = this.hosts.get(subsidiary)) != null) {
+        return R;
+      }
+      throw new Error("no host registered for object");
+    }
+
+  };
 
   //===========================================================================================================
-  // class ???
-
-  //-----------------------------------------------------------------------------------------------------------
-  walk_subsidiaries = function*(host) {
-    var results, subsidiary, subsidiary_key;
-    results = [];
-    for (subsidiary_key in host) {
-      subsidiary = host[subsidiary_key];
-      if (is_subsidiary(subsidiary)) {
-        /* TAINT this loop should be changed so we catch all relevant objects, including from inherited classes */
-        results.push((yield {subsidiary_key, subsidiary}));
-      }
-    }
-    return results;
-  };
-
-  //-----------------------------------------------------------------------------------------------------------
-  create_subsidiary = function(subsidiary) {
-    if (subsidiaries.has(subsidiary)) {
-      throw new Error("object already in use as subsidiary");
-    }
-    subsidiaries.add(subsidiary);
-    return subsidiary;
-  };
-
-  //-----------------------------------------------------------------------------------------------------------
-  /* TAINT safeguard against non-object values */
-  is_subsidiary = function(value) {
-    return subsidiaries.has(value);
-  };
-
-  //-----------------------------------------------------------------------------------------------------------
-  create_ties = function(cfg) {
-    /* TAINT use types, validate */
-    var template;
-    template = {
-      host: null,
-      subsidiary: null,
-      subsidiary_key: '$',
-      host_key: '_'
-    };
-    cfg = {...template, ...cfg};
-    debug('^340-1^', cfg);
-    /* TAINT shouldn't be necessary if done explicitly? */
-    if (!subsidiaries.has(cfg.subsidiary)) {
-      throw new Error("object isn't a subsidiary");
-    }
-    if (hosts.has(cfg.subsidiary)) {
-      throw new Error("subsidiary already has a host");
-    }
-    /* host->subsidiary is a standard containment/compository relationship and is expressed directly;
-     subsidiary-> host is a backlink that would create a circular reference which we avoid by using a
-     `WeakMap` instance, `hosts`: */
-    Object.defineProperty(cfg.host, cfg.subsidiary_key, {
-      value: cfg.subsidiary
-    });
-    Object.defineProperty(cfg.subsidiary, cfg.host_key, {
-      get: function() {
-        return get_host(cfg.subsidiary);
-      }
-    });
-    hosts.set(cfg.subsidiary, cfg.host);
-    return cfg.subsidiary;
-  };
-
-  //-----------------------------------------------------------------------------------------------------------
-  get_host = function(subsidiary) {
-    var R;
-    if ((R = hosts.get(subsidiary)) != null) {
-      return R;
-    }
-    throw new Error("no host registered for object");
-  };
+  SUBSIDIARY = new Subsidiary_helpers();
 
   Host = (function() {
     var B;
@@ -109,17 +114,17 @@
     class Host {
       //---------------------------------------------------------------------------------------------------------
       constructor() {
-        var ref, subsidiary, subsidiary_key, x;
-        ref = walk_subsidiaries(this);
-        for (x of ref) {
-          ({subsidiary_key, subsidiary} = x);
-          create_ties({
+        var ref, subsidiary, subsidiary_key, y;
+        ref = SUBSIDIARY.walk_subsidiaries(this);
+        for (y of ref) {
+          ({subsidiary_key, subsidiary} = y);
+          SUBSIDIARY.tie_host_and_subsidiary({
             host: this,
             subsidiary,
             host_key: '_',
             subsidiary_key
           });
-          debug('^233-1^', subsidiary_key, is_subsidiary(subsidiary), subsidiary._ === this);
+          debug('^233-1^', subsidiary_key, SUBSIDIARY.is_subsidiary(subsidiary), subsidiary._ === this);
           debug('^233-1^', this[subsidiary_key]);
         }
         //   continue unless subsidiary_key.startsWith '$'
@@ -139,7 +144,7 @@
     };
 
     //---------------------------------------------------------------------------------------------------------
-    Host.prototype.$a = create_subsidiary({
+    Host.prototype.$a = SUBSIDIARY.create({
       show: function() {
         warn('^650-1^', "$a.show");
         this._.show();
@@ -148,7 +153,7 @@
     });
 
     //---------------------------------------------------------------------------------------------------------
-    Host.prototype.$b = create_subsidiary(new (B = class B {
+    Host.prototype.$b = SUBSIDIARY.create(new (B = class B {
       show() {
         warn('^650-1^', "$b.show");
         this._.show();
