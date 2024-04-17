599b0a6b1a2a9d6455cf33f657ee931c19d95b20
loveencounterflow <flow@home>
2024-04-17 19:00:44 +0200

    [intertype] re-implementing tests (WIP)

---
+++ dev/intertype/lib/test-basics.js
@@ -0,0 +1,300 @@
+(async function() {
+  'use strict';
+  var GUY, TMP_types, alert, debug, echo, help, info, inspect, log, plain, praise, reverse, rpr, sample_declarations, test, throws, try_and_show, urge, warn, whisper;
+
+  GUY = require('guy');
+
+  ({alert, debug, help, info, plain, praise, urge, warn, whisper} = GUY.trm.get_loggers('intertype'));
+
+  ({rpr, inspect, echo, reverse, log} = GUY.trm);
+
+  test = require('../../../apps/guy-test');
+
+  TMP_types = new (require('intertype')).Intertype();
+
+  //-----------------------------------------------------------------------------------------------------------
+  // s                         = ( name ) -> Symbol.for  name
+  // ps                        = ( name ) -> Symbol      name
+
+  // #-----------------------------------------------------------------------------------------------------------
+  // @[ "_XEMITTER: _" ] = ( T, done ) ->
+  //   { DATOM }                 = require '../../../apps/datom'
+  //   { new_datom
+  //     select }                = DATOM
+  // { Djehuti }               = require '../../../apps/intertalk'
+  //   #.........................................................................................................
+  //   probes_and_matchers = [
+  //     [['^foo', { time: 1500000, value: "msg#1", }],{"time":1500000,"value":"msg#1","$key":"^foo"},null]
+  //     ]
+  //   #.........................................................................................................
+  //   for [ probe, matcher, error, ] in probes_and_matchers
+  //     await T.perform probe, matcher, error, -> return new Promise ( resolve, reject ) ->
+  //       [ key, value, ] = probe
+  //       resolve new_datom key, value
+  //   done()
+  //   return null
+
+  //###########################################################################################################
+
+  // #===========================================================================================================
+  // isa_object = ( x ) -> x? and ( typeof x is 'object' ) and ( ( Object::toString.call x ) is '[object Object]' )
+  // as_object = ( x ) ->
+  //   return x unless isa_object x
+  //   R       = {}
+  //   R[ k ]  = as_object v for k, v of x
+  //   return R
+
+  //===========================================================================================================
+  sample_declarations = {
+    anything: function(x) {
+      return true;
+    },
+    nothing: function(x) {
+      return x == null;
+    },
+    something: function(x) {
+      return x != null;
+    },
+    null: function(x) {
+      return x === null;
+    },
+    undefined: function(x) {
+      return x === void 0;
+    },
+    boolean: function(x) {
+      return (x === true) || (x === false);
+    },
+    function: function(x) {
+      return (Object.prototype.toString.call(x)) === '[object Function]';
+    },
+    asyncfunction: function(x) {
+      return (Object.prototype.toString.call(x)) === '[object AsyncFunction]';
+    },
+    symbol: function(x) {
+      return (typeof x) === 'symbol';
+    },
+    object: function(x) {
+      return (x != null) && (typeof x === 'object') && ((Object.prototype.toString.call(x)) === '[object Object]');
+    },
+    text: function(x) {
+      return (typeof x) === 'string';
+    },
+    nullary: function(x) {
+      return (x != null) && ((x.length === 0) || (x.size === 0));
+    },
+    unary: function(x) {
+      return (x != null) && ((x.length === 1) || (x.size === 1));
+    },
+    binary: function(x) {
+      return (x != null) && ((x.length === 2) || (x.size === 2));
+    },
+    trinary: function(x) {
+      return (x != null) && ((x.length === 3) || (x.size === 3));
+    },
+    unknown: function(x) {
+      return (this.type_of(x)) === 'unknown';
+    }
+  };
+
+  //===========================================================================================================
+  throws = async function(T, matcher, f) {
+    await (async() => {
+      var error, matcher_type, message;
+      error = null;
+      debug('^992-56^', matcher, TMP_types.type_of(matcher));
+      try {
+        await f();
+      } catch (error1) {
+        error = error1;
+        switch (matcher_type = TMP_types.type_of(matcher)) {
+          case 'text':
+            if (T != null) {
+              T.eq(error.message, matcher);
+            }
+            break;
+          case 'regex':
+            matcher.lastIndex = 0;
+            if (matcher.test(error.message)) {
+              null;
+            } else {
+              warn('^992-4^', reverse(message = `error ${rpr(error.message)} doesn't match ${rpr(matcher)}`));
+              T.fail(`^992-4^ ${message}`);
+            }
+            break;
+          default:
+            warn(message = `^992-1^ expected a regex or a text, got a ${matcher_type}`);
+            T.fail(message);
+        }
+      }
+      return T != null ? T.ok(error != null) : void 0;
+    })();
+    return null;
+  };
+
+  //===========================================================================================================
+  try_and_show = function(T, f) {
+    var e, message;
+    e = null;
+    try {
+      urge('^992-2^', f());
+    } catch (error1) {
+      e = error1;
+      help('^992-3^', reverse(`try_and_show: ${rpr(e.message)}`));
+    }
+    if (e == null) {
+      warn('^992-4^', reverse(message = "expected an error but none was thrown"));
+      T.fail("^992-5^ expected an error but none was thrown");
+    }
+    return null;
+  };
+
+  //###########################################################################################################
+
+  //===========================================================================================================
+  this.interface = function(T, done) {
+    var INTERTYPE;
+    INTERTYPE = require('../../../apps/intertype');
+    if (T != null) {
+      T.eq(TMP_types.isa.object(INTERTYPE.types), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.function(INTERTYPE.types.get_isa), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.function(INTERTYPE.types.get_isa_optional), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.function(INTERTYPE.types.get_validate), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.function(INTERTYPE.types.get_validate_optional), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.object(INTERTYPE.types), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.object(INTERTYPE.types.isa), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.object(INTERTYPE.types.isa.optional), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.object(INTERTYPE.types.validate), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.object(INTERTYPE.types.validate.optional), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.function(INTERTYPE.types.isa.boolean), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.function(INTERTYPE.types.isa.optional.boolean), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.function(INTERTYPE.types.validate.boolean), true);
+    }
+    if (T != null) {
+      T.eq(TMP_types.isa.function(INTERTYPE.types.validate.optional.boolean), true);
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.basic_functionality = function(T, done) {
+    var INTERTYPE, types;
+    // T?.halt_on_error()
+    INTERTYPE = require('../../../apps/intertype');
+    types = new INTERTYPE.Intertype(sample_declarations);
+    if (T != null) {
+      T.eq(types.isa.boolean(false), true);
+    }
+    if (T != null) {
+      T.eq(types.isa.boolean(true), true);
+    }
+    if (T != null) {
+      T.eq(types.isa.boolean(null), false);
+    }
+    if (T != null) {
+      T.eq(types.isa.boolean(1), false);
+    }
+    if (T != null) {
+      T.eq(types.isa.optional.boolean(false), true);
+    }
+    if (T != null) {
+      T.eq(types.isa.optional.boolean(true), true);
+    }
+    if (T != null) {
+      T.eq(types.isa.optional.boolean(null), true);
+    }
+    if (T != null) {
+      T.eq(types.isa.optional.boolean(1), false);
+    }
+    //.........................................................................................................
+    if (T != null) {
+      T.eq(types.validate.boolean(false), false);
+    }
+    if (T != null) {
+      T.eq(types.validate.boolean(true), true);
+    }
+    if (T != null) {
+      T.eq(types.validate.optional.boolean(true), true);
+    }
+    if (T != null) {
+      T.eq(types.validate.optional.boolean(false), false);
+    }
+    if (T != null) {
+      T.eq(types.validate.optional.boolean(void 0), void 0);
+    }
+    if (T != null) {
+      T.eq(types.validate.optional.boolean(null), null);
+    }
+    try_and_show(T, function() {
+      return types.validate.boolean(1);
+    });
+    try_and_show(T, function() {
+      return types.validate.optional.boolean(1);
+    });
+    throws(T, /expected a boolean/, function() {
+      return types.validate.boolean(1);
+    });
+    throws(T, /expected an optional boolean/, function() {
+      return types.validate.optional.boolean(1);
+    });
+    //.........................................................................................................
+    if (T != null) {
+      T.eq(types.type_of(null), 'null');
+    }
+    if (T != null) {
+      T.eq(types.type_of(void 0), 'undefined');
+    }
+    if (T != null) {
+      T.eq(types.type_of(false), 'boolean');
+    }
+    if (T != null) {
+      T.eq(types.type_of(Symbol('p')), 'symbol');
+    }
+    if (T != null) {
+      T.eq(types.type_of({}), 'object');
+    }
+    if (T != null) {
+      T.eq(types.type_of(0/0), 'unknown');
+    }
+    if (T != null) {
+      T.eq(types.type_of(+2e308), 'unknown');
+    }
+    if (T != null) {
+      T.eq(types.type_of(-2e308), 'unknown');
+    }
+    return typeof done === "function" ? done() : void 0;
+  };
+
+  //===========================================================================================================
+  if (module === require.main) {
+    await (async() => {
+      return (await test(this));
+    })();
+  }
+
+}).call(this);
+
+//# sourceMappingURL=test-basics.js.map
---
+++ dev/intertype/src/test-basics.coffee
@@ -0,0 +1,173 @@
+
+
+'use strict'
+
+GUY                       = require 'guy'
+{ alert
+  debug
+  help
+  info
+  plain
+  praise
+  urge
+  warn
+  whisper }               = GUY.trm.get_loggers 'intertype'
+{ rpr
+  inspect
+  echo
+  reverse
+  log     }               = GUY.trm
+test                      = require '../../../apps/guy-test'
+TMP_types                 = new ( require 'intertype' ).Intertype()
+#-----------------------------------------------------------------------------------------------------------
+# s                         = ( name ) -> Symbol.for  name
+# ps                        = ( name ) -> Symbol      name
+
+# #-----------------------------------------------------------------------------------------------------------
+# @[ "_XEMITTER: _" ] = ( T, done ) ->
+#   { DATOM }                 = require '../../../apps/datom'
+#   { new_datom
+#     select }                = DATOM
+  # { Djehuti }               = require '../../../apps/intertalk'
+#   #.........................................................................................................
+#   probes_and_matchers = [
+#     [['^foo', { time: 1500000, value: "msg#1", }],{"time":1500000,"value":"msg#1","$key":"^foo"},null]
+#     ]
+#   #.........................................................................................................
+#   for [ probe, matcher, error, ] in probes_and_matchers
+#     await T.perform probe, matcher, error, -> return new Promise ( resolve, reject ) ->
+#       [ key, value, ] = probe
+#       resolve new_datom key, value
+#   done()
+#   return null
+
+
+############################################################################################################
+#
+# #===========================================================================================================
+# isa_object = ( x ) -> x? and ( typeof x is 'object' ) and ( ( Object::toString.call x ) is '[object Object]' )
+# as_object = ( x ) ->
+#   return x unless isa_object x
+#   R       = {}
+#   R[ k ]  = as_object v for k, v of x
+#   return R
+
+#===========================================================================================================
+sample_declarations =
+  anything:               ( x ) -> true
+  nothing:                ( x ) -> not x?
+  something:              ( x ) -> x?
+  null:                   ( x ) -> x is null
+  undefined:              ( x ) -> x is undefined
+  boolean:                ( x ) -> ( x is true ) or ( x is false )
+  function:               ( x ) -> ( Object::toString.call x ) is '[object Function]'
+  asyncfunction:          ( x ) -> ( Object::toString.call x ) is '[object AsyncFunction]'
+  symbol:                 ( x ) -> ( typeof x ) is 'symbol'
+  object:                 ( x ) -> x? and ( typeof x is 'object' ) and ( ( Object::toString.call x ) is '[object Object]' )
+  text:                   ( x ) -> ( typeof x ) is 'string'
+  nullary:                ( x ) -> x? and ( ( x.length is 0 ) or ( x.size is 0 ) )
+  unary:                  ( x ) -> x? and ( ( x.length is 1 ) or ( x.size is 1 ) )
+  binary:                 ( x ) -> x? and ( ( x.length is 2 ) or ( x.size is 2 ) )
+  trinary:                ( x ) -> x? and ( ( x.length is 3 ) or ( x.size is 3 ) )
+  unknown:                ( x ) -> ( @type_of x ) is 'unknown'
+
+
+#===========================================================================================================
+throws = ( T, matcher, f ) ->
+  await do =>
+    error = null
+    debug '^992-56^', matcher, TMP_types.type_of matcher
+    try await f() catch error
+      switch matcher_type = TMP_types.type_of matcher
+        when 'text'
+          T?.eq error.message, matcher
+        when 'regex'
+          matcher.lastIndex = 0
+          if matcher.test error.message
+            null
+          else
+            warn '^992-4^', reverse message = "error #{rpr error.message} doesn't match #{rpr matcher}"
+            T.fail "^992-4^ #{message}"
+        else
+          warn message = "^992-1^ expected a regex or a text, got a #{matcher_type}"
+          T.fail message
+    T?.ok error?
+  return null
+
+#===========================================================================================================
+try_and_show = ( T, f ) ->
+  e = null
+  try ( urge '^992-2^', f() ) catch e then help '^992-3^', reverse "try_and_show: #{rpr e.message}"
+  unless e?
+    warn '^992-4^', reverse message = "expected an error but none was thrown"
+    T.fail "^992-5^ expected an error but none was thrown"
+  return null
+
+
+
+
+############################################################################################################
+#
+#===========================================================================================================
+@interface = ( T, done ) ->
+  INTERTYPE     = require '../../../apps/intertype'
+  T?.eq ( TMP_types.isa.object    INTERTYPE.types                               ), true
+  T?.eq ( TMP_types.isa.function  INTERTYPE.types.get_isa                       ), true
+  T?.eq ( TMP_types.isa.function  INTERTYPE.types.get_isa_optional              ), true
+  T?.eq ( TMP_types.isa.function  INTERTYPE.types.get_validate                  ), true
+  T?.eq ( TMP_types.isa.function  INTERTYPE.types.get_validate_optional         ), true
+  T?.eq ( TMP_types.isa.object    INTERTYPE.types                               ), true
+  T?.eq ( TMP_types.isa.object    INTERTYPE.types.isa                           ), true
+  T?.eq ( TMP_types.isa.object    INTERTYPE.types.isa.optional                  ), true
+  T?.eq ( TMP_types.isa.object    INTERTYPE.types.validate                      ), true
+  T?.eq ( TMP_types.isa.object    INTERTYPE.types.validate.optional             ), true
+  T?.eq ( TMP_types.isa.function  INTERTYPE.types.isa.boolean                   ), true
+  T?.eq ( TMP_types.isa.function  INTERTYPE.types.isa.optional.boolean          ), true
+  T?.eq ( TMP_types.isa.function  INTERTYPE.types.validate.boolean              ), true
+  T?.eq ( TMP_types.isa.function  INTERTYPE.types.validate.optional.boolean     ), true
+  #.........................................................................................................
+  done?()
+
+#-----------------------------------------------------------------------------------------------------------
+@basic_functionality = ( T, done ) ->
+  # T?.halt_on_error()
+  INTERTYPE     = require '../../../apps/intertype'
+  types         = new INTERTYPE.Intertype sample_declarations
+  T?.eq ( types.isa.boolean           false               ), true
+  T?.eq ( types.isa.boolean           true                ), true
+  T?.eq ( types.isa.boolean           null                ), false
+  T?.eq ( types.isa.boolean           1                   ), false
+  T?.eq ( types.isa.optional.boolean  false               ), true
+  T?.eq ( types.isa.optional.boolean  true                ), true
+  T?.eq ( types.isa.optional.boolean  null                ), true
+  T?.eq ( types.isa.optional.boolean  1                   ), false
+  #.........................................................................................................
+  T?.eq ( types.validate.boolean               false      ), false
+  T?.eq ( types.validate.boolean               true       ), true
+  T?.eq ( types.validate.optional.boolean      true       ), true
+  T?.eq ( types.validate.optional.boolean      false      ), false
+  T?.eq ( types.validate.optional.boolean      undefined  ), undefined
+  T?.eq ( types.validate.optional.boolean      null       ), null
+  try_and_show T,                           -> types.validate.boolean           1
+  try_and_show T,                           -> types.validate.optional.boolean  1
+  throws T, /expected a boolean/,           -> types.validate.boolean           1
+  throws T, /expected an optional boolean/, -> types.validate.optional.boolean  1
+  #.........................................................................................................
+  T?.eq ( types.type_of null            ), 'null'
+  T?.eq ( types.type_of undefined       ), 'undefined'
+  T?.eq ( types.type_of false           ), 'boolean'
+  T?.eq ( types.type_of Symbol 'p'      ), 'symbol'
+  T?.eq ( types.type_of {}              ), 'object'
+  T?.eq ( types.type_of NaN             ), 'unknown'
+  T?.eq ( types.type_of +Infinity       ), 'unknown'
+  T?.eq ( types.type_of -Infinity       ), 'unknown'
+  #.........................................................................................................
+  done?()
+
+
+
+#===========================================================================================================
+if module is require.main then await do =>
+  await test @
+
+
